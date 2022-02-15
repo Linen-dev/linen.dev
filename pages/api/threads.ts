83fa96657f05d48e6af6a024ef080459674c1e5b
cheeseblubber <kamleung911@gmail.com>
2022-02-15 11:21:22 -0500

    Filter by channel id

--- pages/api/threads.ts
+++ pages/api/threads.ts
@@ -5,14 +5,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const channelId = req.body.channel_id;
   const limit = req.body.limit;
   const offSet = req.body.off_set || 0;
+
   const threads = await prisma.slackThread.findMany({
     take: limit,
     skip: offSet,
     include: {
       messages: true,
     },
+    where: { channelId },
   });
 
   res.status(200).json({ threads });
