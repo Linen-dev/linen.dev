444c4b08e070332dbbc0a69f1c093b33b815569d
cheeseblubber <kamleung911@gmail.com>
2022-02-15 11:08:51 -0500

    one off create slack channel

---
+++ pages/api/oneOff.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next/types";
+import prisma from "../../client";
+import {
+  fetchConversations,
+  saveMessages,
+} from "../../fetch_all_conversations";
+import { findOrCreateChannel } from "../../lib/slack";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const channelId = "C030HFK836C";
+  const channel = await findOrCreateChannel(channelId, "kam-sync_test");
+  const slackResponse = await fetchConversations(channelId);
+  console.log(slackResponse.body.messages);
+  const messages = await saveMessages(slackResponse.body.messages, channel.id);
+
+  res.status(200).json({ messages });
+}
+
+export const syncChannel = () => {};
