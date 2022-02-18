62de7551a5ff678218d29e4146837e40f818bea2
cheeseblubber <kamleung911@gmail.com>
2022-02-18 14:09:58 -0500

    fix types and tests

--- pages/channel/[channelId]/index.tsx
+++ pages/channel/[channelId]/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarsGroup, Group, Paper, Text, Title } from '@mantine/core';
+import { Avatar, AvatarsGroup, Paper, Text, Title } from '@mantine/core';
 import { useMemo } from 'react';
 import styled from 'styled-components';
 import { format } from 'timeago.js';
@@ -7,11 +7,7 @@ import Table from '../../../components/table/Table';
 import Message from '../../../components/Message';
 import TableRow from '../../../components/table/TableRow';
 import TableElement from '../../../components/table/TableElement';
-import {
-  accountId,
-  channels,
-  threads as exampleThreads,
-} from '../../../constants/examples';
+import { accountId } from '../../../constants/examples';
 import TableHeader from '../../../components/table/TableHeader';
 import { channelIndex, listUsers, threadIndex } from '../../../lib/slack';
 
@@ -22,7 +18,14 @@ const Wrapper = styled.div({
   overflowY: 'auto',
 });
 
-function Channel({ channelId, users, threads, channels }) {
+type Props = {
+  channelId: string;
+  users: any[];
+  channels: any[];
+  threads: any[];
+};
+
+function Channel({ channelId, users, threads, channels }: Props) {
   const channelName = channels.find((c) => c.id === channelId).channelName;
   const img =
     'https://media-exp1.licdn.com/dms/image/C4E03AQHB_3pem0I_gg/profile-displayphoto-shrink_100_100/0/1542209174093?e=1650499200&v=beta&t=GMX8clmk9wSvKGvrQ4u3IDJQGHaoBz3KQQC9lw3AJuI';
@@ -112,7 +115,13 @@ function Channel({ channelId, users, threads, channels }) {
 
 export default Channel;
 
-export async function getServerSideProps({ params: { channelId } }) {
+type Params = {
+  params: {
+    channelId: string;
+  };
+};
+
+export async function getServerSideProps({ params: { channelId } }: Params) {
   const threads = await threadIndex(channelId, 100);
   const channels = await channelIndex(accountId);
   const users = await listUsers(accountId);
--- tests/api_webhook.test.ts
+++ tests/api_webhook.test.ts
@@ -1,6 +1,6 @@
-import prisma from "../client";
+import prisma from '../client';
 
-import { handleWebhook } from "../pages/api/webhook";
+import { handleWebhook } from '../pages/api/webhook';
 
 beforeEach(async () => {
   await Promise.all([prisma.message.deleteMany()]);
@@ -8,61 +8,61 @@ beforeEach(async () => {
 });
 
 const slackNewMessageEvent = {
-  token: "vDJW8zxjB280BnHi84Z19YfU",
-  team_id: "T017CSH2R70",
-  api_app_id: "A0306BRR6AD",
+  token: 'vDJW8zxjB280BnHi84Z19YfU',
+  team_id: 'T017CSH2R70',
+  api_app_id: 'A0306BRR6AD',
   event: {
-    client_msg_id: "d7f1a339-3dc5-4698-b183-1d4527ab2839",
-    type: "message",
-    text: "more thread",
-    user: "U0174S5F9E3",
-    ts: "1644442986.718559",
-    team: "T017CSH2R70",
+    client_msg_id: 'd7f1a339-3dc5-4698-b183-1d4527ab2839',
+    type: 'message',
+    text: 'more thread',
+    user: 'U0174S5F9E3',
+    ts: '1644442986.718559',
+    team: 'T017CSH2R70',
     blocks: [
       {
-        type: "rich_text",
-        block_id: "nRN",
+        type: 'rich_text',
+        block_id: 'nRN',
         elements: [
           {
-            type: "rich_text_section",
+            type: 'rich_text_section',
             elements: [
               {
-                type: "text",
-                text: "more thread",
+                type: 'text',
+                text: 'more thread',
               },
             ],
           },
         ],
       },
     ],
-    thread_ts: "1644440885.347779",
-    parent_user_id: "U0174S5F9E3",
-    channel: "C030HFK836C",
-    event_ts: "1644442986.718559",
-    channel_type: "channel",
+    thread_ts: '1644440885.347779',
+    parent_user_id: 'U0174S5F9E3',
+    channel: 'C030HFK836C',
+    event_ts: '1644442986.718559',
+    channel_type: 'channel',
   },
-  type: "event_callback",
-  event_id: "Ev032GNDD3MJ",
+  type: 'event_callback',
+  event_id: 'Ev032GNDD3MJ',
   event_time: 1644442986,
   authorizations: [
     {
       enterprise_id: null,
-      team_id: "T017CSH2R70",
-      user_id: "U0174S5F9E3",
+      team_id: 'T017CSH2R70',
+      user_id: 'U0174S5F9E3',
       is_bot: false,
       is_enterprise_install: false,
     },
   ],
   is_ext_shared_channel: false,
   event_context:
-    "4-eyJldCI6Im1lc3NhZ2UiLCJ0aWQiOiJUMDE3Q1NIMlI3MCIsImFpZCI6IkEwMzA2QlJSNkFEIiwiY2lkIjoiQzAzMEhGSzgzNkMifQ",
+    '4-eyJldCI6Im1lc3NhZ2UiLCJ0aWQiOiJUMDE3Q1NIMlI3MCIsImFpZCI6IkEwMzA2QlJSNkFEIiwiY2lkIjoiQzAzMEhGSzgzNkMifQ',
 };
 
-test("Creates a message when Slack sends a slack event", async () => {
-  const channel = await prisma.channel.create({
+test('Creates a message when Slack sends a slack event', async () => {
+  const channel = await prisma.channels.create({
     data: {
       slackChannelId: slackNewMessageEvent.event.channel,
-      channelName: "someChannel",
+      channelName: 'someChannel',
     },
   });
 
