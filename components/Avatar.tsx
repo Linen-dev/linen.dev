906f97b0177939402e4ce28f80eea2a21cc7b85f
cheeseblubber <kamleung911@gmail.com>
2022-02-18 11:39:53 -0500

    remove avatar

--- components/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-import Avatar from '@mantine/core';
-
-function Avatar({ name, img }) {
-  console.log('name, img :>> ', name, img);
-  return <div>{name}</div>;
-  // return <Avatar src={img} alt={name} />;
-}
-
-export default Avatar;
