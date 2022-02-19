a9cefec284e1567f61f77df13d30f9b53e926483
cheeseblubber <kamleung911@gmail.com>
2022-02-19 18:52:58 -0500

    set up nextconfig for papercups

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+//https://vercel.com/support/articles/can-i-redirect-from-a-subdomain-to-a-subpath
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `papercups.linen.dev`,
+        // this rewrite will be applied
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'papercups.linen.dev',
+            },
+          ],
+          destination: '/papercups/:path*',
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
