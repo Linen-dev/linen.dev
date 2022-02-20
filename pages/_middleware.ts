0226b442a2ff6aeecf7ef231e9cd8816f60b7f3c
cheeseblubber <kamleung911@gmail.com>
2022-02-20 12:35:58 -0500

    console.log

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,6 +4,8 @@ export default function middleware(req) {
   const url = req.nextUrl.clone(); // clone the request url
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = req.headers.get('host'); // get hostname of request (e.g. demo.vercel.pub)
+  console.log({ url });
+  console.log({ headers: req.headers });
 
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
