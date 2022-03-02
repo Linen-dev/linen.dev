b918418d15e291eeafacf2a60e0a7484ea96eefb
Emil Ajdyna <emil@ajdyna.com>
2022-03-02 16:37:15 +0100

    Add new fields to accounts

---
+++ pages/api/accounts.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {}
---
+++ prisma/migrations/20220302153442_add_fields_to_accounts/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "accounts" ADD COLUMN     "brandColor" TEXT,
+ADD COLUMN     "docsUrl" TEXT,
+ADD COLUMN     "homeUrl" TEXT;
