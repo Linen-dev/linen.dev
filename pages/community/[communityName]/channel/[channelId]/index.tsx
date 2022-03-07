820bd696a01694ca824690ba10c8c64c82cdf4a7
cheeseblubber <kamleung911@gmail.com>
2022-03-07 10:30:03 -0500

    Remove duplicate code by importing channel

--- pages/community/[communityName]/channel/[channelId]/index.tsx
+++ pages/community/[communityName]/channel/[channelId]/index.tsx
@@ -1,199 +1,18 @@
-import { Avatar, AvatarsGroup, Paper, Text, Title } from '@mantine/core';
-import Link from 'next/link';
-import { useMemo } from 'react';
-import styled from 'styled-components';
-import { format } from 'timeago.js';
-import PageLayout from '../../../../../components/layout/PageLayout';
-import Message from '../../../../../components/Message';
+import { links } from '../../../../../constants/examples';
 import {
   findAccountByPath,
   listUsers,
   threadIndex,
 } from '../../../../../lib/slack';
+import Channel from '../../../[communityName]/index';
 import serializeThread from '../../../../../serializers/thread';
-import { links } from '../../../../../constants/examples';
-import { channels, slackThreads, users } from '@prisma/client';
-
-const EXCERPT_LENGTH = 220;
-
-const Wrapper = styled.div({
-  height: 'calc(100vh - 98px)',
-  overflowY: 'auto',
-});
-
-type Props = {
-  channelId: string;
-  users: users[];
-  channels: channels[];
-  threads: any[];
-  slackUrl: string;
-  settings: any;
-};
-
-function Channel({
-  channelId,
-  users,
-  threads,
-  channels,
-  slackUrl,
-  settings,
-}: Props) {
-  const channelName = channels.find((c) => c.id === channelId).channelName;
-
-  const rows = useMemo(() => {
-    return threads.map(({ messages, id: threadId, viewCount }) => {
-      const oldestMessage = messages[messages.length - 1];
-      const newestMessage = messages[0];
-      const participants = messages.reduce((agg, { author }) => {
-        if (
-          author &&
-          !agg.find((a) => a.profileImageUrl === author.profileImageUrl)
-        ) {
-          agg.push(author);
-        }
-        return agg;
-      }, []);
-      const author = participants[0];
-
-      return (
-        <div className="border-solid border-gray-200">
-          <li className="px-4 py-4 hover:bg-gray-50  sm:hidden cursor-pointer">
-            <Link
-              key={threadId}
-              href={`/channel/${channelId}/thread/${threadId}`}
-              passHref
-            >
-              <div className="flex">
-                <div className="flex pr-4 items-center sm:hidden">
-                  <Avatar
-                    radius="xl"
-                    key={author?.id || author?.displayName}
-                    color="indigo"
-                    src={author?.profileImageUrl} // set placeholder with a U sign
-                    alt={author?.displayName} // Set placeholder of a slack user if missing
-                  >
-                    <Text style={{ marginTop: '-2px', fontSize: '14px' }}>
-                      {(author?.displayName || '?').slice(0, 1).toLowerCase()}
-                    </Text>
-                  </Avatar>
-                </div>
-                <div className="flex flex-col w-full">
-                  <div className="pb-2 sm:px-6">
-                    <Message users={users} text={oldestMessage.body} truncate />
-                  </div>
-                  <div className="hidden sm:block"></div>
-                  <div className="text-sm text-gray-400 flex flex-row justify-between">
-                    <p>{messages.length} Views</p>
-                    {format(new Date(newestMessage.sentAt))}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-        </div>
-      );
-    });
-  }, [channelId, threads, users]);
-
-  const tableRows = useMemo(() => {
-    return threads.map(({ messages, id: threadId, viewCount }) => {
-      const oldestMessage = messages[messages.length - 1];
-      const newestMessage = messages[0];
-      const participants = messages.reduce((agg, { author }) => {
-        if (
-          author &&
-          !agg.find((a) => a.profileImageUrl === author.profileImageUrl)
-        ) {
-          agg.push(author);
-        }
-        return agg;
-      }, []);
-      const author = participants[0];
-      return (
-        <Link
-          key={threadId}
-          href={`/channel/${channelId}/thread/${threadId}`}
-          passHref
-        >
-          <tr className="border-solid border-gray-200 cursor-pointer">
-            <td className="px-6 py-3 max-w-[600px]">
-              <Message users={users} text={oldestMessage.body} truncate />
-            </td>
-            <td className="px-6 py-3 align-middle">
-              <AvatarsGroup size={36} limit={3}>
-                {participants.map((p, idx) => (
-                  <Avatar
-                    radius="xl"
-                    key={p.id || p.displayName}
-                    color="indigo"
-                    src={p?.profileImageUrl} // set placeholder with a U sign
-                    alt={p?.displayName} // Set placeholder of a slack user if missing
-                  >
-                    <Text style={{ marginTop: '-2px', fontSize: '14px' }}>
-                      {(p?.displayName || '?').slice(0, 1).toLowerCase()}
-                    </Text>
-                  </Avatar>
-                ))}
-              </AvatarsGroup>
-            </td>
-            <td className="px-6 py-3 text-sm">{viewCount}</td>
-            <td className="px-6 py-3 text-sm">{messages.length}</td>
-            <td className="px-6 py-3 text-sm">
-              {format(new Date(newestMessage.sentAt))}
-            </td>
-          </tr>
-        </Link>
-      );
-    });
-  }, [channelId, threads, users]);
-
-  return (
-    //Super hacky mobile friendly - different component gets
-    //rendered when it is smaller than a specific size and gets unhidden
-    <PageLayout
-      users={users}
-      slackUrl={slackUrl}
-      seo={{ title: `${channelName} threads` }}
-      navItems={{ channels: channels }}
-      settings={settings}
-    >
-      <div className="pt-8 sm:flex sm:justify-center">
-        <table className="hidden sm:block sm:table-fixed ">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 text-left font-medium text-gray-500">
-                Topic
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500">
-                Authors
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500">
-                Views
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500">
-                Replies
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500">
-                Activity
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y">{tableRows}</tbody>
-        </table>
-        <ul className="divide-y sm:hidden">{rows}</ul>
-      </div>
-    </PageLayout>
-  );
-}
-
-export default Channel;
-
 type Params = {
   params: {
     communityName: string;
   };
 };
 
+//TODO: Remove getServerSideProp duplicate code
 export async function getServerSideProps({
   params: { communityName },
 }: Params) {
@@ -229,3 +48,4 @@ export async function getServerSideProps({
     },
   };
 }
+export default Channel;
