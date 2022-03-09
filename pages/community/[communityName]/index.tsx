c37ffb60dc22dc62ca7367c1b4c798d0de6571da
Emil Ajdyna <emil@ajdyna.com>
2022-03-09 17:48:38 +0100

    Nicer pagination

--- pages/community/[communityName]/index.tsx
+++ pages/community/[communityName]/index.tsx
@@ -9,6 +9,7 @@ import { findAccountByPath, listUsers } from '../../../lib/slack';
 import { links } from '../../../constants/examples';
 import { channels, users } from '@prisma/client';
 import { index as fetchThreads } from '../../../services/threads';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
 interface Pagination {
   totalCount: number;
@@ -208,61 +209,33 @@ function Channel({
         </table>
         <ul className="divide-y sm:hidden">{rows}</ul>
       </div>
-      <ReactPaginate
-        breakLabel="..."
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        containerClassName="flex justify-center py-5"
-        breakClassName="flex items-center"
-        breakLinkClassName="block py-2 px-4"
-        previousClassName="flex items-center"
-        hrefBuilder={(pageNumber) => {
-          return `?page=${pageNumber}`;
-        }}
-        previousLinkClassName="block py-2 px-4"
-        previousLabel={
-          <>
-            <span className="sr-only">Previous</span>
-            <svg
-              className="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </>
-        }
-        pageClassName="flex items-center"
-        pageLinkClassName="block py-2 px-4"
-        nextClassName="flex items-center"
-        nextLinkClassName="block py-2 px-4"
-        nextLabel={
-          <>
-            <span className="sr-only">Next</span>
-            <svg
-              className="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </>
-        }
-        renderOnZeroPageCount={null}
-      />
+      <div className="flex justify-center p-5 m-5">
+        <ReactPaginate
+          breakLabel="..."
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          containerClassName="relative z-0 inline-flex rounded-md shadow-lg -space-x-px"
+          breakLinkClassName="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          hrefBuilder={(page: number) => `?page=${page}`}
+          activeLinkClassName="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          pageLinkClassName="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+          previousLinkClassName="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+          previousLabel={
+            <>
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" />
+            </>
+          }
+          nextLinkClassName="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+          nextLabel={
+            <>
+              <ChevronRightIcon className="h-5 w-5" />
+            </>
+          }
+          renderOnZeroPageCount={null}
+        />
+      </div>
     </PageLayout>
   );
 }
