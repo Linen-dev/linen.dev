7a6eea5492deb664efe1bf4880d1286070dd3021
Emil Ajdyna <emil@ajdyna.com>
2022-03-12 16:27:32 +0100

    Add basic specs

---
+++ utilities/sitemap.test.ts
@@ -0,0 +1,17 @@
+import { createXMLSitemap, createXMLSitemapForSubdomain } from './sitemap';
+
+describe('#createXMLSitemap', () => {
+  it('creates a sitemap', async () => {
+    const sitemap = await createXMLSitemap([{ url: '/' }]);
+    expect(sitemap).toContain('<url><loc>https://localhost:3000/</loc></url>');
+  });
+});
+
+describe('#createXMLSitemapForSubdomain', () => {
+  it('creates a sitemap for a subdomain', async () => {
+    const sitemap = await createXMLSitemapForSubdomain('test');
+    expect(sitemap).toContain(
+      '<url><loc>https://test.localhost:3000/</loc></url>'
+    );
+  });
+});
