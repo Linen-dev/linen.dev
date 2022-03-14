a368a5a8463b82cc69f8362a564d7b0d0cfd3518
Emil Ajdyna <emil@ajdyna.com>
2022-03-14 16:53:43 +0100

    Add html[lang] and missing alt

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 //https://vercel.com/support/articles/can-i-redirect-from-a-subdomain-to-a-subpath
 
 const nextConfig = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
 };
 
