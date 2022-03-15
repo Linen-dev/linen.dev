0efc464ce3201b0aff7833e07b6a0dabec8097ad
Emil Ajdyna <emil@ajdyna.com>
2022-03-15 18:26:48 +0100

    Replace Avatars with an async version

---
+++ components/Avatars/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Avatar from '../Avatar';
+import styles from './index.module.css';
+
+interface AvatarType {
+  src?: string;
+  alt?: string;
+  text: string;
+}
+
+interface Props {
+  users: AvatarType[];
+}
+
+export default function AvatarsGroup({ users }: Props) {
+  const avatars = users.slice(0, 3);
+  return (
+    <div className={styles.group}>
+      {avatars.map((user, index) => (
+        <div key={`${user.text}-${index}`} className={styles.item}>
+          <Avatar key={`${index}-avatar`} {...user} />
+        </div>
+      ))}
+      {users.length > 3 && (
+        <div className={styles.item}>
+          <div className={styles.placeholder}>{`+${users.length - 3}`}</div>
+        </div>
+      )}
+    </div>
+  );
+}
