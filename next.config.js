ba03cd9a992b2e19af681599c9fbe50a18750e46
cheeseblubber <kamleung911@gmail.com>
2022-03-18 15:46:03 -0400

    add sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 //https://vercel.com/support/articles/can-i-redirect-from-a-subdomain-to-a-subpath
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
   reactStrictMode: true,
@@ -7,5 +8,16 @@ const nextConfig = {
     domains: ['avatars.slack-edge.com'],
   },
 };
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-module.exports = nextConfig;
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
