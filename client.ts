5cd7ec478a78c00b8269094dbb704bae8a607b85
cheeseblubber <kamleung911@gmail.com>
2022-03-22 10:17:27 -0400

    use single global for prisma

--- client.ts
+++ client.ts
@@ -1,4 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
 export default prisma;
