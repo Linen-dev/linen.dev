a553f9a48a291d68b52f0ec32578e6116a5a1344
cheeseblubber <kamleung911@gmail.com>
2022-03-31 23:22:29 -0400

    fix links on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,16 +63,17 @@ const Home: NextPage = () => {
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
             <div className="rounded-md shadow">
               <a
-                href="#"
+                href="https://linen.dev/signup"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
               >
-                Get started
+                Sign up
               </a>
             </div>
             <div className="mt-3 sm:mt-0 sm:ml-3">
               <a
-                href="#"
+                href="https://osquery.fleetdm.com"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+                target="_blank"
               >
                 Live demo
               </a>
