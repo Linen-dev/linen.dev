d745a501e0e5b5c3532d68dd0e1b7708239c1ed1
cheeseblubber <kamleung911@gmail.com>
2022-04-03 23:18:40 -0400

    Add loading bar to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,37 @@
 import '../styles/reset.css';
+import { useRouter } from 'next/router';
+import '../nprogress.css';
+import NProgress from 'nprogress';
 import '../styles/globals.css';
 import { SWRConfig } from 'swr';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
 import { useEffect } from 'react';
 
 export default function App(props: AppProps) {
+  const router = useRouter();
+
   const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
 
   return (
     <>
@@ -25,3 +49,50 @@ export default function App(props: AppProps) {
     </>
   );
 }
+
+// export default function App({ Component, pageProps }) {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const handleStart = (url) => {
+//       console.log(`Loading: ${url}`);
+//       NProgress.start();
+//     };
+//     const handleStop = () => {
+//       NProgress.done();
+//     };
+
+//     router.events.on('routeChangeStart', handleStart);
+//     router.events.on('routeChangeComplete', handleStop);
+//     router.events.on('routeChangeError', handleStop);
+
+//     return () => {
+//       router.events.off('routeChangeStart', handleStart);
+//       router.events.off('routeChangeComplete', handleStop);
+//       router.events.off('routeChangeError', handleStop);
+//     };
+//   }, [router]);
+
+//   return (
+//     <>
+//       <nav>
+//         <style jsx>{`
+//           a {
+//             margin: 0 10px 0 0;
+//           }
+//         `}</style>
+//         <Link href="/">
+//           <a>Home</a>
+//         </Link>
+//         <Link href="/about">
+//           <a>About</a>
+//         </Link>
+//         <Link href="/forever">
+//           <a>Forever</a>
+//         </Link>
+//         <a href="/non-existing">Non Existing Page</a>
+//       </nav>
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
