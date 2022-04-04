de9d916b6de7aadf8c9aceaa43689c3d34c42a1e
cheeseblubber <kamleung911@gmail.com>
2022-04-03 21:22:10 -0400

    incremental static caching

--- pages/s/[communityName]/t/[threadId]/[slug]/index.tsx
+++ pages/s/[communityName]/t/[threadId]/[slug]/index.tsx
@@ -1,14 +1,22 @@
-import { GetServerSidePropsContext } from 'next';
+import { GetStaticPropsContext } from 'next';
 import { getThreadById } from '../../../../../../services/threads';
 import Thread from '../../../../../../components/Pages/Thread/Thread';
 
 export default Thread;
 
 //Renders the same page as /threadId
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+export async function getStaticProps(context: GetStaticPropsContext) {
   const threadId = context.params?.threadId as string;
   const thread = await getThreadById(threadId);
   return {
     props: { ...thread, isSubDomainRouting: false },
+    revalidate: 60, // In seconds
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
   };
 }
--- pages/s/[communityName]/t/[threadId]/index.tsx
+++ pages/s/[communityName]/t/[threadId]/index.tsx
@@ -1,13 +1,22 @@
 import { getThreadById } from '../../../../../services/threads';
-import { GetServerSidePropsContext } from 'next';
+import { GetStaticPropsContext } from 'next';
 import Thread from '../../../../../components/Pages/Thread/Thread';
 
 export default Thread;
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+//Renders the same page as /threadId
+export async function getStaticProps(context: GetStaticPropsContext) {
   const threadId = context.params?.threadId as string;
   const thread = await getThreadById(threadId);
   return {
     props: { ...thread, isSubDomainRouting: false },
+    revalidate: 60, // In seconds
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
   };
 }
