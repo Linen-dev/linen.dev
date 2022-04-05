978a58d57fa4f0939ed7fbb619fa3e1f0b62325f
cheeseblubber <kamleung911@gmail.com>
2022-04-04 21:59:50 -0400

    Revert "Google analytics id"
    This reverts commit 9320096df599b62676b4f4d35ace5c1bfa601f97.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,8 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import Script from 'next/script';
 
 const POSTHOG_API_KEY = process.env.NEXT_PUBLIC_POSTHOG_API_KEY || '';
-const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
 
 export default function App(props: AppProps) {
   if (POSTHOG_API_KEY) {
@@ -45,17 +43,6 @@ export default function App(props: AppProps) {
   return (
     <SessionProvider>
       <Head>
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', ${GOOGLE_ANALYTICS_ID}, 'auto');
-          ga('send', 'pageview');
-        `}
-        </Script>
-        <Script
-          src="https://www.google-analytics.com/analytics.js"
-          strategy="afterInteractive"
-        />
         <title>Linen Community</title>
         <meta
           name="viewport"
