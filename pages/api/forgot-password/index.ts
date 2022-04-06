d02c2ec77429f65190306151b4e4937382ecc81c
Emil Ajdyna <emil@ajdyna.com>
2022-04-06 21:09:34 +0200

    Reset password basics

--- pages/api/forgot-password/index.ts
+++ pages/api/forgot-password/index.ts
@@ -1,16 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import ApplicationMailer from '../../../mailers/ApplicationMailer';
 
+const HOST = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+
 function create(request: NextApiRequest, response: NextApiResponse) {
   const { email } = JSON.parse(request.body);
   if (!email) {
     return response.status(400).json({ error: 'Email is required' });
   }
+  // const token = await Token.create({ email });
   ApplicationMailer.send({
     to: email,
     subject: 'Linen.dev - Reset your password',
-    text: 'Reset your password here: https://linen.dev/reset-password?token=1234',
-    html: "Reset your password here: <a href='https://linen.dev/reset-password?token=1234'>https://linen.dev/reset-password?token=1234</a>",
+    text: `Reset your password here: ${HOST}/reset-password?token=1234`,
+    html: `Reset your password here: <a href='${HOST}/reset-password?token=1234'>${HOST}/reset-password?token=1234</a>`,
   });
   return response.status(200).json({});
 }
