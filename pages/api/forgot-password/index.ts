e8da594022a60c5835732460c7cfc4054c1b7508
Emil Ajdyna <emil@ajdyna.com>
2022-04-07 14:45:18 +0200

    Generate and use a token

--- pages/api/forgot-password/index.ts
+++ pages/api/forgot-password/index.ts
@@ -1,20 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import ApplicationMailer from '../../../mailers/ApplicationMailer';
+import { generateToken } from '../../../utilities/token';
+import prisma from '../../../client';
 
 const HOST = process.env.NEXTAUTH_URL || 'http://localhost:3000';
 
-function create(request: NextApiRequest, response: NextApiResponse) {
+async function create(request: NextApiRequest, response: NextApiResponse) {
   const { email } = JSON.parse(request.body);
   if (!email) {
     return response.status(400).json({ error: 'Email is required' });
   }
-  // const token = await Token.create({ email });
-  ApplicationMailer.send({
-    to: email,
-    subject: 'Linen.dev - Reset your password',
-    text: `Reset your password here: ${HOST}/reset-password?token=1234`,
-    html: `Reset your password here: <a href='${HOST}/reset-password?token=1234'>${HOST}/reset-password?token=1234</a>`,
-  });
+  try {
+    const token = generateToken();
+    await prisma.auths.update({
+      where: { email },
+      data: {
+        token,
+      },
+    });
+    ApplicationMailer.send({
+      to: email,
+      subject: 'Linen.dev - Reset your password',
+      text: `Reset your password here: ${HOST}/reset-password?token=${token}`,
+      html: `Reset your password here: <a href='${HOST}/reset-password?token=${token}'>${HOST}/reset-password?token=${token}</a>`,
+    });
+  } catch (exception) {
+    return response.status(200).json({});
+  }
+
   return response.status(200).json({});
 }
 
