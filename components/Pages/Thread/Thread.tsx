f1ba8e0fd2955a8bae337a8f06d116b05fcd9153
cheeseblubber <kamleung911@gmail.com>
2022-04-06 21:44:37 -0400

    add more padding on the right

--- components/Pages/Thread/Thread.tsx
+++ components/Pages/Thread/Thread.tsx
@@ -71,7 +71,7 @@ export default function Thread({
       settings={settings}
       isSubDomainRouting={isSubDomainRouting}
     >
-      <div className="py-8 px-4 md:pl-2 lg:pl-0 md:pr-20">
+      <div className="py-8 px-4 md:pl-2 lg:pl-0 md:pr-20 lg:pr-40">
         <ul>{elements}</ul>
 
         <div className={styles.buttons}>
