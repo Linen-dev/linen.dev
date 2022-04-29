b387f0319453b21173ce11593e12567b0633e20c
Emil Ajdyna <emil@ajdyna.com>
2022-04-29 16:21:55 +0200

    Dynamic sidebar links

--- components/layout/DashboardLayout/SidebarLink/index.tsx
+++ components/layout/DashboardLayout/SidebarLink/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import classNames from 'classnames';
 
 interface Props {
   icon: React.ReactNode;
   href: string;
   text: string;
+  active?: boolean;
 }
 
-export default function SidebarLink({ icon, href, text }: Props) {
+export default function SidebarLink({ icon, href, text, active }: Props) {
   return (
     <a
       href={href}
-      className="bg-purple-50 border-purple-600 text-purple-600 group border-l-4 py-2 px-3 flex items-center text-sm font-medium"
+      className={classNames(
+        'border-purple-600 group border-l-4 py-2 px-3 flex items-center text-sm font-medium',
+        {
+          'bg-purple-50': active,
+          'text-purple-600': active,
+        }
+      )}
     >
-      <div className="text-purple-500 mr-3 flex-shrink-0 h-6 w-6">{icon}</div>
+      <div
+        className={classNames('mr-3 flex-shrink-0 h-6 w-6', {
+          'text-purple-500': active,
+        })}
+      >
+        {icon}
+      </div>
       {text}
     </a>
   );
