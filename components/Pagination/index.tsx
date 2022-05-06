a9d4413524a8aa1b1cfb02cfcf6d0849d452d5f8
Emil Ajdyna <emil@ajdyna.com>
2022-05-06 17:10:15 +0200

    Show pagination on mobile

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -16,25 +16,25 @@ export default function Pagination({ onClick, pageCount, channelName }: Props) {
   }
   return (
     <div className={styles.pagination}>
-      <div className="lg:flex justify-center hidden md:hidden">
+      <div className="flex justify-center">
         <ReactPaginate
           breakLabel="..."
           onPageChange={onClick}
-          pageRangeDisplayed={5}
+          pageRangeDisplayed={2}
           pageCount={pageCount}
-          containerClassName="border border-solid border-gray-200 inline-flex rounded-md -space-x-px"
+          containerClassName="border border-solid shadow-sm border-gray-200 inline-flex rounded-md -space-x-px"
           breakLinkClassName="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
           hrefBuilder={(page: number) => `/c/${channelName}/${page}`}
           activeLinkClassName="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
           pageLinkClassName="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-          previousLinkClassName="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50"
+          previousLinkClassName="bg-white relative inline-flex items-center px-2 py-2 rounded-md border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50"
           previousLabel={
             <>
               <span className="sr-only">Previous</span>
               <FontAwesomeIcon icon={faAngleLeft} className="h-5 w-5" />
             </>
           }
-          nextLinkClassName="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50"
+          nextLinkClassName="bg-white relative inline-flex items-center px-2 py-2 rounded-md border border-gray-300 text-sm font-medium text-gray-500 hover:bg-gray-50"
           nextLabel={
             <>
               <span className="sr-only">Next</span>
