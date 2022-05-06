998fdac38e6cb30743bb5be2064fdfb85dd81278
Emil Ajdyna <emil@ajdyna.com>
2022-05-06 16:55:16 +0200

    Add threads

--- bin/factory/index.ts
+++ bin/factory/index.ts
@@ -1,6 +1,8 @@
 import { createAuth, findAuth } from '../../lib/auth';
 import { createAccount, findAccount } from '../../lib/account';
 import { createChannel, findChannel } from '../../lib/channel';
+import { createThread, findThread } from '../../lib/thread';
+import { createMessage, findMessage } from '../../lib/message';
 
 export async function findOrCreateAccount({ domain }: { domain: string }) {
   return (
@@ -44,6 +46,43 @@ export async function findOrCreateChannel({
       name,
       accountId,
       slackChannelId: 'slack-channel-id-1234',
+    }))
+  );
+}
+
+export async function findOrCreateThread({
+  channelId,
+  slug,
+}: {
+  channelId: string;
+  slug: string;
+}) {
+  return (
+    (await findThread({ channelId, slug })) ||
+    (await createThread({
+      channelId,
+      messageCount: 2,
+      slackThreadTs: `slack-thread-ts-${slug}`,
+      slug,
+    }))
+  );
+}
+
+export async function findOrCreateMessage({
+  channelId,
+  threadId,
+  body,
+}: {
+  channelId: string;
+  threadId: string;
+  body: string;
+}) {
+  return (
+    (await findMessage({ channelId, threadId, body })) ||
+    (await createMessage({
+      channelId,
+      threadId,
+      body,
     }))
   );
 }
--- bin/seed.ts
+++ bin/seed.ts
@@ -2,13 +2,37 @@ import {
   findOrCreateAccount,
   findOrCreateUser,
   findOrCreateChannel,
+  findOrCreateThread,
+  findOrCreateMessage,
 } from './factory';
 
 (async () => {
   const account = await findOrCreateAccount({ domain: 'linen.dev' });
+
   await findOrCreateUser({ email: 'emil@linen.dev', accountId: account.id });
   await findOrCreateUser({ email: 'jarek@linen.dev', accountId: account.id });
   await findOrCreateUser({ email: 'kam@linen.dev', accountId: account.id });
   await findOrCreateUser({ email: 'sandro@linen.dev', accountId: account.id });
-  await findOrCreateChannel({ name: 'general', accountId: account.id });
+
+  const channel = await findOrCreateChannel({
+    name: 'general',
+    accountId: account.id,
+  });
+
+  for (let i = 0; i < 100; i++) {
+    const thread = await findOrCreateThread({
+      channelId: channel.id,
+      slug: `slug-${i}`,
+    });
+    await findOrCreateMessage({
+      body: `foo-${i}`,
+      channelId: channel.id,
+      threadId: thread.id,
+    });
+    await findOrCreateMessage({
+      body: `bar-${i}`,
+      channelId: channel.id,
+      threadId: thread.id,
+    });
+  }
 })();
