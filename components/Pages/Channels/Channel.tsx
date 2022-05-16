5c1f239d98f47e24a1dc8764aa988fd6587e5a5b
Emil Ajdyna <emil@ajdyna.com>
2022-05-16 22:30:31 +0200

    Better mobile behavior

--- components/Pages/Channels/Channel.tsx
+++ components/Pages/Channels/Channel.tsx
@@ -11,6 +11,7 @@ import { MentionsWithUsers } from '../../../types/apiResponses/threads/[threadId
 import { capitalize } from '../../../lib/util';
 import CustomRouterPush from 'components/Link/CustomRouterPush';
 import CopyToClipboardLink from './CopyToClipboardLink';
+import styles from './Channel.module.css';
 
 export interface PaginationType {
   totalCount: number;
@@ -143,7 +144,7 @@ export default function Channel({
     return (
       <li
         key={incrementId}
-        className="px-4 py-4 hover:bg-gray-50 border-solid border-gray-200 sm:hidden cursor-pointer"
+        className="px-4 py-4 hover:bg-gray-50 border-solid border-gray-200 md:hidden cursor-pointer"
       >
         <CustomLink
           isSubDomainRouting={isSubDomainRouting}
@@ -153,7 +154,7 @@ export default function Channel({
           key={`${incrementId}-desktop`}
         >
           <div className="flex">
-            <div className="flex pr-4 items-center sm:hidden">
+            <div className="flex pr-4 items-center md:hidden">
               {author && (
                 <Avatar
                   key={`${incrementId}-${
@@ -166,7 +167,7 @@ export default function Channel({
               )}
             </div>
             <div className="flex flex-col w-full">
-              <div className="pb-2 sm:px-6">
+              <div className="pb-2 md:px-6">
                 <Message
                   text={oldestMessage.body}
                   truncate
@@ -209,7 +210,7 @@ export default function Channel({
           key={`${incrementId}-desktop`}
         >
           <tr className="border-solid border-gray-200 cursor-pointer">
-            <td className="px-6 py-3 align-middle md:max-w-[800px]">
+            <td className={styles.td}>
               <Message
                 text={oldestMessage.body}
                 truncate
@@ -234,7 +235,7 @@ export default function Channel({
             <td className="px-6 py-3 text-sm align-middle min-w-[120px]">
               {format(new Date(oldestMessage.sentAt))}
             </td>
-            <td className="pl-6 text-sm text-center align-middle">
+            <td className="px-6 text-sm text-center align-middle">
               <CopyToClipboardLink
                 isSubDomainRouting={isSubDomainRouting}
                 communityName={communityName}
@@ -286,14 +287,14 @@ export default function Channel({
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500">
                 Activity
               </th>
-              <th className="pl-6 text-right text-xs font-medium text-gray-500">
+              <th className="px-6 text-center text-xs font-medium text-gray-500">
                 Actions
               </th>
             </tr>
           </thead>
           <tbody className="divide-y">{tableRows}</tbody>
         </table>
-        <ul className="divide-y sm:hidden">{rows}</ul>
+        <ul className="divide-y md:hidden">{rows}</ul>
         {pageCount && (
           <Pagination
             channelName={currentChannel.channelName}
