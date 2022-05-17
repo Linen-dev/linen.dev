387495439c266dc8fdcb0808a4bdcd55ebb93c51
Sandro de Souza <sandesou@gmail.com>
2022-05-17 18:55:03 -0300

    hide unit test logs

--- tests/unit/random-word-slugs.test.ts
+++ tests/unit/random-word-slugs.test.ts
@@ -5,7 +5,7 @@ describe('random-word-slugs', () => {
   it('simple slug on kebab', () => {
     for (let i = 15; i--; ) {
       const slug = generateSlug(numberOfWords, options);
-      console.log(slug);
+      // console.log(slug);
       expect(slug).toBeDefined();
       const parts = slug.split('-');
       expect(parts.length).toBe(numberOfWords);
@@ -13,7 +13,7 @@ describe('random-word-slugs', () => {
   });
   it('should have more than 300k', () => {
     const totalSlugs = totalUniqueSlugs(numberOfWords, options);
-    console.log(totalSlugs);
+    // console.log(totalSlugs);
     expect(totalSlugs).toBeGreaterThan(300000);
   });
 });
