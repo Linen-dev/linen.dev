874b747b1864e47592590ca5c152919b67f99de6
Sandro de Souza <sandesou@gmail.com>
2022-05-17 18:02:19 -0300

    Typo

--- prisma/migrations/20220517194852_feat_anonymous_users/migration.sql
+++ prisma/migrations/20220517194852_feat_anonymous_users/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "accounts" ADD COLUMN     "anonimyzeUsers" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "accounts" ADD COLUMN     "anonymizeUsers" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "anonymousAlias" TEXT;
