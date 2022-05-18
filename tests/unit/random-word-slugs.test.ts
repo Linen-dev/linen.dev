c6a8f7c9ed8d899c2d582b79d7106034ae8ddede
Sandro de Souza <sandesou@gmail.com>
2022-05-18 17:15:29 -0300

    Replace external lib by our own with numbers

--- tests/unit/random-word-slugs.test.ts
+++ tests/unit/random-word-slugs.test.ts
@@ -1,18 +1,17 @@
-import { numberOfWords, options } from '@/utilities/randomWordSlugs';
-import { generateSlug, totalUniqueSlugs } from 'random-word-slugs';
+import { generateSlug, totalUniqueSlugs } from 'lib/random-word-slugs';
 
 describe('random-word-slugs', () => {
   it('simple slug on kebab', () => {
     for (let i = 15; i--; ) {
-      const slug = generateSlug(numberOfWords, options);
+      const slug = generateSlug();
       // console.log(slug);
       expect(slug).toBeDefined();
       const parts = slug.split('-');
-      expect(parts.length).toBe(numberOfWords);
+      expect(parts.length).toBe(3);
     }
   });
   it('should have more than 300k', () => {
-    const totalSlugs = totalUniqueSlugs(numberOfWords, options);
+    const totalSlugs = totalUniqueSlugs();
     // console.log(totalSlugs);
     expect(totalSlugs).toBeGreaterThan(300000);
   });
