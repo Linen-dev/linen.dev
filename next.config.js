4eb7cd1973e9d4d18e3326ddf69d3d2761bd51ce
Jarosław Gilewski <jarek@linen.dev>
2022-05-22 19:04:35 +0200

    LIN-49 Logo uploading in the settings page

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ const nextConfig = {
   },
   reactStrictMode: true,
   images: {
-    domains: ['avatars.slack-edge.com', 'cdn.discordapp.com'],
+    domains: [
+      'avatars.slack-edge.com',
+      'cdn.discordapp.com',
+      `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
+    ],
   },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
