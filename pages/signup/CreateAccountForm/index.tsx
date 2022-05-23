32893706388109a6eeadb37667ad22fadca74c39
Sandro de Souza <sandesou@gmail.com>
2022-05-23 11:06:47 -0300

    add discord button to signup page + remove unused discord-signup-page

--- pages/signup/CreateAccountForm/index.tsx
+++ pages/signup/CreateAccountForm/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCsrfToken } from 'next-auth/react';
 import Layout from '../../../components/layout/CardLayout';
 import TextField from '../../../components/TextField';
 import ColorField from '../../../components/ColorField';
 import styles from './index.module.css';
 import { stripProtocol } from '../../../utilities/url';
+import DiscordIcon from '@/components/icons/DiscordIcon';
+import SlackIcon from '@/components/icons/SlackIcon';
 
-const REDIRECT_URI =
+const REDIRECT_URI_SLACK =
   process.env.NEXT_PUBLIC_REDIRECT_URI || 'https://linen.dev/api/oauth';
-
 const SLACK_CLIENT_ID =
   process.env.NEXT_PUBLIC_SLACK_CLIENT_ID || '1250901093238.3006399856353';
+
+const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID as string;
+const REDIRECT_URI_DISCORD = encodeURI(
+  process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI as string
+);
 
 interface Props {
   authId: string;
@@ -40,7 +46,35 @@ async function signIn({
   });
 }
 
+function integrationAuthorizer(community: string, accountId: string) {
+  switch (community) {
+    case 'discord':
+      window.location.href =
+        `https://discord.com/api/oauth2/authorize` +
+        `?client_id=${DISCORD_CLIENT_ID}` +
+        `&permissions=17179878400` +
+        `&redirect_uri=${REDIRECT_URI_DISCORD}` +
+        `&response_type=code` +
+        `&scope=guilds.members.read%20guilds%20bot` +
+        `&state=${accountId}`;
+      break;
+    case 'slack':
+      window.location.href =
+        'https://slack.com/oauth/v2/authorize' +
+        `?client_id=${SLACK_CLIENT_ID}` +
+        '&scope=channels:history,channels:join,channels:read,incoming-webhook,reactions:read,users:read,team:read' +
+        '&user_scope=channels:history,search:read' +
+        `&state=${accountId}` +
+        `&redirect_uri=${REDIRECT_URI_SLACK}`;
+      break;
+    default:
+      break;
+  }
+}
+
 export default function CreateAccountForm({ authId, email, password }: Props) {
+  const [community, setCommunity] = useState<string>();
+
   const onSubmit = async (event: any) => {
     event.preventDefault();
     const form = event.target;
@@ -58,7 +92,6 @@ export default function CreateAccountForm({ authId, email, password }: Props) {
         brandColor,
       }),
     });
-
     const account = await response.json();
     const response2 = await fetch('/api/auth', {
       method: 'PUT',
@@ -70,15 +103,7 @@ export default function CreateAccountForm({ authId, email, password }: Props) {
     });
     await response2.json();
     await signIn({ email, password });
-    window.location.href =
-      'https://slack.com/oauth/v2/' +
-      `authorize?client_id=${SLACK_CLIENT_ID}&` +
-      '&scope=channels:history,channels:join,channels:read,incoming-webhook,reactions:read,users:read,team:read' +
-      '&user_scope=channels:history,search:read' +
-      '&state=' +
-      account.id +
-      '&redirect_uri=' +
-      REDIRECT_URI;
+    community && integrationAuthorizer(community, account.id);
   };
 
   return (
@@ -108,14 +133,25 @@ export default function CreateAccountForm({ authId, email, password }: Props) {
           defaultValue="#1B194E"
           required
         />
-        <button className={styles.link} type="submit">
-          <img
-            alt="Add to Slack"
-            height="40"
-            width="139"
-            src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-          />
+        <button
+          className={styles.communityButton}
+          type="submit"
+          onClick={() => setCommunity('discord')}
+        >
+          <DiscordIcon size="20" style={{ margin: '3px 8px 3px 1px' }} />
+          <p>
+            Add to <b>Discord</b>
+          </p>
+        </button>
+        <button
+          className={styles.communityButton}
+          type="submit"
+          onClick={() => setCommunity('slack')}
+        >
+          <SlackIcon size="20" style={{ margin: '3px 8px 3px 1px' }} />
+          <p>
+            Add to <b>Slack</b>
+          </p>
         </button>
       </form>
     </Layout>
