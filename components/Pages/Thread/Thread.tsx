f64a2f460a30045a2c46d647527260b5494bb57e
Kam Leung <kamleung911@gmail.com>
2022-05-23 18:08:52 -0400

    render non threaded messages (#130)

--- components/Pages/Thread/Thread.tsx
+++ components/Pages/Thread/Thread.tsx
@@ -94,11 +94,16 @@ export default function Thread({
             size="sm"
             target="_blank"
           >
-            <AiOutlineLink className={styles.icon} size={18} />
             {settings.communityType === 'discord' ? (
-              <div>Join thread in Discord</div>
+              <div className="flex">
+                <AiOutlineLink className={styles.icon} size={18} />
+                <div>Join thread in Discord</div>
+              </div>
             ) : (
-              <div>Join thread in Slack</div>
+              <div className="flex">
+                <AiOutlineLink className={styles.icon} size={18} />
+                <div>Join thread in Slack</div>
+              </div>
             )}
           </Anchor>
           <div className={styles.count}>
