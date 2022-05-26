662f9ad965a5fb77686959c14a08fc7259256728
Emil Ajdyna <emil@ajdyna.com>
2022-05-26 17:24:31 +0200

    Ensure that messages without an author are not merged together

--- utilities/messages.test.ts
+++ utilities/messages.test.ts
@@ -48,4 +48,31 @@ describe('#mergeMessagesByUserId', () => {
     expect(result.length).toEqual(3);
     expect(result[0].body).toEqual('foo\nbar');
   });
+
+  it('does not merge messages without an author', () => {
+    const messages = [
+      {
+        id: '1',
+        channelId: '1',
+        slackMessageId: '1',
+        slackThreadId: '1',
+        createdAt: new Date(),
+        sentAt: new Date(),
+        usersId: null,
+        body: 'foo',
+      },
+      {
+        id: '2',
+        channelId: '1',
+        slackMessageId: '2',
+        slackThreadId: '1',
+        createdAt: new Date(),
+        sentAt: new Date(),
+        usersId: null,
+        body: 'bar',
+      },
+    ];
+    const result = mergeMessagesByUserId(messages);
+    expect(result.length).toEqual(2);
+  });
 });
