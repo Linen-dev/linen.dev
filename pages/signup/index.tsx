5dc4ece248d637e7a3a431069823bd91a7bc058a
Sandro de Souza <sandesou@gmail.com>
2022-05-27 18:14:07 -0300

    Remove onboarding from signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,40 +1,55 @@
-import { useState } from 'react';
-import CreateAccountForm from './CreateAccountForm';
-import CreateAuthForm from './CreateAuthForm';
+import Layout from 'components/layout/CardLayout';
+import EmailField from 'components/EmailField';
+import PasswordField from 'components/PasswordField';
+import Button from 'components/Button';
+import Link from 'components/Link';
+import { useRouter } from 'next/router';
+import { toast } from 'components/Toast';
 
-enum Step {
-  Auth,
-  Account,
-}
+export default function SignUp() {
+  const router = useRouter();
 
-function SignUp() {
-  const [step, setStep] = useState(Step.Auth);
-  const [authId, setAuthId] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  if (step === Step.Auth) {
-    return (
-      <CreateAuthForm
-        onSuccess={({ authId, email, password }) => {
-          setAuthId(authId);
-          setEmail(email);
-          setPassword(password);
-          setStep(Step.Account);
-        }}
-      />
-    );
-  }
-  if (step === Step.Account) {
-    return (
-      <CreateAccountForm authId={authId} email={email} password={password} />
-    );
-  }
-}
-
-export const getServerSideProps = async () => {
-  return {
-    props: {},
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    if (!email) {
+      return toast.error('Email is required');
+    }
+    if (!password) {
+      return toast.error('Password is required');
+    }
+    fetch('/api/auth', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+    })
+      .then(async (res) => {
+        const { error, message } = await res.json();
+        if (res.ok) {
+          router.push('/signin');
+          return toast.success(message);
+        } else {
+          return toast.error(error);
+        }
+      })
+      .catch(() => {
+        return toast.error('Something went wrong. Please try again.');
+      });
   };
-};
 
-export default SignUp;
+  return (
+    <Layout header="Sign Up">
+      <form onSubmit={onSubmit}>
+        <EmailField label="Email" id="email" required />
+        <PasswordField label="Password" id="password" required />
+        <Button type="submit" block>
+          Submit
+        </Button>
+      </form>
+      <p className="text-sm pt-3">
+        Already have an account? <Link href="/signin">Sign in!</Link>
+      </p>
+    </Layout>
+  );
+}
