6700e8c675257bfd39f30ff9233647dfa8210f84
cheeseblubber <kamleung911@gmail.com>
2022-06-01 14:46:10 -0400

    show communities with atleast 2 threads

--- services/threads.ts
+++ services/threads.ts
@@ -66,7 +66,7 @@ export async function getThreadById(
         return r.channelId === c.id;
       });
 
-      return channelCount && channelCount._count.id > 20;
+      return channelCount && channelCount._count.id > 2;
     });
 
   if (!account) {
