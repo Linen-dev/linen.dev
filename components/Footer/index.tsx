8285159c46edeea6f73c7cbda170cfe19291ace8
cheeseblubber <kamleung911@gmail.com>
2022-06-02 16:20:06 -0400

    Add footer file

---
+++ components/Footer/index.tsx
@@ -0,0 +1,35 @@
+const navigation = {
+  main: [
+    { name: 'Privacy', href: '/legal/privacy' },
+    { name: 'Terms', href: '/legal/terms' },
+    { name: 'Support', href: 'mailto:help@linen.dev' },
+  ],
+};
+
+export default function Footer() {
+  return (
+    <footer className="bg-white">
+      <div className="max-w-7xl mx-auto pt-16 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <nav
+          className="-mx-5 -my-2 flex flex-wrap justify-center"
+          aria-label="Footer"
+        >
+          {navigation.main.map((item) => (
+            <div key={item.name} className="px-5 py-2">
+              <a
+                href={item.href}
+                className="text-base text-gray-500 hover:text-gray-900"
+                target="_blank"
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <p className="mt-8 text-center text-base text-gray-400">
+          &copy; 2022 Linen. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
