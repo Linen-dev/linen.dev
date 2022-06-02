798e2089d04b32131f6b82ffd138894afd22dde6
Emil Ajdyna <emil@ajdyna.com>
2022-06-02 22:43:15 +0200

    Show/hide image

--- components/Message/Link/index.tsx
+++ components/Message/Link/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import styles from './index.module.css';
 import { isImage, isUrlValid } from './utilities';
@@ -10,14 +10,25 @@ interface Props {
 
 export default function Link({ value }: Props) {
   const [href, name] = value.split('|');
+  const [visible, setVisible] = useState(true);
 
   if (isImage(href)) {
     return (
       <div>
-        <p className={styles.text}>{name || href}</p>
-        <a href={href} target="_blank">
-          <Image src={href} />
-        </a>
+        <p className={styles.text}>
+          {name || href}
+          <span
+            className={styles.arrow}
+            onClick={() => setVisible((visible) => !visible)}
+          >
+            {visible ? '▾' : '▸'}
+          </span>
+        </p>
+        {visible && (
+          <a href={href} target="_blank">
+            <Image src={href} />
+          </a>
+        )}
       </div>
     );
   }
