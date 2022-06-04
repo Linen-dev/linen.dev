b4f79071c65549c28a71ce9e6f07ddf7b6da851d
Emil Ajdyna <emil@ajdyna.com>
2022-06-04 14:12:32 +0200

    Add a utility method for converting the youtube url

--- components/Message/Link/utilities.ts
+++ components/Message/Link/utilities.ts
@@ -8,13 +8,37 @@ export function isImage(href: string): boolean {
   return SUPPORTED_EXTENSIONS.includes(extension.toLowerCase());
 }
 
-export function isVideo(href: string): boolean {
+function isYoutubeEmbedUrl(href: string) {
   return (
     href.startsWith('https://www.youtube.com/embed/') ||
     href.startsWith('https://youtube.com/embed/')
   );
 }
 
+function isYoutubeWatchUrl(href: string): boolean {
+  return (
+    href.startsWith('https://www.youtube.com/watch') ||
+    href.startsWith('https://youtube.com/watch')
+  );
+}
+
+function isYoutubeUrl(href: string): boolean {
+  return isYoutubeEmbedUrl(href) || isYoutubeWatchUrl(href);
+}
+
+export function isVideo(href: string): boolean {
+  return isYoutubeUrl(href);
+}
+
 export function isUrlValid(url: string): boolean {
   return !url.startsWith('http://-') && !url.startsWith('https://-');
+}
+
+export function normalizeVideoUrl(url: string): string {
+  if (isYoutubeWatchUrl(url)) {
+    const params = new URLSearchParams(url.split('?')[1]);
+    const version = params.get('v');
+    return `https://www.youtube.com/embed/${version}`;
+  }
+  return url;
 }
