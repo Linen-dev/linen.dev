aaa7cde2291d1dfab5be87919324f56a860228cc
Sandro de Souza <sandesou@gmail.com>
2022-06-08 10:26:23 -0300

    fix styling on channel view

--- components/Pages/Channels/Channel.tsx
+++ components/Pages/Channels/Channel.tsx
@@ -258,7 +258,7 @@ export default function Channel({
       isSubDomainRouting={isSubDomainRouting}
     >
       <div className="sm:pt-6">
-        <table className="hidden sm:block sm:table-fixed ">
+        <table className="hidden md:block md:table-fixed ">
           <thead>
             <tr>
               <th className="px-6 py-3 text-left font-medium text-gray-500">
