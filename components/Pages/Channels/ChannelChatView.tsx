84d0db210760ffccf268d0109167f97ab6373519
Sandro de Souza <sandesou@gmail.com>
2022-06-10 10:28:05 -0300

    Add infinite scroll + reuse message component

--- components/Pages/Channels/ChannelChatView.tsx
+++ components/Pages/Channels/ChannelChatView.tsx
@@ -1,59 +1,15 @@
-import Avatar, { Size } from '../../Avatar';
-import { useEffect, useState } from 'react';
-import { format } from 'timeago.js';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PageLayout from '../../layout/PageLayout';
-import Message from '../../Message';
 import { capitalize } from '../../../lib/util';
 import { Props, messageWithAuthor } from '.';
-import { Anchor, Loader, Text } from '@mantine/core';
+import { Anchor, Loader } from '@mantine/core';
 import { AiOutlineLink } from 'react-icons/ai';
-import styles from './ChannelChatView.module.css';
 import { channels } from '@prisma/client';
 import { Settings } from 'services/communities';
-import useSWRImmutable from 'swr/immutable';
-
-function sortMessagesFromTopDown(a: messageWithAuthor, b: messageWithAuthor) {
-  return new Date(a.sentAt).getTime() - new Date(b.sentAt).getTime();
-}
-
-function Messages({ messages }: { messages?: messageWithAuthor[] }) {
-  return (
-    <div>
-      <ul role="list" className="divide-y divide-gray-200">
-        {messages?.sort(sortMessagesFromTopDown).map((message) => (
-          <li className="pb-8" key={message.id}>
-            <div className="flex justify-between">
-              <div className="flex pb-4">
-                <Avatar
-                  size={Size.lg}
-                  alt={message.author?.displayName || 'avatar'}
-                  src={message.author?.profileImageUrl}
-                  text={(message.author?.displayName || '?')
-                    .slice(0, 1)
-                    .toLowerCase()}
-                />
-                <div className="pl-3">
-                  <p className="font-semibold text-sm inline-block">
-                    {message.author?.displayName || 'user'}
-                  </p>
-                </div>
-              </div>
-              <Text size="sm" color="gray">
-                {format(new Date(message.sentAt))}
-              </Text>
-            </div>
-            <div style={{ maxWidth: '700px' }}>
-              <Message
-                text={message.body}
-                mentions={message.mentions?.map((m) => m.users)}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+import { useRouter } from 'next/router';
+import classNames from 'classnames';
+import { MessageRow } from '../../Message/MessageRow';
 
 export default function ChannelChatView({
   channelId,
@@ -70,40 +26,82 @@ export default function ChannelChatView({
   isSubDomainRouting,
 }: Props) {
   const [currentThreads, setCurrentThreads] = useState(messages);
-  const [pageCount, setPageCount] = useState(pagination?.pageCount);
   const [currentPage, setCurrentPage] = useState(page);
+  const [loading, setLoading] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+  const [error, setError] = useState<Error>();
+  const router = useRouter();
+  const scrollableRootRef = useRef<HTMLDivElement | null>(null);
+  const lastScrollDistanceToBottomRef = useRef<number>();
+
+  const [infiniteRef, { rootRef }] = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore: loadMore,
+    disabled: !!error,
+    rootMargin: '0px 0px 0px 0px',
+  });
+
+  async function loadMore() {
+    setLoading(true);
+    try {
+      const data = await fetcher(
+        `/api/messages?channelId=${channelId}&page=${currentPage + 1}`
+      );
+      setHasNextPage(currentPage + 1 <= (pagination?.pageCount || 0));
+      setCurrentPage(currentPage + 1);
+      setCurrentThreads((currentThreads) => [
+        ...(currentThreads ? currentThreads : []),
+        ...data,
+      ]);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     setCurrentThreads(messages);
-    setPageCount(pagination?.pageCount);
-    window.scrollTo(0, 0);
+    setHasNextPage(page + 1 <= (pagination?.pageCount || 0));
+    window.scrollTo(0, document.body.scrollHeight);
     setCurrentPage(page);
-  }, [messages, pagination, page]);
+    scrollToBottom('rootRefSetter');
+  }, [router.query]);
 
-  if (!messages) {
+  // We keep the scroll position when new items are added
+  useEffect(() => {
+    const scrollableRoot = scrollableRootRef.current;
+    const lastScrollDistanceToBottom =
+      lastScrollDistanceToBottomRef.current ?? 0;
+    if (scrollableRoot) {
+      scrollableRoot.scrollTop =
+        scrollableRoot.scrollHeight - lastScrollDistanceToBottom;
+    }
+  }, [currentThreads, rootRef]);
+
+  const rootRefSetter = useCallback(
+    (node: HTMLDivElement) => {
+      rootRef(node);
+      scrollableRootRef.current = node;
+    },
+    [rootRef]
+  );
+
+  const handleRootScroll = React.useCallback(() => {
+    const rootNode = scrollableRootRef.current;
+    if (rootNode) {
+      const scrollDistanceToBottom = rootNode.scrollHeight - rootNode.scrollTop;
+      lastScrollDistanceToBottomRef.current = scrollDistanceToBottom;
+    }
+  }, []);
+
+  if (!messages || !currentThreads) {
     return <div></div>;
   }
 
-  // Todo: handle missing channels
-  const channelName = channels?.find((c) => c.id === channelId)?.channelName;
-
-  function buildTitle(
-    communityName: string,
-    channelName: string | undefined,
-    page: number = 1
-  ) {
-    const name = capitalize(communityName);
-    const channel = !!channelName
-      ? ` - ${capitalize(channelName)} Threads - Page ${page}`
-      : '';
-    return `${name}${channel}`;
-  }
-
-  const pages: any[] = [];
-  for (let i = page; i < currentPage; i++) {
-    pages.unshift(
-      <PageWrapper index={i} key={i} channelId={currentChannel.id} />
-    );
+  function onlyMessagesFromCurrentChannel(message: messageWithAuthor) {
+    return message.channelId === currentChannel.id;
   }
 
   return (
@@ -117,8 +115,8 @@ export default function ChannelChatView({
       seo={{
         title: buildTitle(
           settings.name || communityName,
-          channelName,
-          currentPage
+          currentChannel.channelName,
+          page
         ),
         // description: `${channelName} Threads - Page ${page}`,
       }}
@@ -127,33 +125,54 @@ export default function ChannelChatView({
       communityName={communityName}
       isSubDomainRouting={isSubDomainRouting}
     >
-      <div className="py-8 px-4">
-        {!!(pageCount && currentPage <= pageCount) && (
-          <LoadMore onClick={() => setCurrentPage(currentPage + 1)} />
+      <div
+        className={classNames(
+          'py-4 px-4 grid grow place-items-center overflow-auto',
+          'lg:h-[calc(100vh_-_80px)] lg:w-[calc(100vw-250px)]',
+          'sm:h-[calc(100vh_-_144px)]',
+          'h-[calc(100vh_-_192px)] w-full'
         )}
-        {pages}
-        <Messages messages={currentThreads} />
-
-        <div className="gap-8 columns-2">
-          <div className={styles.buttons}>
-            <Anchor
-              className={styles.join}
-              href={buildInviteLink(
-                { ...settings, slackInviteUrl },
-                currentChannel
-              )}
-              size="sm"
-              target="_blank"
-            >
-              <div className="flex content-center">
-                <AiOutlineLink className={styles.icon} size={18} />
-                {settings.communityType === 'discord' ? (
-                  <div>Join channel in Discord</div>
-                ) : (
-                  <div>Join channel in Slack</div>
-                )}
+        ref={rootRefSetter}
+        onScroll={handleRootScroll}
+        id="rootRefSetter"
+      >
+        <div className="w-full md:w-[700px] self-end">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 flex flex-col-reverse"
+          >
+            {currentThreads
+              .filter(onlyMessagesFromCurrentChannel)
+              .map((_, index: any) => (
+                <MessageRow key={index} message={_} />
+              ))}
+            {hasNextPage && (
+              <div ref={infiniteRef} className="flex justify-center p-4">
+                <Loader size="sm" />
               </div>
-            </Anchor>
+            )}
+          </ul>
+          <div className="gap-8 columns-2">
+            <div className="min-h-[20px] clear-both">
+              <Anchor
+                className="float-left ml-[4px]"
+                href={buildInviteLink(
+                  { ...settings, slackInviteUrl },
+                  currentChannel
+                )}
+                size="sm"
+                target="_blank"
+              >
+                <div className="flex content-center">
+                  <AiOutlineLink className="inline-block" size={18} />
+                  {settings.communityType === 'discord' ? (
+                    <div>Join channel in Discord</div>
+                  ) : (
+                    <div>Join channel in Slack</div>
+                  )}
+                </div>
+              </Anchor>
+            </div>
           </div>
         </div>
       </div>
@@ -173,35 +192,19 @@ function buildInviteLink(
   }
 }
 
+function buildTitle(
+  communityName: string,
+  channelName: string | undefined,
+  page: number = 1
+) {
+  const name = capitalize(communityName);
+  const channel = !!channelName ? ` - ${capitalize(channelName)} Messages` : '';
+  return `${name}${channel}`;
+}
+
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-function PageWrapper({
-  index,
-  channelId,
-}: {
-  index: number;
-  channelId: string;
-}) {
-  const { data, error } = useSWRImmutable(
-    `/api/messages?channelId=${channelId}&page=${index + 1}`,
-    fetcher
-  );
-  if (error) return <></>;
-  return data ? (
-    <Messages messages={data} />
-  ) : (
-    <div className="flex justify-center p-4">
-      <Loader size="sm" />
-    </div>
-  );
-}
-
-function LoadMore({ onClick }: { onClick: () => void }) {
-  return (
-    <div className="flex justify-end pb-4">
-      <Anchor onClick={onClick} size="sm">
-        Load More
-      </Anchor>
-    </div>
-  );
-}
+const scrollToBottom = (id: string) => {
+  const element = document.getElementById(id);
+  if (element) element.scrollTop = element.scrollHeight;
+};
