d753eb6a57093177acafe5a0f7935cf9ff844e8e
Sandro de Souza <sandesou@gmail.com>
2022-06-13 16:46:08 -0300

    rollback robots.txt disallow _next folder

--- utilities/robots.ts
+++ utilities/robots.ts
@@ -1,15 +1,13 @@
 export function createRobotsTxt(domain: string) {
   return `
 User-agent: *
-
-Disallow: /*.json$
-Disallow: /*_buildManifest.js$
-Disallow: /*_middlewareManifest.js$
-Disallow: /*_ssgManifest.js$
-Disallow: /*.js$
-
 Allow: /
-
 Sitemap: https://${domain}/sitemap.xml
-  `.trim();
+`.trim();
 }
+
+// Disallow: /*.json$
+// Disallow: /*_buildManifest.js$
+// Disallow: /*_middlewareManifest.js$
+// Disallow: /*_ssgManifest.js$
+// Disallow: /*.js$
