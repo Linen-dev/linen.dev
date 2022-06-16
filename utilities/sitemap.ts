b72599105fa5642310ba66c811503949fcf2b4ed
Sandro de Souza <sandesou@gmail.com>
2022-06-15 23:58:03 -0300

    add channels to sitemap + hide hidden channels

--- utilities/sitemap.ts
+++ utilities/sitemap.ts
@@ -21,22 +21,7 @@ export async function internalCreateXMLSitemapForSubdomain(
       redirectDomain: host,
     },
     select: {
-      channels: {
-        select: {
-          slackThreads: {
-            select: {
-              incrementId: true,
-              slug: true,
-              messages: {
-                select: {
-                  body: true,
-                  author: true,
-                },
-              },
-            },
-          },
-        },
-      },
+      id: true,
     },
   });
 
@@ -44,21 +29,41 @@ export async function internalCreateXMLSitemapForSubdomain(
     return Promise.reject();
   }
 
-  const threads = account.channels.reduce((array: any[], item) => {
-    return [
-      ...array,
-      ...item.slackThreads
-        .filter((t) => {
-          return t.messages.length > 1;
-        })
-        .filter((t) => {
-          return t.messages[0].author?.isBot === false;
-        })
-        .map(({ incrementId, slug }) => ({
-          incrementId,
-          slug: slug?.toLowerCase(),
-        })),
-    ];
+  const accountChannels = await prisma.channels.findMany({
+    where: {
+      account: { id: account.id },
+      hidden: false,
+    },
+    select: {
+      channelName: true,
+      slackThreads: {
+        select: {
+          incrementId: true,
+          slug: true,
+          messages: {
+            select: {
+              author: true,
+            },
+          },
+        },
+        orderBy: { incrementId: 'asc' },
+      },
+    },
+  });
+
+  const channels: Record<string, number> = {};
+
+  const threads = accountChannels.reduce((array: any[], item) => {
+    const channelThreads = item.slackThreads
+      .filter((t) => {
+        return t.messages.length > 1;
+      })
+      .filter((t) => {
+        return t.messages[0].author?.isBot === false;
+      });
+    channelThreads.length &&
+      (channels[item.channelName] = channelThreads.length);
+    return [...array, ...channelThreads];
   }, []);
 
   const stream = new SitemapStream({
@@ -66,12 +71,26 @@ export async function internalCreateXMLSitemapForSubdomain(
   });
 
   const urls = threads.map(({ incrementId, slug }) => {
-    return `/t/${incrementId}/${slug || 'topic'}`;
+    return `/t/${incrementId}/${slug.toLowerCase() || 'topic'}`;
   });
 
-  return streamToPromise(Readable.from(urls).pipe(stream)).then((data) =>
-    data.toString()
-  );
+  const channelsPages: string[] = getChannelPages(channels);
+  if (!channelsPages.length || !urls.length) return '';
+
+  return streamToPromise(
+    Readable.from([...channelsPages, ...urls]).pipe(stream)
+  ).then((data) => data.toString());
+}
+
+function getChannelPages(channels: Record<string, number>, prefix = '') {
+  const channelsPages: string[] = [];
+  for (const [channel, threads] of Object.entries(channels)) {
+    const pages = Math.floor(threads / 10) + (threads % 10 > 0 ? 1 : 0);
+    for (let i = 0; i < pages; i++) {
+      channelsPages.push(`${prefix}/c/${channel}/${i + 1}`);
+    }
+  }
+  return channelsPages;
 }
 
 export async function createXMLSitemapForLinen(host: string) {
@@ -139,6 +158,7 @@ export async function internalCreateXMLSitemapForFreeCommunity(
       slug: true,
       channel: {
         select: {
+          channelName: true,
           account: {
             select: {
               discordDomain: true,
@@ -152,6 +172,7 @@ export async function internalCreateXMLSitemapForFreeCommunity(
     where: {
       messages: { some: { id: { not: undefined } } },
       channel: {
+        hidden: false,
         account: {
           OR: [
             { discordDomain: community },
@@ -165,16 +186,29 @@ export async function internalCreateXMLSitemapForFreeCommunity(
       incrementId: 'asc',
     },
   });
+  const channels: Record<string, number> = {};
+
   const protocol = host.includes('localhost') ? 'http' : PROTOCOL;
   const stream = new SitemapStream({
     hostname: `${protocol}://${host}`,
   });
   if (!threads || !threads.length) return '';
   const prefix = threads[0].channel.account?.discordServerId ? 'd' : 's';
-  const urls = threads?.map(({ incrementId, slug }) => {
+  const urls = threads?.map(({ incrementId, slug, channel }) => {
+    channels[channel.channelName]
+      ? channels[channel.channelName]++
+      : (channels[channel.channelName] = 1);
     return `/${prefix}/${community}/t/${incrementId}/${
       slug || 'topic'
     }`.toLowerCase();
   });
-  return streamToPromise(Readable.from(urls).pipe(stream)).then(String);
+  const channelsPages: string[] = getChannelPages(
+    channels,
+    `/${prefix}/${community}`
+  );
+  if (!channelsPages.length || !urls.length) return '';
+
+  return streamToPromise(
+    Readable.from([...channelsPages, ...urls]).pipe(stream)
+  ).then(String);
 }
