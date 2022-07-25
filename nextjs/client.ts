5c41afe6063c370ea93464f0e17934a7c182f5de
Sandro de Souza <sandesou@gmail.com>
2022-07-25 16:14:05 -0300

    enforce ssl on db connection

--- nextjs/client.ts
+++ nextjs/client.ts
@@ -2,28 +2,36 @@ import { PrismaClient } from '@prisma/client';
 import { tmpdir } from 'os';
 import fs from 'fs';
 
+const rds_ca_2019_root = process.env.RDS_CERTIFICATE as string;
+
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-if (!global.prisma && process.env.RDS_CERTIFICATE) {
-  const certOut = `${tmpdir()}/rds-ca-2019-root.pem`;
-  const cert = process.env.RDS_CERTIFICATE;
+const certOut = `${tmpdir()}/rds-ca-2019-root.pem`;
+
+if (!global.prisma) {
   const certExists = fs.existsSync(certOut);
   if (!certExists) {
     console.log(`Writing certificate to ${certOut}`);
-    fs.writeFile(certOut, cert, (err) => {
-      if (err) return console.log(err);
-    });
+    fs.writeFileSync(certOut, rds_ca_2019_root);
   }
 }
+
+const db_url_join =
+  (process.env.DATABASE_URL as string).indexOf('?') > -1 ? '&' : '?';
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
     log: ['warn', 'error'],
+    datasources: {
+      db: {
+        url: `${process.env.DATABASE_URL}${db_url_join}sslmode=require&sslcert=${certOut}`,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
