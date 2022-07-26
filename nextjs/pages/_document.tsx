2802802a0f1d983fef04488742f73b12cf046dce
Emil Ajdyna <emil@ajdyna.com>
2022-07-26 15:40:04 +0200

    Remove mantine

--- nextjs/pages/_document.tsx
+++ nextjs/pages/_document.tsx
@@ -5,23 +5,9 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import { ServerStyles, createStylesServer } from '@mantine/next';
-
-const stylesServer = createStylesServer();
-
 export default class _Document extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <ServerStyles html={initialProps.html} server={stylesServer} />
-        </>
-      ),
-    };
+  static async getInitialProps(context: DocumentContext) {
+    return Document.getInitialProps(context);
   }
   render() {
     return (
