f9072e1921d91bc2d7047371eb3e58675dc41222
Emil Ajdyna <emil@ajdyna.com>
2022-08-08 13:14:25 +0200

    Refactor html to a separate fn

--- nextjs/mailers/ResetPasswordMailer.ts
+++ nextjs/mailers/ResetPasswordMailer.ts
@@ -6,13 +6,23 @@ interface Options {
   token: string;
 }
 
+function html({ host, token }: { host: string; token: string }) {
+  return `
+    <html>
+      <body>
+        Reset your password here: <a href='${host}/reset-password?token=${token}'>${host}/reset-password?token=${token}</a>
+      </body>
+    </html>
+  `;
+}
+
 export default class ResetPasswordMailer {
   static async send({ host, to, token }: Options) {
     return ApplicationMailer.send({
       to,
       subject: 'Linen.dev - Reset your password',
       text: `Reset your password here: ${host}/reset-password?token=${token}`,
-      html: `Reset your password here: <a href='${host}/reset-password?token=${token}'>${host}/reset-password?token=${token}</a>`,
+      html: html({ host, token }),
     });
   }
 }
