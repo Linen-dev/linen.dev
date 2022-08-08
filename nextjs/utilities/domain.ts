17a29a52f58beda480cb0ce870728166d16ba6f0
Emil Ajdyna <emil@ajdyna.com>
2022-08-08 11:52:15 +0200

    Remove unused code

--- nextjs/utilities/domain.ts
+++ nextjs/utilities/domain.ts
@@ -1,12 +1,4 @@
-export function getDomain(hostname: string): string {
-  return hostname.split('.').slice(-1)[0];
-}
-
-export function getSubdomain(hostname: string): string | null {
-  return hostname.includes('.') ? hostname.split('.')[0] : null;
-}
-
-const linenHostname = [
+const LINEN_HOSTNAMES = [
   'localhost',
   'localhost:3000',
   'linen.dev',
@@ -16,15 +8,7 @@ const linenHostname = [
 
 export function isLinenDomain(host?: string) {
   if (!host) return true;
-  return !!linenHostname.find(
+  return !!LINEN_HOSTNAMES.find(
     (linenHost) => linenHost === host || host.endsWith(linenHost)
   );
-}
-
-export function cleanUpDomain(url: string) {
-  try {
-    return new URL(url).host;
-  } catch (error) {
-    return url;
-  }
 }
