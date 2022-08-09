22e3f8a7efbfedfe318e170a3479e60371fae953
Emil Ajdyna <emil@ajdyna.com>
2022-08-09 13:40:53 +0200

    Basics of a better email template

--- nextjs/mailers/views/layouts/default.ts
+++ nextjs/mailers/views/layouts/default.ts
@@ -1,3 +1,5 @@
+import div from '../components/div';
+
 export default function layout(children: string) {
   return `
       <html lang="en">
@@ -6,7 +8,23 @@ export default function layout(children: string) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </head>
       <body>
-        ${children.trim()}
+        ${div(
+          {
+            background: '#f9fafb',
+            width: '100%',
+            height: '100%',
+            spacing: '48px',
+          },
+          div(
+            {
+              background: '#ffffff',
+              padding: '32px',
+              width: '480px',
+              textAlign: 'center',
+            },
+            children.trim()
+          )
+        )}
       </body>
     </html>
   `.trim();
