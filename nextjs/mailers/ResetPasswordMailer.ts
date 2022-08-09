f6124a81a84ebf31a050216080f102b6a6e76b65
Emil Ajdyna <emil@ajdyna.com>
2022-08-09 13:22:35 +0200

    Extract the view to a separate fn and add tests

--- nextjs/mailers/ResetPasswordMailer.ts
+++ nextjs/mailers/ResetPasswordMailer.ts
@@ -1,29 +1,10 @@
 import ApplicationMailer from './ApplicationMailer';
+import view from './views/emails/reset-password';
 
 interface Options {
   host: string;
   to: string;
   token: string;
-}
-
-function html(children: string) {
-  return `
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body>
-        ${children.trim()}
-      </body>
-    </html>
-  `.trim();
-}
-
-function template({ host, token }: { host: string; token: string }) {
-  return html(`
-    Reset your password here: <a href='${host}/reset-password?token=${token}'>${host}/reset-password?token=${token}</a>
-  `);
 }
 
 export default class ResetPasswordMailer {
@@ -32,7 +13,7 @@ export default class ResetPasswordMailer {
       to,
       subject: 'Linen.dev - Reset your password',
       text: `Reset your password here: ${host}/reset-password?token=${token}`,
-      html: template({ host, token }),
+      html: view({ host, token }),
     });
   }
 }
---
+++ nextjs/mailers/views/emails/reset-password/index.spec.ts
@@ -0,0 +1,10 @@
+import view from '.';
+
+describe('reset password email', () => {
+  it('renders a link to the reset password page', () => {
+    const html = view({ host: 'http://localhost:3000', token: 'abc123' });
+    expect(html).toContain(
+      "<a href='http://localhost:3000/reset-password?token=abc123'>http://localhost:3000/reset-password?token=abc123</a>"
+    );
+  });
+});
