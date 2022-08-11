ac73709cac34ae03a8cda53a6b5de21b1ccedb3b
Sandro de Souza <sandesou@gmail.com>
2022-08-11 12:30:36 -0300

    these changes are breaking local development

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -17,6 +17,8 @@ const nextConfig = {
     domains: [
       'avatars.slack-edge.com',
       'cdn.discordapp.com',
+      `linen-assets.s3.amazonaws.com`,
+      'linen-assets.s3.us-east-1.amazonaws.com',
       `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
       `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
     ],
