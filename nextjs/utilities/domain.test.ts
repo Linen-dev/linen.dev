c39bb7a8d0a6fd7c9b254c88b683d0b34a11ccd2
Sandro de Souza <sandesou@gmail.com>
2022-08-11 12:30:36 -0300

    these changes are breaking local development

--- nextjs/utilities/domain.test.ts
+++ nextjs/utilities/domain.test.ts
@@ -2,6 +2,6 @@ import { getCurrentUrl } from './domain';
 
 describe('#getCurrentUrl', () => {
   it('returns http://localhost:3000 in local env', () => {
-    expect(getCurrentUrl()).toBe('http://localhost:3000');
+    expect(getCurrentUrl()).toBe(process.env.NEXTAUTH_URL);
   });
 });
