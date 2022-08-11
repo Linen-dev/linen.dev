7db3c1ddacd2854cf85b4c61bdf1d2dfdbf6b913
Sandro de Souza <sandesou@gmail.com>
2022-08-11 10:22:36 -0300

    update data model + auth prisma adapter

--- nextjs/lib/auth.ts
+++ nextjs/lib/auth.ts
@@ -1,5 +1,7 @@
 import prisma from '../client';
 import { generateHash, generateSalt } from '../utilities/password';
+import type { PrismaClient, Prisma, auths } from '@prisma/client';
+import type { Adapter, AdapterUser } from 'next-auth/adapters';
 
 interface CreateAuthParams {
   email: string;
@@ -23,3 +25,110 @@ export async function createAuth({
     },
   });
 }
+
+const parseAuthToAdapterUser = ({
+  email,
+  emailVerified,
+  id,
+}: auths): AdapterUser => ({ email, emailVerified, id });
+
+const parseAdapterUserToAuth = ({
+  email,
+  emailVerified,
+}: Partial<AdapterUser>): Prisma.authsCreateInput => ({
+  email: email as string,
+  emailVerified,
+  password: '', // we should deprecate this field (or make it optional)
+  salt: '', // we should deprecate this field (or make it optional)
+});
+
+// copy from https://github.com/nextauthjs/next-auth/blob/main/packages/adapter-prisma/src/index.ts
+export function CustomPrismaAdapter(p: PrismaClient): Adapter {
+  return {
+    createUser: (data) =>
+      p.auths
+        .create({ data: parseAdapterUserToAuth(data) })
+        .then(parseAuthToAdapterUser),
+
+    getUser: (id) =>
+      p.auths
+        .findUnique({ where: { id } })
+        .then((e) => e && parseAuthToAdapterUser(e)),
+
+    getUserByEmail: (email) =>
+      p.auths
+        .findUnique({ where: { email } })
+        .then((e) => e && parseAuthToAdapterUser(e)),
+
+    async updateUser({ id, ...data }) {
+      // used for update emailVerified field that we don't have
+      return await p.auths
+        .update({ where: { id }, data: { emailVerified: data.emailVerified } })
+        .then((e) => e && parseAuthToAdapterUser(e));
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const userAndSession = await p.session.findUnique({
+        where: { sessionToken },
+        include: { user: true },
+      });
+      if (!userAndSession) return null;
+      const { user, ...session } = userAndSession;
+      return { user: parseAuthToAdapterUser(user), session };
+    },
+
+    createSession: (data) => p.session.create({ data }),
+
+    updateSession: (data) =>
+      p.session.update({ where: { sessionToken: data.sessionToken }, data }),
+
+    async deleteSession(sessionToken) {
+      try {
+        return await p.session.delete({ where: { sessionToken } });
+      } catch (error) {
+        return processFailure(error);
+      }
+    },
+
+    async createVerificationToken(data) {
+      return await p.verificationToken.create({
+        data,
+      });
+    },
+
+    async useVerificationToken(identifier_token) {
+      try {
+        return await p.verificationToken.delete({
+          where: { identifier_token },
+        });
+      } catch (error) {
+        return processFailure(error);
+      }
+    },
+
+    // @ts-ignore
+    deleteUser: (id) => new Error(),
+
+    // TODO: functions that we need to implement to allow social login
+    // async getUserByAccount(provider_providerAccountId) {
+    //   const account = await p.account.findUnique({
+    //     where: { provider_providerAccountId },
+    //     select: { user: true },
+    //   })
+    //   return account?.user ?? null
+    // },
+
+    // linkAccount: (data) => p.account.create({ data }) as any,
+
+    // unlinkAccount: (provider_providerAccountId) =>
+    //   p.account.delete({ where: { provider_providerAccountId } }) as any,
+  };
+}
+
+const processFailure = (error: unknown) => {
+  // If token already used/deleted, just return null
+  // https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+  if ((error as Prisma.PrismaClientKnownRequestError).code === 'P2025')
+    return null;
+  throw error;
+};
---
+++ nextjs/prisma/migrations/20220811130908_email_verification_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "auths" ADD COLUMN     "emailVerified" TIMESTAMP(3);
