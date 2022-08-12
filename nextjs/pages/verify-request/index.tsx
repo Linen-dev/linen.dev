6db572eb1fb16151175203507d366b8b553cbb2b
cheeseblubber <kamleung911@gmail.com>
2022-08-12 10:32:43 -0400

    update copy

--- nextjs/pages/verify-request/index.tsx
+++ nextjs/pages/verify-request/index.tsx
@@ -3,7 +3,8 @@ import Layout from '../../components/layout/CardLayout';
 export default function VerifyRequest({}: {}) {
   return (
     <Layout header="Check your email">
-      A sign in link has been sent to your email address.
+      We emailed a magic link to your email address. Click the link to log in or
+      sign up
     </Layout>
   );
 }
