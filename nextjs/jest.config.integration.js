767d5651bae4fccdade208771ca2c96284bd78cd
cheeseblubber <kamleung911@gmail.com>
2022-08-15 09:50:52 -0400

    working test with creating new schema each time

--- nextjs/jest.config.integration.js
+++ nextjs/jest.config.integration.js
@@ -4,13 +4,12 @@ const createJestConfig = nextJest(nextConfig);
 
 // integration test files must have itest or ispec on the filename
 // it will use the .env.test file as default
-console.log('RUNNING INTEGRATION TESTS');
 
 const config = {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  globalSetup: '<rootDir>/jest.setup.integration.js',
-  testMatch: ['**/?(*.)+(ispec|itest).[tj]s?(x)'],
+  // collectCoverage: true,
+  // coverageDirectory: 'coverage',
+  testMatch: ['**/?(*.)+(iispec|iitest).[tj]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/mocks.ts', '<rootDir>/__mocks__/client.ts'],
 };
 
 module.exports = createJestConfig({ ...customJestConfig, ...config });
