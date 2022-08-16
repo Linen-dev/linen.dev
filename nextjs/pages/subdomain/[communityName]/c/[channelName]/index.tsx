de4df905852a3760b40edd487ddf17b151c47de1
cheeseblubber <kamleung911@gmail.com>
2022-08-16 16:00:46 -0400

    Refactor and clean up in preparation for linen 2.0
    - rename communities service to channel
    - Extract buildthreads to buildfeed

--- nextjs/pages/subdomain/[communityName]/c/[channelName]/index.tsx
+++ nextjs/pages/subdomain/[communityName]/c/[channelName]/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticPropsContext } from 'next/types';
 import Channel from '../../../../../components/Pages/Channels';
-import { channelGetStaticProps } from '../../../../../services/communities';
+import { channelGetStaticProps } from '../../../../../services/channel';
 
 export async function getStaticProps(context: GetStaticPropsContext) {
   return channelGetStaticProps(context, true);
