079363dcf3f1a776ae5362ff4a51bfbe387e43d0
Sandro de Souza <sandesou@gmail.com>
2022-08-29 13:21:32 -0300

    split channel view for bots and users

---
+++ nextjs/components/Channel/ChannelForBots.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+
+import CustomLinkHelper from 'components/Link/CustomLinkHelper';
+import ButtonPagination from 'components/ButtonPagination';
+
+import { Feed } from 'components/Feed';
+import { SerializedThread } from 'serializers/thread';
+import { ChannelViewProps } from 'components/Pages/ChannelsPage';
+
+export function ChannelForBots({
+  threads,
+  settings,
+  channelName,
+  isSubDomainRouting,
+  nextCursor,
+  isBot,
+}: ChannelViewProps) {
+  const [currentThreads, setCurrentThreads] = useState<SerializedThread[]>();
+
+  useEffect(() => {
+    setCurrentThreads(threads);
+  }, [threads]);
+
+  if (!threads) {
+    return <div />;
+  }
+
+  return (
+    <>
+      <div
+        className="
+        overflow-hidden
+        hover:overflow-auto
+        pr-4
+        hover:pr-0
+        lg:h-[calc(100vh_-_80px)]
+        md:h-[calc(100vh_-_144px)] 
+        h-[calc(100vh_-_192px)]
+        lg:w-[calc(100vw_-_250px)]
+        flex justify-center
+        w-[100vw]
+        "
+      >
+        <div className="sm:pt-6 justify-center">
+          <ul className="divide-y sm:max-w-4xl px-1">
+            <Feed
+              threads={currentThreads}
+              isSubDomainRouting={isSubDomainRouting}
+              communityName={settings.communityName}
+              communityType={settings.communityType}
+              isBot={isBot}
+              onClick={() => {}}
+            />
+            <div className="text-center p-4">
+              {nextCursor?.prev && (
+                <ButtonPagination
+                  href={CustomLinkHelper({
+                    isSubDomainRouting,
+                    communityName: settings.communityName,
+                    communityType: settings.communityType,
+                    path: `/c/${channelName}/${nextCursor.prev}`,
+                  })}
+                  label="Previous"
+                />
+              )}
+              {nextCursor?.next && (
+                <ButtonPagination
+                  href={CustomLinkHelper({
+                    isSubDomainRouting,
+                    communityName: settings.communityName,
+                    communityType: settings.communityType,
+                    path: `/c/${channelName}/${nextCursor.next}`,
+                  })}
+                  label="Next"
+                  className="ml-3"
+                />
+              )}
+            </div>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
