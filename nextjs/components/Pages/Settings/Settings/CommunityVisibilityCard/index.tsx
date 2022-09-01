975fa4ab73ae2fe79028bb7a53b082b06ca9818b
Emil Ajdyna <emil@ajdyna.com>
2022-09-01 13:32:29 +0200

    Show the current type of community in the settings page

---
+++ nextjs/components/Pages/Settings/Settings/CommunityVisibilityCard/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SerializedAccount } from 'serializers/account';
+import { AccountType } from '@prisma/client';
+import Card from '../Card';
+
+interface Props {
+  account: SerializedAccount;
+}
+
+function description(type: AccountType) {
+  switch (type) {
+    case AccountType.PUBLIC:
+      return 'Your community is currently public. It can be viewed by anyone.';
+    case AccountType.PRIVATE:
+      return 'Your community is currently private. It can be viewed by admins.';
+  }
+}
+
+export default function CommunityVisibilityCard({ account }: Props) {
+  return (
+    <Card
+      header="Community visibility"
+      description={description(account.type)}
+    />
+  );
+}
