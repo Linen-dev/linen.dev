ce6a3a52eb343ed2d7f1a3b31e50c56a8c2039c4
Emil Ajdyna <emil@ajdyna.com>
2022-09-08 10:16:52 +0200

    Remove an unnecessary file

--- nextjs/pages/s/[communityName]/inbox/index.tsx
+++ nextjs/pages/s/[communityName]/inbox/index.tsx
@@ -4,12 +4,16 @@ import { findAccountByPath } from 'lib/models';
 import { buildSettings, Settings } from 'services/accountSettings';
 import { AccountWithSlackAuthAndChannels } from 'types/partialTypes';
 import { channels } from '@prisma/client';
+import PermissionsService from 'services/permissions';
+import { Permissions } from 'types/shared';
+import { RedirectTo } from 'utilities/response';
 
 interface Props {
   communityName: string;
   isSubDomainRouting: boolean;
   settings: Settings;
   channels: channels[];
+  permissions: Permissions;
 }
 
 export default function Inbox({
@@ -17,6 +21,7 @@ export default function Inbox({
   isSubDomainRouting,
   settings,
   channels,
+  permissions,
 }: Props) {
   return (
     <PageLayout
@@ -24,6 +29,7 @@ export default function Inbox({
       isSubDomainRouting={isSubDomainRouting}
       settings={settings}
       channels={channels}
+      permissions={permissions}
     >
       Inbox
     </PageLayout>
@@ -31,6 +37,10 @@ export default function Inbox({
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const access = await PermissionsService.inbox(context);
+  if (!access) {
+    return RedirectTo('/signin');
+  }
   const communityName = context?.params?.communityName as string;
   const account = (await findAccountByPath(communityName, {
     include: { channels: { where: { hidden: false } } },
@@ -45,6 +55,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       isSubDomainRouting: false,
       settings,
       channels: account.channels,
+      permissions: {
+        inbox: access,
+      },
     },
   };
 }
