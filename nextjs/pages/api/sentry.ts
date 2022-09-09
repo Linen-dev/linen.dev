fb11da0bc96828619b2ec9616b71e9db83cdeb70
Sandro de Souza <sandesou@gmail.com>
2022-09-09 18:42:45 -0300

    clean up sentry endpoints

--- nextjs/pages/api/sentry.ts
+++
@@ -1,8 +0,0 @@
-import { withSentry } from '@sentry/nextjs';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  throw new Error('[test] api, forced failure, unhandled exception');
-}
-
-export default withSentry(handler);
