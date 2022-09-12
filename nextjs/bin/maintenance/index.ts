a935083df649f8c6d1da86b6d1fffa40028a874c
Sandro de Souza <sandesou@gmail.com>
2022-09-12 10:55:20 -0300

    report google stats on our slack

--- nextjs/bin/maintenance/index.ts
+++ nextjs/bin/maintenance/index.ts
@@ -1,8 +1,10 @@
+import { crawlGoogleResults } from 'services/crawler';
 import { anonymizeUsers } from '../../services/anonymize';
 import { slugify } from '../../services/slugify';
 import { updateMessagesCount } from './thread-message-count';
 
 (async () => {
+  await crawlGoogleResults();
   await anonymizeUsers();
   await slugify();
   await updateMessagesCount();
---
+++ nextjs/services/crawler/index.ts
@@ -0,0 +1,39 @@
+import { load } from 'cheerio';
+import axios from 'axios';
+import { findAccountsPremium } from 'lib/account';
+import { sendNotification } from 'services/slack';
+
+function getResult(text: string) {
+  let result = text.split(/\(.+\)/).join('');
+  return result.match(/\d/g)?.join('');
+}
+
+async function query(site: string) {
+  const result = await axios.get(
+    `https://www.google.com/search?q=site:${site}`,
+    {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+      },
+    }
+  );
+  const $ = load(result.data);
+  const resultStats = $('#result-stats').text();
+  return getResult(resultStats);
+}
+
+export async function crawlGoogleResults() {
+  const accounts = await findAccountsPremium();
+
+  const result = await Promise.all(
+    [...accounts, { redirectDomain: 'linen.dev' }].map(async (account) => {
+      if (account.redirectDomain) {
+        const stats = await query(account.redirectDomain);
+        return account.redirectDomain.padEnd(50, '.') + (stats || '-');
+      }
+    })
+  );
+
+  await sendNotification(result.filter(Boolean).join('\n'));
+}
