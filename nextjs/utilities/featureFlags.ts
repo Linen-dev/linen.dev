f53d6932643a6e58f7f6c46152ed9a8fff979cfc
Sandro de Souza <sandesou@gmail.com>
2022-09-14 15:26:34 -0300

    redirect users without community to new onboarding flow

--- nextjs/utilities/featureFlags.ts
+++ nextjs/utilities/featureFlags.ts
@@ -4,6 +4,7 @@ export const isStripeEnabled = false;
 export const isFeedEnabled = true;
 export const isChatEnabled = true;
 export const isCreateChannelEnabled = false;
+export const isRedirectToNewOnboardingEnabled = false;
 
 export default {
   isStripeEnabled,
