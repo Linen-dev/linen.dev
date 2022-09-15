58c32811d192618f4b8d4913f6bec9a8661eb859
cheeseblubber <kamleung911@gmail.com>
2022-09-14 22:51:21 -0400

    secure sending api

--- push_service/lib/push_service_web/channels/room_channel.ex
+++ push_service/lib/push_service_web/channels/room_channel.ex
@@ -9,9 +9,13 @@ defmodule PushServiceWeb.RoomChannel do
     {:ok, assign(socket, :channel_id, channel_id)}
   end
 
-  def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast!(socket, "new_msg", %{body: body})
-    {:noreply, socket}
+  def handle_in("new_msg", %{"body" => body, "token" => token}, socket) do
+    if(token != System.get_env("PUSH_SERVICE_KEY")) do
+      {:noreply, socket}
+    else
+      broadcast!(socket, "new_msg", %{body: body})
+      {:noreply, socket}
+    end
   end
 
   def join("room:" <> _private_room_id, _params, _socket) do
--- push_service/lib/push_service_web/controllers/message_controller.ex
+++ push_service/lib/push_service_web/controllers/message_controller.ex
@@ -2,8 +2,13 @@ defmodule PushServiceWeb.MessageController do
   use PushServiceWeb, :controller
   action_fallback PushServiceWeb.FallbackController
 
-  def create(conn, %{"channel_id" => channel_id, "body" => body}) do
-    PushServiceWeb.Endpoint.broadcast!("room:lobby:" <> channel_id, "new_msg", %{body: body})
-    json(conn, %{body: 200})
+  # use pattern matching
+  def create(conn, %{"channel_id" => channel_id, "body" => body, "token" => token}) do
+    if(token != System.get_env("PUSH_SERVICE_KEY")) do
+      json(conn, %{status: 400})
+    else
+      PushServiceWeb.Endpoint.broadcast!("room:lobby:" <> channel_id, "new_msg", %{body: body})
+      json(conn, %{status: 200})
+    end
   end
 end
