bd2f14d89c959c34715cd82f5158398497f67e32
Emil Ajdyna <emil@ajdyna.com>
2022-09-16 18:05:38 +0200

    Add a html/css version of Linen logo

---
+++ nextjs/components/Logo/Linen/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './index.module.css';
+
+export default function LinenLogo() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <div
+          className={classNames(
+            styles['line-vertical'],
+            styles['line-vertical-one']
+          )}
+        ></div>
+        <div
+          className={classNames(
+            styles['line-vertical'],
+            styles['line-vertical-two']
+          )}
+        ></div>
+        <div
+          className={classNames(
+            styles['line-vertical'],
+            styles['line-vertical-three']
+          )}
+        ></div>
+        <div
+          className={classNames(
+            styles['line-horizontal'],
+            styles['line-horizontal-one']
+          )}
+        >
+          <div className={styles.square}></div>
+        </div>
+        <div
+          className={classNames(
+            styles['line-horizontal'],
+            styles['line-horizontal-two']
+          )}
+        >
+          <div className={styles.square}></div>
+        </div>
+        <div
+          className={classNames(
+            styles['line-horizontal'],
+            styles['line-horizontal-three']
+          )}
+        >
+          <div className={styles.square}></div>
+        </div>
+      </div>
+      <div className={styles.text}>Linen</div>
+    </div>
+  );
+}
