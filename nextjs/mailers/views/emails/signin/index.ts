52a867662be004efdcf1d7b1360edff0aad7c116
Emil Ajdyna <emil@ajdyna.com>
2022-09-20 08:42:55 +0200

    Use sign in vs log in

--- nextjs/mailers/views/emails/signin/index.ts
+++ nextjs/mailers/views/emails/signin/index.ts
@@ -3,7 +3,7 @@ import layout from '../../layouts/default';
 export default function email({ url }: { url: string }): string {
   return layout(`
     <h1>Sign in to Linen</h1>
-    <p>Click the link below to securely log in.<p>
+    <p>Click the link below to securely sign in.<p>
     <p><a href='${url}'>Sign in to Linen</a></p>
   `);
 }
