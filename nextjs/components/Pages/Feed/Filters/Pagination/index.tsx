8151f4f9b80f5a2ce9798840ab817b70c6d8ec3d
Emil Ajdyna <emil@ajdyna.com>
2022-09-26 08:41:45 +0200

    Add count to the pagination

--- nextjs/components/Pages/Feed/Filters/Pagination/index.tsx
+++ nextjs/components/Pages/Feed/Filters/Pagination/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import classNames from 'classnames';
+import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
 import styles from './index.module.css';
 
 export default function Pagination() {
   return (
     <div className={styles.pagination}>
-      <div className={styles.back}>
-        <BsChevronLeft />
+      <div className={styles.count}>1-10 of 20 000</div>
+      <div className={classNames(styles.icon, { [styles.disabled]: true })}>
+        <GoChevronLeft />
       </div>
-      <div className={styles.next}>
-        <BsChevronRight />
+      <div className={styles.icon}>
+        <GoChevronRight />
       </div>
     </div>
   );
