ddda806013ec17d3c28e6baea4df91cc24136130
Sandro de Souza <sandesou@gmail.com>
2022-09-29 10:24:25 -0300

    chore: skip sentry from client-side

--- nextjs/sentry.server.config.js
+++ nextjs/sentry.server.config.js
@@ -4,7 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN;
 const SKIP_SENTRY = process.env.SKIP_SENTRY === 'true';
 
 if (!SKIP_SENTRY) {
