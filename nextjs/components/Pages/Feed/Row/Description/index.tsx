c70db805c5d882c243761fc4941e83379451d970
Emil Ajdyna <emil@ajdyna.com>
2022-10-05 14:11:40 +0200

    Nicer description for a feed row

---
+++ nextjs/components/Pages/Feed/Row/Description/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './index.module.css';
+import { SerializedMessage } from 'serializers/message';
+import { format } from 'utilities/date';
+
+interface Props {
+  messages: SerializedMessage[];
+}
+
+export default function Description({ messages }: Props) {
+  if (messages.length === 1) {
+    const message = messages[0];
+    const date = format(message.sentAt);
+    return (
+      <div className={styles.description}>
+        {message.author?.displayName || 'User'} started this thread {date}.
+      </div>
+    );
+  }
+  const message = messages[messages.length - 1];
+  const date = format(message.sentAt);
+  return (
+    <div className={styles.description}>
+      {message.author?.displayName || 'User'} replied to this thread {date}.
+    </div>
+  );
+}
