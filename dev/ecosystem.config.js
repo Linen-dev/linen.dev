9b105f7ca9d8bdea248a493a63da396869d6a196
Sandro de Souza <sandesou@gmail.com>
2022-10-05 15:25:26 -0300

    clean up cdk and moving devtools

--- dev/ecosystem.config.js
+++ dev/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       name: 'nextjs',
       script: 'npm run dev',
-      cwd: 'nextjs',
+      cwd: '../nextjs',
       env: {
         PORT,
       },
@@ -15,22 +15,22 @@ module.exports = {
     {
       name: 'queue-chat',
       script: 'npm run queue:worker:chat-sync',
-      cwd: 'nextjs',
+      cwd: '../nextjs',
     },
     {
       name: 'queue-sync',
       script: 'npm run queue:worker:sync',
-      cwd: 'nextjs',
+      cwd: '../nextjs',
     },
     {
       name: 'queue-webhook',
       script: 'npm run queue:worker:webhook',
-      cwd: 'nextjs',
+      cwd: '../nextjs',
     },
     {
       name: 'push-service',
       script: 'mix phx.server',
-      cwd: 'push_service',
+      cwd: '../push_service',
       env: {
         MIX_ENV: 'prod',
         PORT: 4000,
