2ea6d342ad753ad1536ba09df66e7e6ef48ffa61
Sandro de Souza <sandesou@gmail.com>
2022-10-10 12:34:53 -0300

    chore: secure mentions and messages

--- nextjs/pages/api/mentions/index.ts
+++ nextjs/pages/api/mentions/index.ts
@@ -1,31 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getAuthFromSession } from 'utilities/session';
 import prisma from 'client';
 import serializeUser from 'serializers/user';
+import PermissionsService from 'services/permissions';
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const user = await getAuthFromSession(request, response);
-  if (!user) {
-    return response.status(401).end();
-  }
+  const permissions = await PermissionsService.get({
+    request,
+    response,
+    params: {},
+  });
+
+  // we may check for chat permissions here
+  // right now the users can only list users from same tenant
 
   if (request.method === 'GET') {
     const term = request.query.term as string;
-    const condition = term
-      ? { displayName: { contains: term, mode: 'insensitive' } }
-      : null;
-    const users = await prisma.users.findMany({
-      where: {
-        account: { id: user.accountId },
-        ...condition,
-      } as any,
-      take: 5,
-    });
-    return response.status(200).json(users.map(serializeUser));
+    const users = await getMentions(term, permissions.user?.accountId!);
+    response.status(200).json(users.map(serializeUser));
+    return response.end();
   }
 
   return response.status(405).end();
+}
+
+async function getMentions(term: string, accountId: string) {
+  const condition = term
+    ? { displayName: { contains: term, mode: 'insensitive' } }
+    : null;
+  const users = await prisma.users.findMany({
+    where: {
+      account: { id: accountId },
+      ...condition,
+    } as any,
+    take: 5,
+  });
+  return users;
 }
