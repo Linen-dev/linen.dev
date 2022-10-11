d771aad36cca7c062b50ac10364a4d98e72571ab
Emil Ajdyna <emil@ajdyna.com>
2022-10-11 12:06:56 +0200

    index based key handling

--- nextjs/components/Message/utilities/transform.ts
+++ nextjs/components/Message/utilities/transform.ts
@@ -1,13 +1,14 @@
 export default function transform(node: any, count = 1) {
-  node.cid = `${node.source}-${count}`;
+  count += 1;
+  node.cid = `${node.source || node.id}-${count}`;
   if (node.type === 'code') {
     if (node.value.includes('\n')) {
       node.type = 'pre';
     }
   }
   if (node.children) {
-    node.children = node.children.map((node: any) =>
-      transform(node, count + 1)
+    node.children = node.children.map((node: any, index: number) =>
+      transform(node, count + index)
     );
   }
   return node;
