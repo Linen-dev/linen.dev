a6a0fd6b27af15add316650a7bd07d61252d5c1d
Sandro de Souza <sandesou@gmail.com>
2022-10-11 18:36:05 -0300

    order feed by message desc

--- nextjs/pages/api/feed/index.ts
+++ nextjs/pages/api/feed/index.ts
@@ -1,10 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import PermissionsService from 'services/permissions';
 import CommunityService from 'services/community';
-import ChannelsService from 'services/channels';
 import Session from 'services/session';
 import prisma from 'client';
-import { ThreadState } from '@prisma/client';
+import { Prisma, ThreadState } from '@prisma/client';
 import serializeThread from 'serializers/thread';
 import { Scope } from 'types/shared';
 import { users } from '@prisma/client';
@@ -30,55 +29,59 @@ export async function index({
 }) {
   const community = await CommunityService.find(params);
   if (!community) {
-    return { status: 404 };
+    return { status: 404, data: 'NotFound' };
   }
   const page = getPage(params.page);
   const scope = params.scope || Scope.All;
   const limit = 10;
-  const channels = await ChannelsService.find(community.id);
   const condition = {
-    hidden: false,
-    state: params.state || ThreadState.OPEN,
-    channelId: { in: channels.map((channel) => channel.id) },
-    messageCount: { gte: 1 },
-    messages: {
-      some: {},
+    threads: {
+      hidden: false,
+      channel: { account: { id: community.id } },
+      state: params.state || ThreadState.OPEN,
     },
-  } as any;
+    ...(scope === Scope.Participant && {
+      OR: [
+        { usersId: currentUser.id },
+        { mentions: { some: { usersId: currentUser.id } } },
+      ],
+    }),
+  } as Prisma.messagesWhereInput;
 
-  if (scope === Scope.Participant) {
-    condition.messages.some.OR = [
-      { usersId: currentUser.id },
-      { mentions: { some: { usersId: currentUser.id } } },
-    ];
-  }
-
-  const total = await prisma.threads.count({
-    where: condition,
+  const total = await countThreads({
+    state: params.state || ThreadState.OPEN,
+    communityId: community.id,
+    currentUserId: scope === Scope.Participant ? currentUser.id : undefined,
   });
 
-  const threads = await prisma.threads.findMany({
+  const messages = await prisma.messages.findMany({
     where: condition,
-    include: {
-      messages: {
-        include: {
-          author: true,
-          mentions: {
-            include: {
-              users: true,
-            },
-          },
-          reactions: true,
-          attachments: true,
-        },
-        orderBy: { sentAt: 'asc' },
-      },
-      channel: true,
-    },
     orderBy: { sentAt: 'desc' },
+    distinct: ['threadId'],
     take: limit,
     skip: (page - 1) * limit,
+    select: {
+      threads: {
+        include: {
+          messages: {
+            include: {
+              author: true,
+              mentions: {
+                include: {
+                  users: true,
+                },
+              },
+              reactions: true,
+              attachments: true,
+            },
+            orderBy: { sentAt: 'asc' },
+          },
+          channel: true,
+        },
+      },
+    },
   });
+  const threads = messages.map(({ threads }) => threads!);
   return {
     status: 200,
     data: {
@@ -125,4 +128,33 @@ export default async function handler(
     return handlers.index(request, response);
   }
   return response.status(404).json({});
+}
+
+async function countThreads({
+  state,
+  communityId,
+  currentUserId,
+}: {
+  state: ThreadState;
+  communityId: string;
+  currentUserId?: string;
+}) {
+  return await prisma.threads.count({
+    where: {
+      hidden: false,
+      state: state || ThreadState.OPEN,
+      channel: { account: { id: communityId } },
+      messageCount: { gte: 1 },
+      messages: {
+        some: {
+          ...(!!currentUserId && {
+            OR: [
+              { usersId: currentUserId },
+              { mentions: { some: { usersId: currentUserId } } },
+            ],
+          }),
+        },
+      },
+    },
+  });
 }
