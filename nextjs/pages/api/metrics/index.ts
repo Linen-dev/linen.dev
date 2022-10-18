2780f87db4df04c27a7129282539b9f8e716a659
Emil Ajdyna <emil@ajdyna.com>
2022-10-18 14:07:42 +0200

    Add a first dummy metric

---
+++ nextjs/pages/api/metrics/index.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
+import prisma from 'client';
+import PermissionsService from 'services/permissions';
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === 'GET') {
+    const { communityId } = request.query;
+    if (typeof communityId !== 'string') {
+      return response.status(400).end();
+    }
+
+    const permissions = await PermissionsService.get({
+      request,
+      response,
+      params: { communityId },
+    });
+
+    if (!permissions.manage) {
+      return response.status(401).json({});
+    }
+
+    const members = await prisma.users.count({
+      where: { accountsId: communityId },
+    });
+    return response.status(200).json({
+      members,
+    });
+  }
+  return response.status(404).json({});
+}
--- nextjs/utilities/featureFlags.ts
+++ nextjs/utilities/featureFlags.ts
@@ -1,11 +1,9 @@
 export const isStripeEnabled = false;
 export const isRedirectToNewOnboardingEnabled = false;
 export const isNewOnboardingButtonEnabled = false;
-export const isMetricEnabled = false;
 
 const featureFlags = {
   isStripeEnabled,
-  isMetricEnabled,
 };
 
 export default featureFlags;
