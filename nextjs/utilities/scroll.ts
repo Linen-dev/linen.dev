24b9cbb55f9f3079cfcf1b667bdc42cba858fa67
Emil Ajdyna <emil@ajdyna.com>
2022-10-18 12:30:18 +0200

    Scroll to bottom when opening a thread

--- nextjs/utilities/scroll.ts
+++ nextjs/utilities/scroll.ts
@@ -3,3 +3,13 @@ export function scrollToBottom(node: HTMLElement) {
     node.scrollTop = node.scrollHeight;
   }
 }
+
+export function isInViewport(node: HTMLElement) {
+  const { top, left, bottom, right } = node.getBoundingClientRect();
+  return (
+    top >= 0 &&
+    left >= 0 &&
+    bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
