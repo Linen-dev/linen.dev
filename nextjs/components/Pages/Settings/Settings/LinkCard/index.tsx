dac3f42c28d868a2d05df11207150e0098ca1e98
cheeseblubber <kamleung911@gmail.com>
2022-10-18 23:12:52 -0400

    change community URL to be more obvious and clickable

--- nextjs/components/Pages/Settings/Settings/LinkCard/index.tsx
+++ nextjs/components/Pages/Settings/Settings/LinkCard/index.tsx
@@ -13,13 +13,21 @@ export default function LinkCard({ account }: Props) {
     return null;
   }
   return (
-    <div className="bg-blue-100">
-      <div className="px-4 py-5 sm:p-6">
-        <h3 className="text-lg leading-6 font-medium text-blue-900">
-          <a href={url} target="_blank" rel="noreferrer">
-            {text}
-          </a>
+    <div className="px-4 py-5 sm:p-6 flex flex-row justify-between">
+      <div className="grow">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Community URL
         </h3>
+        <div className="mt-2 sm:flex sm:items-start sm:justify-between">
+          <div className="max-w-xl text-sm text-gray-500">
+            <p>Navigate to your community.</p>
+          </div>
+        </div>
+      </div>
+      <div className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <a href={url} target="_blank" rel="noreferrer">
+          {text}
+        </a>
       </div>
     </div>
   );
