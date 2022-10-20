1080dc6e456da533ef06dfd3c04eafc77b660983
Emil Ajdyna <emil@ajdyna.com>
2022-10-20 15:45:12 +0200

    Fix avatar sizes

---
+++ nextjs/components/Avatar/index.test.tsx
@@ -0,0 +1,28 @@
+import Avatar from '.';
+import { render } from '@testing-library/react';
+
+describe('Avatar', () => {
+  it('renders an avatar', () => {
+    const { container } = render(<Avatar text="a" />);
+    expect(container).toHaveTextContent('a');
+  });
+
+  it('renders size classes', () => {
+    const { container } = render(<Avatar text="a" />);
+    expect(container.querySelector('.md')).toBeInTheDocument();
+  });
+
+  describe('when size is set to sm', () => {
+    it('renders size classes', () => {
+      const { container } = render(<Avatar text="a" size="sm" />);
+      expect(container.querySelector('.sm')).toBeInTheDocument();
+    });
+  });
+
+  describe('when shadow is set to none', () => {
+    it('does not apply the shadow class', () => {
+      const { container } = render(<Avatar text="a" shadow="none" />);
+      expect(container.querySelector('.shadow')).not.toBeInTheDocument();
+    });
+  });
+});
