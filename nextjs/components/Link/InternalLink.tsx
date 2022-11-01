d0f99a6e5d906c14aba08eee0ee6fe9c863c3d21
Emil Ajdyna <emil@ajdyna.com>
2022-11-01 10:04:37 +0100

    Move threads between channels

--- nextjs/components/Link/InternalLink.tsx
+++ nextjs/components/Link/InternalLink.tsx
@@ -4,6 +4,7 @@ import usePath from 'hooks/path';
 interface Props {
   className?: string;
   onClick?(): void;
+  onDrop?(event: React.DragEvent): void;
   href: string;
   children: React.ReactNode;
 }
@@ -11,13 +12,14 @@ interface Props {
 export default function InternalLink({
   className,
   onClick,
+  onDrop,
   href,
   children,
 }: Props) {
   const path = usePath({ href });
   return (
     <Link href={path} prefetch={false}>
-      <a onClick={onClick} className={className}>
+      <a onClick={onClick} onDrop={onDrop} className={className}>
         {children}
       </a>
     </Link>
