98211943e0813b0249b27db209ef90cba906caaa
Emil Ajdyna <emil@ajdyna.com>
2022-11-04 10:27:29 +0100

    Send file to the backend

--- nextjs/components/MessageForm/FileInput/index.tsx
+++ nextjs/components/MessageForm/FileInput/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import styles from './index.module.scss';
 import { FiUpload } from 'react-icons/fi';
 
-export default function FileInput() {
+interface Props {
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void;
+}
+
+export default function FileInput({ onChange }: Props) {
   return (
     <label className={styles.label} htmlFor="file">
       <FiUpload />
-      <input className={styles.input} type="file" id="file" name="file" />
+      <input
+        className={styles.input}
+        type="file"
+        id="file"
+        name="file"
+        onChange={onChange}
+      />
     </label>
   );
 }
