32b47be8e8d9158b3a23a63044e76e7884a8f6b1
Emil Ajdyna <emil@ajdyna.com>
2022-11-09 15:21:57 +0100

    Allow users to upload avatars

--- nextjs/components/layout/PageLayout/Header/UserAvatar/ProfileForm/AvatarField/index.tsx
+++ nextjs/components/layout/PageLayout/Header/UserAvatar/ProfileForm/AvatarField/index.tsx
@@ -2,21 +2,33 @@ import Avatar from 'components/Avatar';
 import Label from 'components/Label';
 import Field from 'components/Field';
 import { SerializedUser } from 'serializers/user';
+import styles from './index.module.scss';
 
 interface Props {
+  disabled: boolean;
   user: SerializedUser;
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void;
 }
 
-export default function AvatarField({ user }: Props) {
+export default function AvatarField({ disabled, user, onChange }: Props) {
+  // TODO add loading state to the Avatar
   return (
     <Field>
-      <Label htmlFor="avatar">
+      <Label className={styles.label} htmlFor="avatar">
         Avatar
         <Avatar
           size="md"
           shadow="none"
           src={user.profileImageUrl}
           text={user.displayName}
+        />
+        <input
+          className={styles.input}
+          disabled={disabled}
+          type="file"
+          id="avatar"
+          name="avatar"
+          onChange={onChange}
         />
       </Label>
     </Field>
