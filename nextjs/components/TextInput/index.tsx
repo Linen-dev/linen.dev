fbb8779ca8d2a3d97332226b6f413c26929a1e35
Emil Ajdyna <emil@ajdyna.com>
2022-11-10 10:55:25 +0100

    Change input colors

--- nextjs/components/TextInput/index.tsx
+++ nextjs/components/TextInput/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import Label from '../Label';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 interface Props {
   className?: string;
