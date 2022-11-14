7d2735c30cc7a8b78fb2a3771d50d5dedd6f4885
Emil Ajdyna <emil@ajdyna.com>
2022-11-14 17:21:35 +0100

    Add sass

--- apps/desktop/src/pages/index.tsx
+++ apps/desktop/src/pages/index.tsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react';
 import { get } from '../utilities/http';
+import styles from './index.module.scss';
 
 function App() {
   const [active, setActive] = useState(false);
 
   useEffect(() => {
     let mounted = true;
+    get('/api/health')
+      .then(async () => {
+        if (mounted) {
+          setActive(true);
+        }
+      })
+      .catch(() => {
+        if (mounted) {
+          setActive(false);
+        }
+      });
     const intervalId = setInterval(() => {
       get('/api/health')
         .then(async () => {
@@ -27,7 +39,7 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>Welcome to Linen!</h1>
       <p>Linen is: {active ? 'Online' : 'Offline'}</p>
     </div>
