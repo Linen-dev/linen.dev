a5b5a355b411418c628f4a285be72321000f9914
Emil Ajdyna <emil@ajdyna.com>
2022-11-14 15:27:57 +0100

    Add a basic http call to https://linen.dev

--- apps/desktop/src/pages/index.tsx
+++ apps/desktop/src/pages/index.tsx
@@ -1,75 +1,35 @@
-import { useState } from 'react';
-import { invoke } from '@tauri-apps/api/tauri';
-import Image from 'next/image';
-import reactLogo from '../assets/react.svg';
-import tauriLogo from '../assets/tauri.svg';
-import nextLogo from '../assets/next.svg';
+import { useEffect, useState } from 'react';
+import { get } from '../utilities/http';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('');
-  const [name, setName] = useState('');
+  const [active, setActive] = useState(false);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name }));
-  }
+  useEffect(() => {
+    let mounted = true;
+    const intervalId = setInterval(() => {
+      get('/api/health')
+        .then(async () => {
+          if (mounted) {
+            setActive(true);
+          }
+        })
+        .catch(() => {
+          if (mounted) {
+            setActive(false);
+          }
+        });
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+      mounted = false;
+    };
+  }, []);
 
   return (
     <div className="container">
       <h1>Welcome to Linen!</h1>
-
-      <div className="row">
-        <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
-      </div>
-
-      <p>Click on the Tauri, Next, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-
-      <p>{greetMsg}</p>
+      <p>Linen is: {active ? 'Online' : 'Offline'}</p>
     </div>
   );
 }
