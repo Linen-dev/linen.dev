7f1229819fdea240253a6b50df9c5f3da5e80898
Emil Ajdyna <emil@ajdyna.com>
2022-11-15 12:09:21 +0100

    Remove the unnecessary active code

--- apps/desktop/src/pages/index.tsx
+++ apps/desktop/src/pages/index.tsx
@@ -1,46 +1,9 @@
-import { useEffect, useState } from 'react';
-import { get } from '../utilities/http';
-import styles from './index.module.scss';
 import Logo from '../components/Logo/Linen';
 import Button from '../components/Pages/Splash/Button';
 import { FiRss } from 'react-icons/fi';
+import styles from './index.module.scss';
 
 function App() {
-  const [active, setActive] = useState(false);
-
-  useEffect(() => {
-    let mounted = true;
-    get('/api/health')
-      .then(async () => {
-        if (mounted) {
-          setActive(true);
-        }
-      })
-      .catch(() => {
-        if (mounted) {
-          setActive(false);
-        }
-      });
-    const intervalId = setInterval(() => {
-      get('/api/health')
-        .then(async () => {
-          if (mounted) {
-            setActive(true);
-          }
-        })
-        .catch(() => {
-          if (mounted) {
-            setActive(false);
-          }
-        });
-    }, 5000);
-
-    return () => {
-      clearInterval(intervalId);
-      mounted = false;
-    };
-  }, []);
-
   return (
     <div className={styles.container}>
       <div className={styles.column}>
