87f6a2a2a659315bc8939cc5fededd01fc4177b8
Emil Ajdyna <emil@ajdyna.com>
2022-11-15 11:32:41 +0100

    Add notes about api/signin security

--- nextjs/pages/api/signin/index.ts
+++ nextjs/pages/api/signin/index.ts
@@ -7,6 +7,21 @@ interface CreateParams {
   password: string;
 }
 
+// we could make this more secure in few ways
+// some of these might be good enough for some automated attacks
+// 1. rate limit the /signin endpoint, e.g. w/ redis [high effort]
+// 2. detect and block suspicious traffic [high effort]
+// 3. force endpoint delay after each attempt [med effort]
+// 4. allow N attempts per user, lock after that [med effort]
+// 5. only allow certain clients, e.g. embed token within desktop app during build [med effort]
+// 6. implement and force 2FA for all users [high effort]
+// 7. require complex passwords [low effort]
+
+// we could also make this process a bit more complex too to at least annoy an attacker
+// 1. increase complexity by requiring csrf token from /api/auth/csrf [low effort]
+// 2. increase complexity by keeping an extra token in the repo [low effort]
+// 3. increase complexity, encode password on the client, decode here [low effort]
+
 export async function create(params: CreateParams) {
   const { email, password } = params;
   if (!email || !password) {
