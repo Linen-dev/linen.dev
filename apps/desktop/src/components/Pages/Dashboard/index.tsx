d5dc68e803c9aec99984e39e655044ef2135a0d3
Emil Ajdyna <emil@ajdyna.com>
2022-11-16 13:08:28 +0100

    [desktop] Reuse colors from the main app and add a header

--- apps/desktop/src/components/Pages/Dashboard/index.tsx
+++ apps/desktop/src/components/Pages/Dashboard/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './index.module.scss';
+import Title from './Title';
+import Header from './Header';
 
 export default function Dashboard() {
-  return <div className={styles.container}>Dashboard</div>;
+  return (
+    <div className={styles.container}>
+      <Title />
+      <Header />
+    </div>
+  );
 }
