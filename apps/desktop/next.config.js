09c1757d02c4cc5479582753b66632ea7b25c34c
Emil Ajdyna <emil@ajdyna.com>
2022-11-16 15:22:01 +0100

    [desktop] Reuse react components from the web app

--- apps/desktop/next.config.js
+++ apps/desktop/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 
+const { addPackagesPathToSwcLoader } = require('../../packages/config');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
+  },
+  webpack(config) {
+    config.module.rules.forEach(addPackagesPathToSwcLoader);
+    return config;
   },
 };
 
