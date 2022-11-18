f61f1adb5d59fb0949bdd95a5ef364baab8ae519
Emil Ajdyna <emil@ajdyna.com>
2022-11-18 10:26:36 +0100

    [web] improve db seed to match linen format

--- apps/web/bin/factory/messages.ts
+++ apps/web/bin/factory/messages.ts
@@ -2,19 +2,19 @@ const messages = [
   `
 Hey 👋
 Quick note, I saw that there's a spelling error in the license dashboard UI:
-&gt; 1 devices @ $1.00/device/month
-&gt; Billed *anually* at $12.00/yr
-&gt; Next payment on Apr 13, 2023
+> 1 devices @ $1.00/device/month
+> Billed *anually* at $12.00/yr
+> Next payment on Apr 13, 2023
 Should be *annually*.
 `,
   `
-<https://github.com/> - that should take care of that. Thanks for reporting it
+https://github.com/ - that should take care of that. Thanks for reporting it
 `,
   `
 Thank you both!
 `,
   `
-Check this out! <https://www.youtube.com/embed/Xsew54QshN8>
+Check this out! https://www.youtube.com/embed/Xsew54QshN8
 `,
   `
 Having the *right amount of characters* on each line is key to the readability of your text.
