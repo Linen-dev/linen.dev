2d8243d0c5a13f9bc0c43bedc9f5e1f34a9caf8e
Emil Ajdyna <emil@ajdyna.com>
2022-11-21 15:30:47 +0100

    [web] add margins to message headers

--- apps/web/components/Message/Header/index.tsx
+++ apps/web/components/Message/Header/index.tsx
@@ -8,17 +8,17 @@ interface Props {
 export default function Header({ depth, children }: Props) {
   switch (depth) {
     case 1:
-      return <h1 className="text-2xl font-bold">{children}</h1>;
+      return <h1 className="text-2xl font-bold mb-2">{children}</h1>;
     case 2:
-      return <h2 className="text-xl font-bold">{children}</h2>;
+      return <h2 className="text-xl font-bold mb-2">{children}</h2>;
     case 3:
-      return <h3 className="text-lg font-bold">{children}</h3>;
+      return <h3 className="text-lg font-bold mb-2">{children}</h3>;
     case 4:
-      return <h4 className="text-base font-bold">{children}</h4>;
+      return <h4 className="text-base font-bold mb-2">{children}</h4>;
     case 5:
-      return <h5 className="text-sm font-bold">{children}</h5>;
+      return <h5 className="text-sm font-bold mb-2">{children}</h5>;
     case 6:
-      return <h6 className="text-xs font-bold">{children}</h6>;
+      return <h6 className="text-xs font-bold mb-2">{children}</h6>;
     default:
       return <>{children}</>;
   }
