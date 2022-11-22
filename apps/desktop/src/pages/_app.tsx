a236ede815340cdc9777d2b329ee6744ec15aea0
Emil Ajdyna <emil@ajdyna.com>
2022-11-22 15:17:13 +0100

    [desktop] add ui package css

--- apps/desktop/src/pages/_app.tsx
+++ apps/desktop/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import '../style.css';
+import '@linen/ui/dist/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
