3585566836c88dc4a5aa1ab3571c8842620ccc35
Emil Ajdyna <emil@ajdyna.com>
2022-11-29 22:32:42 +0100

    [apps/desktop] fetch feed data in the desktop app

--- apps/desktop/src/utilities/http.ts
+++ apps/desktop/src/utilities/http.ts
@@ -2,9 +2,12 @@ import { fetch, Body } from '@tauri-apps/api/http';
 const HOST = 'http://localhost:3000';
 // const HOST = 'https://linen.dev';
 
-export async function get(url: string): Promise<any> {
+export async function get(url: string, token?: string): Promise<any> {
   return fetch(`${HOST}${url}`, {
     method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
--- apps/web/api/routes.ts
+++ apps/web/api/routes.ts
@@ -4,6 +4,7 @@ import ChannelsRoute from 'api/routes/channels';
 import TenantsRoute from 'api/routes/threads';
 import PingRoute from 'api/routes/ping';
 import DemoRoute from 'api/routes/demo';
+import FeedRoute from 'api/routes/feed';
 
 const routes = [
   AuthRoute,
@@ -12,5 +13,6 @@ const routes = [
   TenantsRoute,
   PingRoute,
   DemoRoute,
+  FeedRoute,
 ];
 export default routes;
