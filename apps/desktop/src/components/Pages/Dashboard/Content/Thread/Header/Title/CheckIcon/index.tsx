0ea5bd099f79e58d4b37312ae219f7aa9fc4d9a7
Emil Ajdyna <emil@ajdyna.com>
2022-12-01 10:41:37 +0100

    [apps/desktop] add a naive version of the thread view in the desktop app

---
+++ apps/desktop/src/components/Pages/Dashboard/Content/Thread/Header/Title/CheckIcon/index.tsx
@@ -0,0 +1,10 @@
+import { GoCheck } from 'react-icons/go';
+import styles from './index.module.scss';
+
+export default function CheckIcon() {
+  return (
+    <div className={styles.check}>
+      <GoCheck />
+    </div>
+  );
+}
---
+++ apps/desktop/src/components/Pages/Dashboard/Content/Thread/Header/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import Title from './Title';
+import { StickyHeader } from '@linen/ui';
+import classNames from 'classnames';
+import { ThreadState } from '@prisma/client';
+import { GoCheck, GoChevronLeft, GoX, GoSync } from 'react-icons/go';
+import { SerializedThread } from '@linen/types';
+import styles from './index.module.scss';
+
+interface Props {
+  thread: SerializedThread;
+  channelName: string;
+  manage: boolean;
+  onClose?(): void;
+  onCloseThread(): void;
+  onReopenThread(): void;
+  onSetTitle(title: string): void;
+}
+
+export default function Header({
+  thread,
+  channelName,
+  manage,
+  onClose,
+  onCloseThread,
+  onReopenThread,
+  onSetTitle,
+}: Props) {
+  const { title, state } = thread;
+  return (
+    <StickyHeader>
+      <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-center">
+          {onClose && (
+            <div className="flex items-center md:hidden mr-2">
+              <a onClick={onClose}>
+                <GoChevronLeft />
+              </a>
+            </div>
+          )}
+
+          <div>
+            <Title
+              title={title}
+              state={state}
+              manage={manage}
+              onSetTitle={onSetTitle}
+            />
+            <div className="text-gray-600 text-xs ">#{channelName}</div>
+          </div>
+        </div>
+        <div className={styles.icons}>
+          {manage && (
+            <>
+              {state === ThreadState.OPEN && (
+                <a
+                  className={styles.icon}
+                  title="Close thread"
+                  onClick={() => {
+                    onCloseThread();
+                  }}
+                >
+                  <GoCheck />
+                </a>
+              )}
+              {state === ThreadState.CLOSE && (
+                <a
+                  className={styles.icon}
+                  title="Reopen thread"
+                  onClick={() => {
+                    onReopenThread();
+                  }}
+                >
+                  <GoSync />
+                </a>
+              )}
+            </>
+          )}
+          {onClose && (
+            <a
+              onClick={onClose}
+              className={classNames(styles.icon, styles.close)}
+            >
+              <GoX />
+            </a>
+          )}
+        </div>
+      </div>
+    </StickyHeader>
+  );
+}
