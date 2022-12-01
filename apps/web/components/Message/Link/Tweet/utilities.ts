4ac5223be6b91df93b926b31dff15cb092458c05
Emil Ajdyna <emil@ajdyna.com>
2022-12-01 13:30:27 +0100

    [apps/desktop] remove tweet code

--- apps/web/components/Message/Link/Tweet/utilities.ts
+++
@@ -1,5 +0,0 @@
-export function getTweetId(src: string) {
-  const parts = src.split('/');
-  const id = parts[parts.length - 1];
-  return id.split('?')[0];
-}
--- apps/web/components/Message/Link/index.tsx
+++ apps/web/components/Message/Link/index.tsx
@@ -5,7 +5,6 @@ import { isImage, isVideo, isUrlValid, isTweet, isMail } from './utilities';
 import Accordion from 'components/Accordion';
 import Image from './Image';
 import Video from './Video';
-import Tweet from './Tweet';
 import Mail from './Mail';
 
 interface Props {
@@ -31,15 +30,6 @@ export default function Link({ value }: Props) {
       </Accordion>
     );
   }
-
-  // Disabling twitter since it is not playing well with mobile view
-  // if (isTweet(href)) {
-  //   return (
-  //     <Accordion header={name || href}>
-  //       <Tweet src={href} />
-  //     </Accordion>
-  //   );
-  // }
 
   if (isMail(href)) {
     return <Mail value={value} />;
