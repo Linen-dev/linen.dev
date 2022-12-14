50ca1db9bdb627932ad3640d5360543020e1cd3d
Emil Ajdyna <emil@ajdyna.com>
2022-12-14 11:30:35 +0100

    [apps/web] add an api that upserts multiple read statuses

--- apps/web/pages/api/read-status/index.ts
+++ apps/web/pages/api/read-status/index.ts
@@ -1,37 +1,49 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-// import Session from 'services/session';
-// import { updateReadStatus } from 'services/users/read-status';
-// import to from 'utilities/await-to-js';
-// import { z } from 'zod';
+import Session from 'services/session';
+import to from 'utilities/await-to-js';
+import { z } from 'zod';
+import { prisma } from 'client';
 
 async function post(request: NextApiRequest, response: NextApiResponse) {
-  // const user = await Session.auth(request, response);
-  // if (!user) {
-  //   return response.status(401).json({});
-  // }
+  const user = await Session.auth(request, response);
+  if (!user) {
+    return response.status(401).json({});
+  }
 
-  // const schema = z.object({
-  //   channelId: z.string().min(1),
-  //   timestamp: z.number(),
-  // });
-  // const [badRequest, body] = await to(schema.parseAsync(request.body));
-  // if (badRequest) {
-  //   return response.status(400).json({ error: badRequest.message });
-  // }
+  const schema = z.object({
+    channelIds: z.string().array().min(1)
+  });
+  const [badRequest, body] = await to(schema.parseAsync(request.body));
+  if (badRequest) {
+    return response.status(400).json({ error: badRequest.message });
+  }
 
-  // const [err, _] = await to(
-  //   updateReadStatus({
-  //     channelId: body.channelId,
-  //     authId: user.id,
-  //     timestamp: BigInt(body.timestamp),
-  //   })
-  // );
-  // if (err) {
-  //   console.error(err);
-  //   return response.status(500).json({});
-  // }
+  const [err, readStatuses] = await to(prisma.$transaction(
+    body.channelIds.map((channelId: string) => {
+      const data = {
+        authId: user.id,
+        channelId,
+        lastReadAt: BigInt(new Date().getTime())
+      };
+      return prisma.readStatus.upsert({
+        create: data,
+        update: data,
+        where: {
+          authId_channelId: {
+            authId: user.id,
+            channelId,
+          },
+        },
+      });
+    })
+  ))
 
-  return response.status(200).json({});
+  if (err) {
+    console.error(err);
+    return response.status(500).json({});
+  }
+
+  return response.status(200).json({ readStatuses });
 }
 
 export default async function handler(
