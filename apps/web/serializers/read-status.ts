1d04e60d8306d14bb69e8656af9255e0c591aef2
Emil Ajdyna <emil@ajdyna.com>
2022-12-14 12:45:43 +0100

    [apps/web] add a boolean to the read status serializer to simplify the client

--- apps/web/serializers/read-status.ts
+++ apps/web/serializers/read-status.ts
@@ -1,12 +1,19 @@
 import { SerializedReadStatus } from '@linen/types'
 
+function isChannelRead (lastReadAt: BigInt, lastReplyAt: BigInt | null): boolean {
+  if (!lastReplyAt) { return true }
+  return lastReadAt >= lastReplyAt
+}
+
 function serialize (status: any): SerializedReadStatus {
   const thread = status.channel.threads[0]
-  const lastReplyAt = thread?.sentAt?.toString() || null
+  const lastReadAt = status.lastReadAt
+  const lastReplyAt = thread?.sentAt
   return {
     channelId: status.channelId,
-    lastReadAt: status.lastReadAt.toString(),
-    lastReplyAt
+    lastReadAt: lastReadAt.toString(),
+    lastReplyAt: lastReplyAt ? lastReplyAt.toString() : null,
+    read: isChannelRead(lastReadAt, lastReplyAt),
   }
 }
 
