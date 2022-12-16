5cb135e3c1fcc1f2ad98128a31990be4d6b09b16
Emil Ajdyna <emil@ajdyna.com>
2022-12-16 12:08:41 +0100

    [packages/utilities] improve storage.set

--- packages/utilities/src/storage.ts
+++ packages/utilities/src/storage.ts
@@ -6,8 +6,9 @@ const storage = {
       return null
     }
   },
-  set (key: string, value: string) {
+  set (key: string, input: string | object) {
     try {
+      const value = typeof input === 'object' ? JSON.stringify(input) : input
       return window.localStorage.setItem(key, value)
     } catch (exception) {
       return null
