fb12b445f9512d6a75b75cde0978d5e0a8abb61b
Emil Ajdyna <emil@ajdyna.com>
2022-12-21 12:31:20 +0100

    [packages/utilities] add an event-emitter utility

---
+++ packages/utilities/src/event.js
@@ -0,0 +1,17 @@
+const EventEmitter = {
+  events: {},
+  emit (name, data) {
+    if (!this.events[name]) { return }
+    this.events[name].forEach(callback => callback(data))
+  },
+  on (name, callback) {
+    if (!this.events[name]) { this.events[name] = [] }
+    this.events[name].push(callback)
+  },
+  off (name, callback) {
+    if (!this.events[name]) { return }
+    this.events[name] = this.events[name].filter(handler => handler !== callback)
+  }
+}
+
+export default EventEmitter
