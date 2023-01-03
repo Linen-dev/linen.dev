db1d4c0c11fe8c726e02ad78139e250e332b3a6a
Sandro de Souza <sandesou@gmail.com>
2023-01-03 19:07:09 -0300

    refactor(web): add new integrations in our event system

---
+++ apps/web/services/events/eventNewIntegration.ts
@@ -0,0 +1,19 @@
+import { createSyncJob } from 'queue/jobs';
+import { SyncJobType } from 'services/sync';
+
+export type NewMessageEvent = {
+  accountId: string;
+};
+
+export async function eventNewIntegration(event: NewMessageEvent) {
+  const syncJob: SyncJobType = {
+    account_id: event.accountId,
+  };
+
+  const promises: Promise<any>[] = [
+    // it should dispatch messages to our queue system
+    createSyncJob(syncJob),
+  ];
+
+  await Promise.allSettled(promises);
+}
