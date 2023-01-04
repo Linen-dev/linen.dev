5b0ba7532ffe0f4a693537b12564185d7e0ce3c5
Emil Ajdyna <emil@ajdyna.com>
2023-01-04 09:03:41 +0100

    [apps/web] use prevent default to explicitly handle shift key behavior

--- apps/desktop/src/components/Pages/Feed/Content/Thread/MessageForm/index.tsx
+++ apps/desktop/src/components/Pages/Feed/Content/Thread/MessageForm/index.tsx
@@ -309,6 +309,7 @@ function MessageForm({
             }
             if (event.key === 'Enter') {
               if (event.ctrlKey || event.shiftKey) {
+                event.preventDefault();
                 const position = getCaretPosition(ref);
                 setMessage((message) => {
                   return [
