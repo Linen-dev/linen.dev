ea35f476041ddef2d6399d5a5ff2f0191043f41a
Emil Ajdyna <emil@ajdyna.com>
2023-01-04 13:35:48 +0100

    [apps/web] more explicit drag and drop state

--- apps/web/components/GridRow/index.tsx
+++ apps/web/components/GridRow/index.tsx
@@ -69,7 +69,7 @@ export function Row({
       })}
     >
       {header}
-      <div className={styles.row}>
+      <div className={classNames(styles.row, { [styles.drag]: mode === Mode.Drag })}>
         <div className={styles.left}>
           {top ? (
             <Avatar
