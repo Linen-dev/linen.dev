5714ebe10463ad8b4be590d556c334084cf5f8e0
Emil Ajdyna <emil@ajdyna.com>
2023-01-06 14:08:14 +0100

    [apps/web] simplify the DefaultChannelRow component

--- apps/web/components/Pages/Settings/Content/DefaultChannelRow/index.tsx
+++ apps/web/components/Pages/Settings/Content/DefaultChannelRow/index.tsx
@@ -1,107 +1,50 @@
-import React, { useState, Fragment } from 'react';
-import { GoCheck, GoChevronDown } from 'react-icons/go';
-import { Listbox, Transition } from '@headlessui/react';
-import classNames from 'classnames';
+import React, { useState } from 'react';
 import { Toast } from '@linen/ui';
 import { SerializedAccount, SerializedChannel } from '@linen/types';
 import DiscordIcon from 'components/icons/DiscordIcon';
 import SlackIcon from 'components/icons/SlackIcon';
+import { NativeSelect } from '@linen/ui';
 
-type DropDownChannelsProps = {
-  channels?: SerializedChannel[];
-  selected?: SerializedChannel;
-  onChange: (channel: SerializedChannel) => void;
-  hasAuth?: boolean;
-  CommunityIcon: any;
-};
-
-function DropDownChannels({
-  channels,
-  selected,
-  onChange,
-  CommunityIcon,
-}: DropDownChannelsProps) {
-  return (
-    <Listbox value={selected} onChange={onChange}>
-      {({ open }) => (
-        <>
-          <Listbox.Label className="sr-only">
-            Change published status
-          </Listbox.Label>
-          <div className="relative">
-            <div className="inline-flex shadow-sm rounded-md divide-x divide-blue-700">
-              <div className="relative z-0 inline-flex shadow-sm rounded-md divide-x divide-blue-700">
-                <div className="relative inline-flex items-center bg-blue-700 py-2 pl-3 pr-4 border border-transparent rounded-l-md shadow-sm text-white">
-                  <CommunityIcon color="#eee" />
-                  <p className="ml-2.5 text-sm font-medium">
-                    {selected?.channelName || 'choose one'}
-                  </p>
-                </div>
-                <Listbox.Button className="relative inline-flex items-center bg-blue-700 p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-blue-300">
-                  <span className="sr-only">Change default channel</span>
-                  <GoChevronDown className="h-5 w-5 text-white" />
-                </Listbox.Button>
-              </div>
-            </div>
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="origin-top-right absolute z-10 right-0 mt-2 w-72 rounded-md shadow-lg overflow-hidden bg-white divide-y divide-gray-200 ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {channels?.map((option) => (
-                  <Listbox.Option
-                    key={option.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'text-white bg-blue-700' : 'text-gray-900',
-                        'cursor-default select-none relative p-2 text-sm'
-                      )
-                    }
-                    value={option}
-                  >
-                    {({ selected, active }) => (
-                      <div className="flex flex-col">
-                        <div className="flex justify-between">
-                          <p
-                            className={
-                              selected ? 'font-semibold' : 'font-normal'
-                            }
-                          >
-                            {option.channelName}
-                          </p>
-                          {selected ? (
-                            <span
-                              className={
-                                active ? 'text-white' : 'text-blue-600'
-                              }
-                            >
-                              <GoCheck className="h-5 w-5" />
-                            </span>
-                          ) : null}
-                        </div>
-                      </div>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
-      )}
-    </Listbox>
-  );
+interface Props {
+  channels: SerializedChannel[],
+  currentCommunity: SerializedAccount
 }
 
-function ChannelsDefaultComponent({
-  channels,
-  onChange,
-  selected,
-  hasAuth,
-  CommunityIcon,
-}: DropDownChannelsProps) {
+export default function DefaultChannelRow({ channels, currentCommunity }: Props) {
+  const [defaultChannel, setDefaultChannel] = useState(
+    channels.find((channel) => channel.default)
+  );
+  const [selected, setSelected] = useState(defaultChannel);
+
+  const CommunityIcon =
+    currentCommunity.communityType === 'discord' ? DiscordIcon : SlackIcon;
+
+  async function onChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const id = event.currentTarget.value
+    const channelSelected = channels.find((channel) => channel.id === id) as SerializedChannel
+    if (channelSelected && channelSelected.id !== defaultChannel?.id) {
+      return fetch('/api/channels/default', {
+        method: 'PUT',
+        body: JSON.stringify({
+            communityId: currentCommunity.id,
+            channelId: channelSelected?.id,
+            originalChannelId: defaultChannel?.id,
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              setDefaultChannel(channelSelected);
+              setSelected(channelSelected);
+              Toast.success('Saved successfully!');
+            } else {
+              Toast.error('Something went wrong!');
+            }
+          })
+          .catch(() => Toast.error('Something went wrong'));
+    }
+    
+  }
+
   return (
     <div className="bg-white">
       <div className="py-3">
@@ -120,63 +63,16 @@ function ChannelsDefaultComponent({
             </div>
           </div>
           <div className="self-center">
-            {hasAuth ? (
-              <DropDownChannels
-                channels={channels}
-                selected={selected}
-                onChange={onChange}
-                CommunityIcon={CommunityIcon}
-              />
-            ) : (
-              <p className="text-sm text-gray-400">Waiting for integration</p>
-            )}
+            <NativeSelect
+              icon={<CommunityIcon color='#fff' />}
+              theme='blue'
+              defaultValue={selected}
+              onChange={onChange}
+              options={channels?.map(channel => ({ label: channel.channelName, value: channel.id }))}
+            />
           </div>
         </div>
       </div>
     </div>
-  );
-}
-
-export default function DefaultChannelRow({ channels, currentCommunity }: { channels: SerializedChannel[], currentCommunity: SerializedAccount }) {
-  const [defaultChannel, setDefaultChannel] = useState(
-    channels?.find((channel) => channel.default)
-  );
-  const [selected, setSelected] = useState(defaultChannel);
-
-  const CommunityIcon =
-    currentCommunity?.communityType === 'discord' ? DiscordIcon : SlackIcon;
-
-  async function onDefaultChannelChange(channelSelected: SerializedChannel) {
-    if (defaultChannel?.id === channelSelected.id) {
-      return; // nothing to do
-    }
-    fetch('/api/channels/default', {
-      method: 'PUT',
-      body: JSON.stringify({
-        communityId: currentCommunity?.id,
-        channelId: channelSelected?.id,
-        originalChannelId: defaultChannel?.id,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          setDefaultChannel(channelSelected);
-          setSelected(channelSelected);
-          Toast.success('Saved successfully!');
-        } else {
-          Toast.error('Something went wrong!');
-        }
-      })
-      .catch(() => Toast.error('Something went wrong'));
-  }
-
-  return (
-    <ChannelsDefaultComponent
-      onChange={onDefaultChannelChange}
-      selected={selected}
-      channels={channels}
-      hasAuth={!!currentCommunity}
-      CommunityIcon={CommunityIcon}
-    />
   );
 }
