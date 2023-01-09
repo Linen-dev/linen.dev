c17fe9d3f13d555f69a5d403ed8d590f035dbf2d
Emil Ajdyna <emil@ajdyna.com>
2023-01-09 14:40:37 +0100

    [apps/web] migrate the members page

---
+++ apps/web/services/members/index.tsx
@@ -0,0 +1,46 @@
+import { GetServerSidePropsContext } from 'next';
+import serializeSettings from 'serializers/account/settings';
+import ChannelsService from 'services/channels';
+import CommunityService from 'services/community';
+import Session from 'services/session';
+import PermissionsService from 'services/permissions';
+import { NotFound, RedirectTo } from 'utilities/response';
+import serializeAccount from 'serializers/account';
+import serializeChannel from 'serializers/channel';
+import { qs } from 'utilities/url';
+
+export async function getMembersServerSideProps(
+  context: GetServerSidePropsContext,
+  isSubDomainRouting: boolean
+) {
+  const community = await CommunityService.find(context.params);
+  if (!community) {
+    return NotFound();
+  }
+
+  const permissions = await PermissionsService.for(context);
+  if (!permissions.access || !permissions.manage) {
+    return RedirectTo(
+      `/signin?${qs({
+        ...(permissions.auth?.id && { error: 'private' }),
+        callbackUrl: context.req.url,
+      })}`
+    );
+  }
+
+  const channels = await ChannelsService.find(community.id);
+  const settings = serializeSettings(community);
+
+  const token = await Session.tokenRaw(context.req);
+
+  return {
+    props: {
+      token: token || null,
+      currentCommunity: serializeAccount(community),
+      channels: channels.map(serializeChannel),
+      permissions,
+      settings,
+      isSubDomainRouting,
+    },
+  };
+}
