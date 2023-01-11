76e122b6e074e892ca76bed8641d7246540167ee
Sandro de Souza <sandesou@gmail.com>
2023-01-11 20:03:03 -0300

    feat(seo): show pages from channel on channel view

--- apps/web/components/Pages/Channel/ChannelForBots.tsx
+++ apps/web/components/Pages/Channel/ChannelForBots.tsx
@@ -3,7 +3,9 @@ import Grid from './Content/Grid';
 import { ChannelProps } from '.';
 import PageLayout from 'components/layout/PageLayout';
 import { buildChannelSeo } from 'utilities/seo';
-import { SerializedChannel } from '@linen/types';
+import { SerializedChannel, Settings } from '@linen/types';
+import ButtonPagination from 'components/ButtonPagination';
+import CustomLinkHelper from 'components/Link/CustomLinkHelper';
 
 export default function ChannelForBots({
   threads,
@@ -49,7 +51,47 @@ export default function ChannelForBots({
           onReaction={() => {}}
           onDrop={() => {}}
         />
+        <PaginationForBots
+          {...{ currentChannel, isSubDomainRouting, settings }}
+        />
       </ul>
     </PageLayout>
+  );
+}
+
+function PaginationForBots({
+  currentChannel,
+  isSubDomainRouting,
+  settings,
+}: {
+  currentChannel: SerializedChannel;
+  isSubDomainRouting: boolean;
+  settings: Settings;
+}) {
+  return (
+    <>
+      {currentChannel && currentChannel.pages ? (
+        <div className="text-center p-4">
+          {Array.from(Array(currentChannel.pages).keys()).map((page) => {
+            return (
+              <ButtonPagination
+                key={page + 1}
+                href={CustomLinkHelper({
+                  isSubDomainRouting,
+                  communityName: settings.communityName,
+                  communityType: settings.communityType,
+                  path: `/c/${currentChannel.channelName}/${
+                    currentChannel.pages! - page
+                  }`,
+                })}
+                label={String(currentChannel.pages! - page)}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
