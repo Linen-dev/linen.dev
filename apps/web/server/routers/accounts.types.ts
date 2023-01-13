0a01771b4dbb513a514d8698eace94ff0ed94eac
Emil Ajdyna <emil@ajdyna.com>
2023-01-13 13:29:01 +0100

    [apps/web] allow empty strings in the account validation

--- apps/web/server/routers/accounts.types.ts
+++ apps/web/server/routers/accounts.types.ts
@@ -3,15 +3,15 @@ import { AccountType } from '@linen/types';
 
 export const updateAccountSchema = z.object({
   description: z.string().optional(),
-  homeUrl: z.string().url().optional(),
-  docsUrl: z.string().url().optional(),
-  logoUrl: z.string().url().optional(),
-  logoSquareUrl: z.string().url().optional(),
+  homeUrl: z.string().url().or(z.literal('')).optional(),
+  docsUrl: z.string().url().or(z.literal('')).optional(),
+  logoUrl: z.string().url().or(z.literal('')).optional(),
+  logoSquareUrl: z.string().url().or(z.literal('')).optional(),
   redirectDomain: z.string().url().or(z.literal('')).optional(),
   brandColor: z.string().optional(),
   googleAnalyticsId: z.string().optional(),
   anonymizeUsers: z.boolean().optional(),
-  communityInviteUrl: z.string().url().optional(),
+  communityInviteUrl: z.string().url().or(z.literal('')).optional(),
   type: z.enum([AccountType.PRIVATE, AccountType.PUBLIC]).optional(),
 });
 export type updateAccountType = z.infer<typeof updateAccountSchema>;
