83eff7d8e757a3b3a4332e960f409116582b3a50
Sandro de Souza <sandesou@gmail.com>
2023-01-23 14:22:28 -0300

    chore(web): opt out from vercel image optimization

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,16 +6,17 @@
 
 const nextConfig = {
   images: {
-    domains: [
-      'a.slack-edge.com',
-      'avatars.slack-edge.com',
-      'cdn.discordapp.com',
-      'avatars.githubusercontent.com',
-      `linen-assets.s3.amazonaws.com`,
-      'linen-assets.s3.us-east-1.amazonaws.com',
-      `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
-      `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
-    ],
+    unoptimized: true,
+    // domains: [
+    //   'a.slack-edge.com',
+    //   'avatars.slack-edge.com',
+    //   'cdn.discordapp.com',
+    //   'avatars.githubusercontent.com',
+    //   `linen-assets.s3.amazonaws.com`,
+    //   'linen-assets.s3.us-east-1.amazonaws.com',
+    //   `${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`,
+    //   `${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com`,
+    // ],
   },
 };
 
