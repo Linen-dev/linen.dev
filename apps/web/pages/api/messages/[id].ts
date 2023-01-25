79aedd1d47fa84c2e1c51d43460ed99f6e76459e
Emil Ajdyna <emil@ajdyna.com>
2023-01-25 09:04:40 +0100

    [apps/web] add a DELETE action

--- apps/web/pages/api/messages/[id].ts
+++ apps/web/pages/api/messages/[id].ts
@@ -2,16 +2,17 @@ import { NextApiRequest, NextApiResponse } from 'next/types';
 import { findMessageById } from 'lib/messages';
 import serializeMessage from 'serializers/message';
 import PermissionsService from 'services/permissions';
+import prisma from 'client';
 
-async function get(request: NextApiRequest, response: NextApiResponse) {
+async function index(request: NextApiRequest, response: NextApiResponse) {
   const id = request.query.id as string;
 
   const message = await findMessageById({ id });
   if (!message) {
-    return response.status(404).end();
+    return response.status(404).json({});
   }
   if (!message.channel.accountId) {
-    return response.status(401).end();
+    return response.status(401).json({});
   }
 
   const permissions = await PermissionsService.get({
@@ -22,18 +23,73 @@ async function get(request: NextApiRequest, response: NextApiResponse) {
     },
   });
   if (!permissions.access) {
-    return response.status(401).end();
+    return response.status(401).json({});
   }
 
-  response.status(200).json(serializeMessage(message));
-  return response.end();
+  return response.status(200).json(serializeMessage(message));
+}
+
+async function destroy(request: NextApiRequest, response: NextApiResponse) {
+  const id = request.query.id as string;
+
+  const message = await findMessageById({ id });
+  if (!message) {
+    return response.status(404).json({});
+  }
+  if (!message.channel.accountId || !message.usersId) {
+    return response.status(401).json({});
+  }
+
+  const permissions = await PermissionsService.get({
+    request,
+    response,
+    params: {
+      communityId: message.channel.accountId,
+    },
+  });
+  if (!permissions.access) {
+    return response.status(401).json({});
+  }
+
+  if (permissions.user.usersId !== message.usersId || !permissions.manage) {
+    return response.status(401).json({});
+  }
+
+  await prisma.messages.delete({
+    where: {
+      id: message.id,
+    },
+  });
+
+  if (message.threadId) {
+    const thread = await prisma.threads.findFirst({
+      where: {
+        id: message.threadId,
+      },
+      include: {
+        messages: true,
+      },
+    });
+
+    if (thread && thread.messages.length === 0) {
+      await prisma.threads.delete({
+        where: {
+          id: thread.id,
+        },
+      });
+    }
+  }
+
+  return response.status(200).json({});
 }
 
 async function handler(request: NextApiRequest, response: NextApiResponse) {
   if (request.method === 'GET') {
-    return get(request, response);
+    return index(request, response);
+  } else if (request.method === 'DELETE') {
+    return destroy(request, response);
   }
-  return response.status(405).end();
+  return response.status(405).json({});
 }
 
 export default handler;
