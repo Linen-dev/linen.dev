b927ad1c40d07bfb0a9bf30f15df34dd092bd2f6
Emil Ajdyna <emil@ajdyna.com>
2023-01-27 13:38:23 +0100

    [apps/web] add a confirmation modal when user tries to remove the message

---
+++ packages/ui/src/ConfirmationModal/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '../Button';
+import Modal from '../Modal';
+import styles from './index.module.scss';
+
+interface Props {
+  open: boolean;
+  close(): void;
+  onConfirm(): void;
+  title?: string;
+  description?: string;
+  confirm?: string;
+  cancel?: string;
+}
+
+export default function ConfirmationModal({
+  open,
+  close,
+  onConfirm,
+  title,
+  description,
+  confirm,
+  cancel,
+}: Props) {
+  return (
+    <Modal open={open} close={close}>
+      <h3 className={styles.header}>{title || 'Confirmation'}</h3>
+      <p className={styles.description}>{description || 'Are you sure?'}</p>
+      <div className={styles.buttons}>
+        <Button color="blue" onClick={onConfirm} type="button">
+          {confirm || 'Confirm'}
+        </Button>
+        <Button color="gray" onClick={close} type="button">
+          {cancel || 'Cancel'}
+        </Button>
+      </div>
+    </Modal>
+  );
+}
