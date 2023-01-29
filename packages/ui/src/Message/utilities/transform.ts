9dc0475a02516ac5574023375a92d990396308ec
Emil Ajdyna <emil@ajdyna.com>
2023-01-29 18:15:46 +0100

    [apps/web] better auto json highlighting

--- packages/ui/src/Message/utilities/transform.ts
+++ packages/ui/src/Message/utilities/transform.ts
@@ -6,6 +6,15 @@ export default function transform(node: any, count = 1) {
       node.type = 'pre';
     }
   }
+  if (node.type === 'text') {
+    const value = node.value.trim();
+    if (value.startsWith('{') && value.endsWith('}')) {
+      try {
+        JSON.parse(value);
+        node.type = 'pre';
+      } catch (exception) {}
+    }
+  }
   if (node.children) {
     node.children = node.children.map((node: any, index: number) =>
       transform(node, count + index)
