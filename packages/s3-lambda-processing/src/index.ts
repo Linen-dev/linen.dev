47933c65456cbead2e3ae24807ce9b3c6a6c4813
Sandro de Souza <sandesou@gmail.com>
2023-02-01 10:20:21 +0000

    feat: s3 lambda handler for ses messages

---
+++ packages/s3-lambda-processing/src/index.ts
@@ -0,0 +1,66 @@
+'use strict';
+import AWS from 'aws-sdk';
+import { simpleParser, ParsedMail } from 'mailparser';
+import axios, { Axios } from 'axios';
+const s3 = new AWS.S3();
+const ssm = new AWS.SSM();
+const BUCKET = process.env.BUCKET!;
+const URL = process.env.URL!;
+const STAGE = process.env.STAGE!;
+
+let cacheInstance: Axios;
+async function buildAxios() {
+  if (cacheInstance) {
+    return cacheInstance;
+  }
+
+  const apiKey = await ssm
+    .getParameter({
+      Name: `/linen-dev/${STAGE}/INTERNAL_API_KEY`,
+      WithDecryption: true,
+    })
+    .promise();
+
+  cacheInstance = axios.create({
+    headers: { 'x-api-internal': apiKey.Parameter?.Value },
+  });
+
+  return cacheInstance;
+}
+
+module.exports.handler = async function (event, context, callback) {
+  console.log('Process email');
+  console.log(event.Records[0]);
+  const s3Object = event.Records[0].s3.object;
+  console.log('S3 Obj.:\n', JSON.stringify(s3Object, null, 2));
+
+  // Retrieve the email from your bucket
+  const req = {
+    Bucket: BUCKET,
+    Key: s3Object.key,
+  };
+
+  try {
+    const data = await s3.getObject(req).promise();
+    console.log('Raw email:\n' + data.Body);
+
+    const parsed = await new Promise<ParsedMail>((res, rej) => {
+      simpleParser(data.Body as any, (err, success) => {
+        if (err) rej(err);
+        res(success);
+      });
+    });
+    console.log('date:', parsed.date);
+    console.log('subject:', parsed.subject);
+    console.log('body:', parsed.text);
+    console.log('from:', parsed.from?.text);
+    console.log('attachments:', parsed.attachments);
+
+    const instance = await buildAxios();
+    await instance.post(URL, parsed);
+    callback(null, null);
+  } catch (err) {
+    console.log(err, err.stack);
+    callback(err);
+  }
+};
