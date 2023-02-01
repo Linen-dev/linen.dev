185e7864b8e8db106b08f9c70426d0082190360e
Emil Ajdyna <emil@ajdyna.com>
2023-02-01 20:01:56 +0100

    [apps/web] add a placeholder footer

---
+++ apps/web/components/Pages/Channel/Content/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://linen.dev">Powered by Linen</a>
+    </footer>
+  );
+}
