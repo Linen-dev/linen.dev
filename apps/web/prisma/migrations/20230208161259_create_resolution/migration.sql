025be7e139daf0a883b03888ffd4ceb915edcd97
cheeseblubber <kamleung911@gmail.com>
2023-02-08 11:17:06 -0500

    [app/web] fix migration

---
+++ apps/web/prisma/migrations/20230208161259_create_resolution/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "threads" ADD COLUMN     "messagesId" TEXT;
---
+++ apps/web/prisma/migrations/20230208161553_remove_accidental_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `messagesId` on the `threads` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "threads" DROP COLUMN "messagesId";
