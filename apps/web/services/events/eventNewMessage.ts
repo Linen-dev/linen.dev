9b20a53c798ed6129c8169d7e682a7f6008e6940
Sandro de Souza <sandesou@gmail.com>
2023-02-09 11:25:17 -0300

    chore: more logging

--- apps/web/services/events/eventNewMessage.ts
+++ apps/web/services/events/eventNewMessage.ts
@@ -5,6 +5,7 @@ import ThreadsServices from 'services/threads';
 import UserThreadStatusService from 'services/user-thread-status';
 import { eventNewMentions } from 'services/events/eventNewMentions';
 import { notificationListener } from 'services/notifications';
+import { stringify } from 'superjson';
 
 type MentionNode = {
   type: string;
@@ -61,5 +62,5 @@ export async function eventNewMessage({
   ];
 
   const result = await Promise.allSettled(promises);
-  console.log(JSON.stringify(result));
+  console.log(stringify(result));
 }
