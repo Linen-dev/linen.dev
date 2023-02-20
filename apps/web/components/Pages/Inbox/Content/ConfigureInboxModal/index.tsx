ba53ec1777c0d2948275005a0a62dc8b7555af41
Emil Ajdyna <emil@ajdyna.com>
2023-02-20 16:15:32 +0100

    [apps/web] change how channels are displayed in the inbox settings

--- apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
+++ apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Modal } from '@linen/ui';
+import { Modal, Toggle } from '@linen/ui';
 import { SerializedChannel } from '@linen/types';
+import { FiHash } from 'react-icons/fi';
+import styles from './index.module.scss';
 
 interface Props {
   open: boolean;
@@ -10,10 +12,19 @@ interface Props {
 
 export default function ConfigureInboxModal({ open, close, channels }: Props) {
   return (
-    <Modal open={open} close={close}>
-      {channels.map((channel) => {
-        return channel.channelName;
-      })}
+    <Modal open={open} close={close} size="lg">
+      <h2 className={styles.h2}>
+        <FiHash /> Channels
+      </h2>
+      <ul className={styles.toggles}>
+        {channels.map((channel) => {
+          return (
+            <li className={styles.toggle}>
+              <Toggle checked onChange={() => {}} /> {channel.channelName}
+            </li>
+          );
+        })}
+      </ul>
     </Modal>
   );
 }
