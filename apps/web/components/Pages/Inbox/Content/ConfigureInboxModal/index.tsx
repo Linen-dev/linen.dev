a769af1b9f6bb195962447ab0ca5448a9289c0d1
Emil Ajdyna <emil@ajdyna.com>
2023-02-21 11:50:31 +0100

    [apps/web] change configuration

--- apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
+++ apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Toggle } from '@linen/ui';
 import { SerializedChannel } from '@linen/types';
 import { FiHash } from 'react-icons/fi';
@@ -10,17 +10,56 @@ interface Props {
   channels: SerializedChannel[];
 }
 
+interface InboxConfig {
+  channels: InboxChannelConfig[];
+}
+
+interface InboxChannelConfig {
+  channel: SerializedChannel;
+  subscribed: boolean;
+}
+
 export default function ConfigureInboxModal({ open, close, channels }: Props) {
+  const [configuration, setConfiguration] = useState<InboxConfig>({
+    channels: channels.map((channel) => {
+      return {
+        channel,
+        subscribed: true,
+      };
+    }),
+  });
+
   return (
     <Modal open={open} close={close} size="lg">
-      <h2 className={styles.h2}>
+      <h2 className={styles.h2}>Inbox</h2>
+      <h3 className={styles.h3}>
         <FiHash /> Channels
-      </h2>
+      </h3>
       <ul className={styles.toggles}>
-        {channels.map((channel) => {
+        {configuration.channels.map((config: InboxChannelConfig) => {
+          const { channel, subscribed } = config;
           return (
             <li className={styles.toggle}>
-              <Toggle checked onChange={() => {}} /> {channel.channelName}
+              <Toggle
+                checked={subscribed}
+                onChange={() => {
+                  setConfiguration((configuration) => {
+                    return {
+                      ...configuration,
+                      channels: configuration.channels.map((config) => {
+                        if (config.channel.id === channel.id) {
+                          return {
+                            ...config,
+                            subscribed: !config.subscribed,
+                          };
+                        }
+                        return config;
+                      }),
+                    };
+                  });
+                }}
+              />{' '}
+              {channel.channelName}
             </li>
           );
         })}
