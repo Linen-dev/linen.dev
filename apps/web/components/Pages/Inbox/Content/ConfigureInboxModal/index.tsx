24ea0068593c50dc2118718da9930a301559bef8
Emil Ajdyna <emil@ajdyna.com>
2023-02-21 13:08:41 +0100

    [apps/web] move config to the parent

--- apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
+++ apps/web/components/Pages/Inbox/Content/ConfigureInboxModal/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Modal, Toggle } from '@linen/ui';
-import { SerializedChannel } from '@linen/types';
 import { FiHash } from 'react-icons/fi';
 import styles from './index.module.scss';
 import { InboxConfig, InboxChannelConfig } from '../../types';
@@ -8,19 +7,16 @@ import { InboxConfig, InboxChannelConfig } from '../../types';
 interface Props {
   open: boolean;
   close(): void;
-  channels: SerializedChannel[];
+  configuration: InboxConfig;
+  onChange(channelId: string): void;
 }
 
-export default function ConfigureInboxModal({ open, close, channels }: Props) {
-  const [configuration, setConfiguration] = useState<InboxConfig>({
-    channels: channels.map((channel) => {
-      return {
-        channel,
-        subscribed: true,
-      };
-    }),
-  });
-
+export default function ConfigureInboxModal({
+  open,
+  close,
+  configuration,
+  onChange,
+}: Props) {
   return (
     <Modal open={open} close={close} size="lg">
       <h2 className={styles.h2}>Inbox</h2>
@@ -34,22 +30,7 @@ export default function ConfigureInboxModal({ open, close, channels }: Props) {
             <li className={styles.toggle}>
               <Toggle
                 checked={subscribed}
-                onChange={() => {
-                  setConfiguration((configuration) => {
-                    return {
-                      ...configuration,
-                      channels: configuration.channels.map((config) => {
-                        if (config.channel.id === channel.id) {
-                          return {
-                            ...config,
-                            subscribed: !config.subscribed,
-                          };
-                        }
-                        return config;
-                      }),
-                    };
-                  });
-                }}
+                onChange={() => onChange(channel.id)}
               />{' '}
               {channel.channelName}
             </li>
