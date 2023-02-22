910ab33868be13247741c092e317ee4872cb2e54
Emil Ajdyna <emil@ajdyna.com>
2023-02-22 10:58:47 +0100

    [apps/web] remove the temporary, marked parser in favor of keeping the logic in the main parser

--- packages/ui/src/Message/Text/index.test.tsx
+++ packages/ui/src/Message/Text/index.test.tsx
@@ -3,59 +3,6 @@ import { render } from '@testing-library/react';
 import Text from '.';
 
 describe('Text', () => {
-  it('renders h1 headers', () => {
-    const { container } = render(<Text value="# foo" />);
-    const node = container.querySelector('h1');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders h2 headers', () => {
-    const { container } = render(<Text value="## foo" />);
-    const node = container.querySelector('h2');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders h3 headers', () => {
-    const { container } = render(<Text value="### foo" />);
-    const node = container.querySelector('h3');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders h4 headers', () => {
-    const { container } = render(<Text value="#### foo" />);
-    const node = container.querySelector('h4');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders h5 headers', () => {
-    const { container } = render(<Text value="##### foo" />);
-    const node = container.querySelector('h5');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders h6 headers', () => {
-    const { container } = render(<Text value="###### foo" />);
-    const node = container.querySelector('h6');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders blockquotes', () => {
-    const { container } = render(<Text value="> foo" />);
-    const node = container.querySelector('blockquote');
-    expect(node).toHaveTextContent('foo');
-  });
-
-  it('renders emojis', () => {
-    const { container } = render(<Text value="Hey :)" />);
-    expect(container).toHaveTextContent('😃');
-  });
-
-  it('renders bold text', () => {
-    const { container } = render(<Text value="*hey*" />);
-    expect(container.querySelector('strong')).toHaveTextContent('hey');
-    expect(container).not.toHaveTextContent('*hey*');
-  });
-
   it('escapes html entities', () => {
     const { container } = render(<Text value="# &lt;foo&gt;" />);
     expect(container).toHaveTextContent('<foo>');
