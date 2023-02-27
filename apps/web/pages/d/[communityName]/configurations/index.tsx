bd450dd4e64ae423d52641f6c2fba697cad5f47d
Sandro de Souza <sandesou@gmail.com>
2023-02-27 17:05:06 -0300

    refactor: remove dup pages (/d/... rewrite to /s/...)

--- apps/web/pages/d/[communityName]/configurations/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSidePropsContext } from 'next';
-import Configurations from 'components/Pages/Configurations';
-import { getSettingsServerSideProps } from 'services/settings';
-
-export default Configurations;
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  return getSettingsServerSideProps(context, false);
-}
--- apps/web/pages/d/[communityName]/members/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSidePropsContext } from 'next';
-import Members from 'components/Pages/Members';
-import { getMembersServerSideProps } from 'services/members';
-
-export default Members;
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  return getMembersServerSideProps(context, false);
-}
