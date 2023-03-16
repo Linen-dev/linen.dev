9b2c0b5b0769a4e012f7ef2bcf4ec44ce031905c
Emil Ajdyna <emil@ajdyna.com>
2023-03-16 12:25:00 +0100

    Improve preload by adding a local cache

--- packages/ui/src/Image/utilities/preload.ts
+++ packages/ui/src/Image/utilities/preload.ts
@@ -1,8 +1,15 @@
+const cache: { [key: string]: Promise<HTMLImageElement> } = {};
+
 export default async function preload(src: string): Promise<HTMLImageElement> {
-  return new Promise((resolve, reject) => {
+  if (cache.hasOwnProperty(src)) {
+    return cache[src];
+  }
+  const promise = new Promise((resolve, reject) => {
     const image = new Image();
     image.onload = () => resolve(image);
     image.onerror = () => reject(new Error(`Could not load image at ${src}`));
     image.src = src;
-  });
+  }) as Promise<HTMLImageElement>;
+  cache[src] = promise;
+  return promise;
 }
