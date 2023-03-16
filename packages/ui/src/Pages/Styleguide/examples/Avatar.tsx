daf527613541050aaf6ab07657b411dfbcac7bef
Emil Ajdyna <emil@ajdyna.com>
2023-03-16 11:57:42 +0100

    [apps/web] add intersection observer in avatars (as an experiment)

--- packages/ui/src/Pages/Styleguide/examples/Avatar.tsx
+++ packages/ui/src/Pages/Styleguide/examples/Avatar.tsx
@@ -5,10 +5,13 @@ import Avatar from '../../../Avatar';
 export default function AvatarExample() {
   return (
     <Example header="Avatar">
-      <Example description="Avatars can have text" inline>
+      <Example description="Avatars can have text." inline>
         {'abcdefghijklmnopqrstuvwxyz'.split('').map((letter) => (
           <Avatar text={letter} />
         ))}
+      </Example>
+      <Example description="Avatars can have images." inline>
+        <Avatar src="https://static.main.linendev.com/logos/logo05dab315-0b75-415b-aca4-f56a1867f045.png" />
       </Example>
     </Example>
   );
