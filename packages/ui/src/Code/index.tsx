a9802dd138a2e18bc74ab3f13a74c236b94174bb
Emil Ajdyna <emil@ajdyna.com>
2023-03-17 09:42:19 +0100

    [packages/ui] optimize highlight.js rendering by only rendering the content in view

--- packages/ui/src/Code/index.tsx
+++ packages/ui/src/Code/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import styles from './index.module.scss';
+import { useInView } from 'react-intersection-observer';
 
 interface Props {
   className?: string;
@@ -10,30 +11,40 @@ interface Props {
 }
 
 function Code({ className, content, highlight, inline }: Props): JSX.Element {
-  const [hljs, setHljs] = useState<any>(null);
+  const { ref, inView } = useInView({ threshold: 0, skip: !highlight });
+  const [highlighted, setHighlighted] = useState<any>();
 
   useEffect(() => {
-    import('highlight.js').then((hljs) => setHljs(hljs.default));
-  }, []);
+    let mounted = true;
+    if (!highlighted && inView) {
+      import('highlight.js').then((hljs: any) => {
+        if (mounted) {
+          setHighlighted(hljs.default.highlightAuto(content));
+        }
+      });
+    }
+    return () => {
+      mounted = false;
+    };
+  }, [highlighted, inView]);
 
-  if (!highlight || !hljs) {
+  if (highlighted && inView) {
     return (
-      <pre className={classNames({ [styles.inline]: inline })}>
-        <code className={className}>{content}</code>
+      <pre
+        ref={ref}
+        className={classNames('hljs', styles.pre, { [styles.inline]: inline })}
+      >
+        <code
+          className={styles.code}
+          dangerouslySetInnerHTML={{ __html: highlighted.value }}
+        />
       </pre>
     );
   }
 
-  const highlighted = hljs.highlightAuto(content);
-
   return (
-    <pre
-      className={classNames('hljs', styles.pre, { [styles.inline]: inline })}
-    >
-      <code
-        className={styles.code}
-        dangerouslySetInnerHTML={{ __html: highlighted.value }}
-      />
+    <pre ref={ref} className={classNames({ [styles.inline]: inline })}>
+      <code className={className}>{content}</code>
     </pre>
   );
 }
