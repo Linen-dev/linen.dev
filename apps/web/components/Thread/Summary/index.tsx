bc6e935a57a5b546eec9a1a74ce54f8aa4b0fa0b
Emil Ajdyna <emil@ajdyna.com>
2023-03-28 11:10:26 +0200

    [apps/web] minor ui fixes

--- apps/web/components/Thread/Summary/index.tsx
+++ apps/web/components/Thread/Summary/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { SerializedThread } from '@linen/types';
 import styles from './index.module.scss';
 // import { FiInfo } from '@react-icons/all-files/fi/FiInfo';
@@ -6,14 +7,17 @@ import styles from './index.module.scss';
 
 interface Props {
   thread: SerializedThread;
+  expanded?: boolean;
 }
 
-export default function Summary({ thread }: Props) {
+export default function Summary({ thread, expanded }: Props) {
   if (!thread.question || !thread.answer) {
     return null;
   }
   return (
-    <div className={styles.summary}>
+    <div
+      className={classNames(styles.summary, { [styles.expanded]: expanded })}
+    >
       <div className={styles.content}>
         <p className={styles.description}>Question</p>
         <h1>{thread.question}</h1>
