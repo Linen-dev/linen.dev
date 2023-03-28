3052e5eedd0d664591a1046dc00111f5be6e5578
Emil Ajdyna <emil@ajdyna.com>
2023-03-28 08:44:59 +0200

    [apps/web] move the summary into a separate component

---
+++ apps/web/components/Thread/Summary/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { SerializedThread } from '@linen/types';
+import styles from './index.module.scss';
+// import { FiInfo } from '@react-icons/all-files/fi/FiInfo';
+// import { Tooltip } from '@linen/ui';
+
+interface Props {
+  thread: SerializedThread;
+}
+
+export default function Summary({ thread }: Props) {
+  if (!thread.question || !thread.answer) {
+    return null;
+  }
+  return (
+    <div className={styles.summary}>
+      <div className={styles.content}>
+        <p className={styles.description}>Question</p>
+        <h1>{thread.question}</h1>
+      </div>
+
+      <div className={styles.answer}>
+        <div className={styles.header}>
+          <p className={styles.description}>Answer</p>
+          {/* <Tooltip className={styles.tooltip} text="Summarized by XYZ">
+            <FiInfo />
+          </Tooltip> */}
+        </div>
+        <p>{thread.answer}</p>
+      </div>
+    </div>
+  );
+}
