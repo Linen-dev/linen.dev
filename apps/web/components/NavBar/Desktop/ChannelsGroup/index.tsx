10edd19ba7eede514d8a3290d4c7ab826a7479c6
Emil Ajdyna <emil@ajdyna.com>
2023-03-29 09:26:38 +0200

    [apps/web] collapsable channels

--- apps/web/components/NavBar/Desktop/ChannelsGroup/index.tsx
+++ apps/web/components/NavBar/Desktop/ChannelsGroup/index.tsx
@@ -8,7 +8,8 @@ import { FiLock } from '@react-icons/all-files/fi/FiLock';
 import { FiPlus } from '@react-icons/all-files/fi/FiPlus';
 import Link from 'components/Link/InternalLink';
 import NewChannelModal from 'components/Modals/NewChannelModal';
-import styles from '../index.module.scss';
+import styles from './index.module.scss';
+import { FiChevronDown } from '@react-icons/all-files/fi/FiChevronDown';
 
 interface Props {
   channelName: string;
@@ -41,14 +42,29 @@ export default function ChannelsGroup({
   onChannelClick,
   onDrop,
 }: Props) {
+  const [show, toggle] = useState(true);
   const [modal, setModal] = useState(false);
   return (
     <>
-      <Nav.Group>
+      <Nav.Group
+        onClick={() => {
+          toggle((show) => !show);
+        }}
+      >
         Channels
         {currentUser && permissions.channel_create && !!permissions.accountId && (
           <>
-            <FiPlus className="cursor-pointer" onClick={() => setModal(true)} />
+            <div className={styles.flex}>
+              <FiPlus
+                className="cursor-pointer"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  setModal(true);
+                }}
+              />
+              <FiChevronDown />
+            </div>
+
             <NewChannelModal
               permissions={permissions}
               show={modal}
@@ -57,62 +73,64 @@ export default function ChannelsGroup({
           </>
         )}
       </Nav.Group>
-      <div>
-        {channels.map((channel: SerializedChannel, index: number) => {
-          const count = highlights.reduce((count: number, id: string) => {
-            if (id === channel.id) {
-              return count + 1;
+      {show && (
+        <>
+          {channels.map((channel: SerializedChannel, index: number) => {
+            const count = highlights.reduce((count: number, id: string) => {
+              if (id === channel.id) {
+                return count + 1;
+              }
+              return count;
+            }, 0);
+
+            function handleDrop(event: React.DragEvent) {
+              const id = channel.id;
+              const text = event.dataTransfer.getData('text');
+              const data = JSON.parse(text);
+              if (data.id === id) {
+                return event.stopPropagation();
+              }
+              return onDrop?.({
+                source: data.source,
+                target: 'channel',
+                from: data.id,
+                to: id,
+              });
             }
-            return count;
-          }, 0);
 
-          function handleDrop(event: React.DragEvent) {
-            const id = channel.id;
-            const text = event.dataTransfer.getData('text');
-            const data = JSON.parse(text);
-            if (data.id === id) {
-              return event.stopPropagation();
+            function handleDragEnter(event: React.DragEvent) {
+              event.currentTarget.classList.add(styles.drop);
             }
-            return onDrop?.({
-              source: data.source,
-              target: 'channel',
-              from: data.id,
-              to: id,
-            });
-          }
 
-          function handleDragEnter(event: React.DragEvent) {
-            event.currentTarget.classList.add(styles.drop);
-          }
+            function handleDragLeave(event: React.DragEvent) {
+              event.currentTarget.classList.remove(styles.drop);
+            }
 
-          function handleDragLeave(event: React.DragEvent) {
-            event.currentTarget.classList.remove(styles.drop);
-          }
+            const active = channel.channelName === channelName;
+            const highlighted = !active && count > 0;
 
-          const active = channel.channelName === channelName;
-          const highlighted = !active && count > 0;
+            const Icon = channel.type === 'PRIVATE' ? FiLock : FiHash;
 
-          const Icon = channel.type === 'PRIVATE' ? FiLock : FiHash;
-
-          return (
-            <Link
-              className={classNames(styles.item, {
-                [styles.dropzone]: mode === Mode.Drag,
-              })}
-              onDragEnter={handleDragEnter}
-              onDragLeave={handleDragLeave}
-              onDrop={handleDrop}
-              onClick={() => onChannelClick(channel.id)}
-              key={`${channel.channelName}-${index}`}
-              href={`/c/${channel.channelName}`}
-            >
-              <Nav.Item active={active} highlighted={highlighted}>
-                <Icon /> {channel.channelName}
-              </Nav.Item>
-            </Link>
-          );
-        })}
-      </div>
+            return (
+              <Link
+                className={classNames(styles.item, {
+                  [styles.dropzone]: mode === Mode.Drag,
+                })}
+                onDragEnter={handleDragEnter}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+                onClick={() => onChannelClick(channel.id)}
+                key={`${channel.channelName}-${index}`}
+                href={`/c/${channel.channelName}`}
+              >
+                <Nav.Item active={active} highlighted={highlighted}>
+                  <Icon /> {channel.channelName}
+                </Nav.Item>
+              </Link>
+            );
+          })}
+        </>
+      )}
     </>
   );
 }
