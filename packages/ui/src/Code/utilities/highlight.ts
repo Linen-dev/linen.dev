5dbb988a553cd9e8ae60c42823339b6dcf14fe72
Emil Ajdyna <emil@ajdyna.com>
2023-03-31 14:08:00 +0200

    [apps/web] optimize ui by calling an endpoint instead of highlighting client side

---
+++ packages/ui/src/Code/utilities/highlight.ts
@@ -0,0 +1,16 @@
+const cache: { [key: string]: Promise<string> } = {};
+
+export function highlightCode(input: string): Promise<string> {
+  if (!!cache[input]) {
+    return cache[input];
+  }
+  const promise = fetch('/api/highlight', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ input }),
+  })
+    .then((response) => response.json())
+    .then(({ output }) => output);
+  cache[input] = promise;
+  return promise;
+}
