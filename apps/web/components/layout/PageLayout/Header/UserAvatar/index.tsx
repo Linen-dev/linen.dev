4a851236b76114a370f55fa2eea24aef0926ef98
Emil Ajdyna <emil@ajdyna.com>
2023-04-07 10:31:23 +0200

    [apps/web] reuse dropdown in the user navigation

--- apps/web/components/layout/PageLayout/Header/UserAvatar/index.tsx
+++ apps/web/components/layout/PageLayout/Header/UserAvatar/index.tsx
@@ -1,12 +1,13 @@
-import React, { Fragment, useState } from 'react';
-import classNames from 'classnames';
-import { Menu, Transition } from '@headlessui/react';
+import React, { useState } from 'react';
+import Dropdown from '@linen/ui/Dropdown';
 import { signOut } from 'utilities/auth/react';
 import { SerializedUser } from '@linen/types';
 import Avatar from '@linen/ui/Avatar';
 import Modal from '@linen/ui/Modal';
 import ProfileForm from './ProfileForm';
 import { AxiosRequestConfig } from 'axios';
+import { FiUser } from '@react-icons/all-files/fi/FiUser';
+import { FiLogOut } from '@react-icons/all-files/fi/FiLogOut';
 
 interface Props {
   currentUser: SerializedUser;
@@ -26,13 +27,15 @@ export default function UserAvatar({
 }: Props) {
   const userNavigation = [
     {
-      name: 'Profile',
+      label: 'Profile',
+      icon: <FiUser />,
       onClick() {
         setMode(Mode.Profile);
       },
     },
     {
-      name: 'Sign out',
+      label: 'Sign out',
+      icon: <FiLogOut />,
       onClick() {
         signOut();
       },
@@ -41,49 +44,19 @@ export default function UserAvatar({
   const [mode, setMode] = useState(Mode.Menu);
   return (
     <>
-      <div className="flex h-16 justify-between">
-        <div className="flex items-center">
-          <Menu as="div" className="relative">
-            <div>
-              <Menu.Button className="flex items-center ">
-                <span className="sr-only">Open user menu</span>
-                <Avatar
-                  shadow="none"
-                  src={currentUser.profileImageUrl}
-                  text={currentUser.displayName}
-                />
-              </Menu.Button>
-            </div>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        onClick={item.onClick}
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer'
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    )}
-                  </Menu.Item>
-                ))}
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
-      </div>
+      <Dropdown
+        button={
+          <>
+            <span className="sr-only">Open user menu</span>
+            <Avatar
+              shadow="none"
+              src={currentUser.profileImageUrl}
+              text={currentUser.displayName}
+            />
+          </>
+        }
+        items={userNavigation}
+      />
       <Modal open={mode === Mode.Profile} close={() => setMode(Mode.Menu)}>
         <ProfileForm
           currentUser={currentUser}
