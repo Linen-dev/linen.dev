5ac983eddc3ee2fd295c1e1ece18f8cbf0d614c1
Sandro de Souza <sandesou@gmail.com>
2023-04-10 15:41:24 -0300

    fix: handle verify token exception

--- apps/web/server/middlewares/jwt.ts
+++ apps/web/server/middlewares/jwt.ts
@@ -2,15 +2,22 @@ import { AuthedRequest, Response, NextFunction } from 'server/types';
 import UsersService from 'services/users';
 import { getToken, verifyToken } from 'utilities/auth/server/tokens';
 import { Unauthorized } from 'server/exceptions';
+import to from 'utilities/await-to-js';
+import { expireSessionCookies } from 'utilities/auth/server/cookies';
 
 export default function jwtMiddleware(_?: never) {
-  return async (req: AuthedRequest, _: any, next: NextFunction) => {
+  return async (req: AuthedRequest, res: any, next: NextFunction) => {
     const token = getToken(req);
     if (!token) {
       return next(new Unauthorized());
     }
 
-    const session = await verifyToken(token);
+    const [err, session] = await to(verifyToken(token));
+
+    if (err) {
+      expireSessionCookies({ req, res });
+      return next(new Unauthorized());
+    }
 
     if (!session) {
       return next(new Unauthorized());
