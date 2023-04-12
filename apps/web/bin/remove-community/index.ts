f016c63cbf8c62d5dcc99dc3d815d6a95403b824
Sandro de Souza <sandesou@gmail.com>
2023-04-12 10:21:28 -0300

    fix: remove community logger for script

--- apps/web/bin/remove-community/index.ts
+++ apps/web/bin/remove-community/index.ts
@@ -1,8 +1,17 @@
 import { cleanUp } from 'queue/tasks/remove-community';
 import { config } from 'dotenv';
+import { Logger } from 'graphile-worker';
+
+function logFactory(scope: any) {
+  return (level: any, message: any, meta: any) => {
+    console.log(level, message, scope, meta);
+  };
+}
+
+const logger = new Logger(logFactory);
 
 config();
 
 console.log(process.env.DATABASE_URL);
 
-cleanUp('account-uuid', console.log).catch(console.error);
+cleanUp('account-uuid', logger).catch(console.error);
