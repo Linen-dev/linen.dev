6a03162705a9133a6941dd9fe029b27e6eb760ae
Emil Ajdyna <emil@ajdyna.com>
2023-04-12 08:53:15 +0200

    [apps/web] use an internal link

--- apps/web/components/layout/PageLayout/Header/UpgradeButton/index.tsx
+++ apps/web/components/layout/PageLayout/Header/UpgradeButton/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './index.module.scss';
 import { FiZap } from '@react-icons/all-files/fi/FiZap';
+import Link from 'components/Link/InternalLink';
 
 export default function UpgradeButton() {
   return (
-    <a className={styles.button} href="/plans">
+    <Link className={styles.button} href="/plans">
       <FiZap /> Upgrade
-    </a>
+    </Link>
   );
 }
