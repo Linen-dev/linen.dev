c1e733430855b3bb547dbdd584b8f3e80dbd8bb5
Sandro de Souza <sandesou@gmail.com>
2023-04-12 13:37:07 -0300

    Revert "debug: enable sentry debug"
    This reverts commit c0eeb84e71ee708cddd5c5a8c4363c485408fc92.

--- apps/web/utilities/ssr-metrics.ts
+++ apps/web/utilities/ssr-metrics.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: SENTRY_DNS,
   integrations: [new Sentry.Integrations.Http({ tracing: true })],
   tracesSampleRate: 1.0,
-  debug: true,
+  debug: process.env.NODE_ENV === 'development',
   enabled: !!SENTRY_DNS && process.env.NODE_ENV === 'production',
 });
 
