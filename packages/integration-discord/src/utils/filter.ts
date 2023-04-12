af800a71fb0617de468c95fa5f88ba3c0ee3d236
Sandro de Souza <sandesou@gmail.com>
2023-04-12 19:19:31 -0300

    discord: allow replies on-message-create event

--- packages/integration-discord/src/utils/filter.ts
+++ packages/integration-discord/src/utils/filter.ts
@@ -2,7 +2,7 @@ import type { Message, Channel } from 'discord.js';
 import { supportedChannelTypes } from './constrains';
 
 export function filterMessageType(message: Message) {
-  return ![0].includes(message.type);
+  return ![0, 19].includes(message.type);
 }
 export function filterSupportedChannel(channel: Channel) {
   return !supportedChannelTypes.includes(channel.type);
