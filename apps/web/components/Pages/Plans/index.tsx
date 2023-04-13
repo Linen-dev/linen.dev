6039f0aa278d82bd77b238f9165c08c0d8f81d32
Emil Ajdyna <emil@ajdyna.com>
2023-04-13 15:59:22 +0200

    Fetch plans

--- apps/web/components/Pages/Plans/index.tsx
+++ apps/web/components/Pages/Plans/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageLayout from 'components/layout/PageLayout';
 import Tiers from 'components/Pages/Tiers';
 import {
@@ -9,6 +9,7 @@ import {
 } from '@linen/types';
 import { FiZap } from '@react-icons/all-files/fi/FiZap';
 import styles from './index.module.scss';
+import Spinner from '@linen/ui/Spinner';
 
 export interface Props {
   channels: SerializedChannel[];
@@ -21,8 +22,8 @@ export interface Props {
 }
 
 export enum Period {
-  Monthly,
-  Yearly,
+  Monthly = 'monthly',
+  Yearly = 'yearly',
 }
 
 export default function PlansPage({
@@ -34,72 +35,25 @@ export default function PlansPage({
   isSubDomainRouting,
   dms,
 }: Props) {
-  const tiers = [
-    {
-      name: 'Veni',
-      href: '#',
-      description:
-        'The essentials to see Linen on your own domain with custom branding.',
-      features: [
-        'Up to 10,000 members',
-        'Custom domain',
-        'Custom branding',
-        'SEO benefits',
-        'Private communities',
-        'Analytics',
-        'Support',
-      ],
-      prices: { [Period.Monthly]: 200, [Period.Yearly]: 2000 },
-    },
-    {
-      name: 'Vidi',
-      href: '#',
-      description:
-        'The essentials to see Linen on your own domain with custom branding.',
-      features: [
-        'Up to 15,000 members',
-        'Custom domain',
-        'Custom branding',
-        'SEO benefits',
-        'Private communities',
-        'Analytics',
-        'Support',
-      ],
-      prices: { [Period.Monthly]: 250, [Period.Yearly]: 2500 },
-    },
-    {
-      name: 'Vici',
-      href: '#',
-      description:
-        'Recommended plan that supports the ongoing growth of the platform.',
-      features: [
-        'Up to 20,000 members',
-        'Custom domain',
-        'Custom branding',
-        'SEO benefits',
-        'Private communities',
-        'Analytics',
-        'Priority support',
-      ],
-      prices: { [Period.Monthly]: 300, [Period.Yearly]: 3000 },
-      active: true,
-    },
-    {
-      name: 'Enterprise',
-      href: '#',
-      description: 'Dedicated support and infrastructure for your community.',
-      features: [
-        'Unlimited members',
-        'Custom domain',
-        'Custom branding',
-        'SEO benefits',
-        'Private communities',
-        'Analytics',
-        'Priority support',
-      ],
-      prices: { [Period.Monthly]: 'Custom', [Period.Yearly]: 'Custom' },
-    },
-  ];
+  const [loading, setLoading] = useState(true);
+  const [tiers, setTiers] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    fetch('/api/plans', {
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.plans) {
+          setTiers(data.plans);
+          setLoading(false);
+        }
+      });
+    return () => {
+      mounted = false;
+    };
+  }, []);
 
   return (
     <PageLayout
@@ -122,11 +76,15 @@ export default function PlansPage({
           Choose an affordable plan that matches your community size and unlock
           additional features of the platform.
         </p>
-        <Tiers
-          activePeriod={Period.Monthly}
-          tiers={tiers}
-          account={currentCommunity}
-        />
+        {loading ? (
+          <Spinner className={styles.loader} />
+        ) : (
+          <Tiers
+            activePeriod={Period.Monthly}
+            tiers={tiers}
+            currentCommunity={currentCommunity}
+          />
+        )}
       </div>
     </PageLayout>
   );
--- apps/web/components/Pages/Tiers/index.tsx
+++ apps/web/components/Pages/Tiers/index.tsx
@@ -8,10 +8,12 @@ import Card from './Card';
 import styles from './index.module.scss';
 
 interface Tier {
+  id: string;
   name: string;
   description: string;
   href: string;
   features: string[];
+  priceId: string;
   prices: { [key: string]: number };
   active?: boolean;
 }
@@ -19,59 +21,104 @@ interface Tier {
 interface Props {
   tiers: Tier[];
   activePeriod: Period;
-  account?: SerializedAccount;
+  currentCommunity: SerializedAccount;
 }
 
-export default function Tiers({ tiers, activePeriod, account }: Props) {
+const ENTERPRISE_PLAN = {
+  name: 'Enterprise',
+  description: 'Dedicated support and infrastructure for your community.',
+  features: [
+    'Unlimited members',
+    'Custom domain',
+    'Custom branding',
+    'SEO benefits',
+    'Private communities',
+    'Analytics',
+    'Priority Support',
+  ],
+  prices: { monthly: 'Custom', yearly: 'Custom' },
+};
+
+export default function Tiers({
+  currentCommunity,
+  tiers,
+  activePeriod,
+}: Props) {
   return (
     <div className={styles.grid}>
-      {tiers.map((tier) => (
-        <Card
-          key={tier.name}
-          title={tier.name}
-          description={tier.description}
-          price={tier.prices[activePeriod]}
-          period={activePeriod}
-          active={tier.active}
-        >
-          <div>
-            {!account && tier.prices ? (
-              <a
-                className="shadow-sm mt-8 block w-full bg-indigo-500 border border-indigo-500 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-indigo-600"
-                href="mailto:help@linen.dev?subject=Linen%20Premium"
-              >
-                Contact Us
-              </a>
-            ) : !!account && !account.premium && tier.prices ? (
-              <a
-                className="shadow-sm mt-8 block w-full bg-indigo-500 border border-indigo-500 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-indigo-600"
-                href="mailto:help@linen.dev?subject=Linen%20Premium"
-              >
-                Contact Us
-              </a>
-            ) : (
-              <a
+      {tiers.map((tier) => {
+        const active = tier.name === 'Vici';
+        return (
+          <Card
+            key={tier.name}
+            title={tier.name}
+            description={tier.description}
+            price={tier.prices[activePeriod]}
+            period={activePeriod}
+            active={active}
+          >
+            <form method="POST" action="/api/plans">
+              <input
+                type="hidden"
+                name="communityId"
+                value={currentCommunity.id}
+              />
+              <input type="hidden" name="priceId" value={tier.priceId} />
+              <input
+                type="hidden"
+                name="successUrl"
+                value={window && window.location.href}
+              />
+              <input
+                type="hidden"
+                name="cancelUrl"
+                value={window && window.location.href}
+              />
+              <button
+                type="submit"
                 className={classNames(styles.button, {
-                  [styles.active]: tier.active,
+                  [styles.active]: active,
                 })}
               >
-                {tier.active && <FiZap />}
+                {active && <FiZap />}
                 Buy plan
-              </a>
-            )}
-          </div>
-          <div>
-            <ul className={styles.list}>
-              {tier.features.map((feature) => (
-                <li>
-                  <FiCheck />
-                  {feature}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </Card>
-      ))}
+              </button>
+            </form>
+            <div>
+              <ul className={styles.list}>
+                {tier.features.map((feature) => (
+                  <li>
+                    <FiCheck />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Card>
+        );
+      })}
+      <Card
+        title={ENTERPRISE_PLAN.name}
+        description={ENTERPRISE_PLAN.description}
+        price="Custom"
+      >
+        <a
+          href="mailto:support@linen.dev?subject=Linen Enterprise"
+          className={styles.button}
+        >
+          Contact us
+        </a>
+        <div>
+          <ul className={styles.list}>
+            {ENTERPRISE_PLAN.features.map((feature) => (
+              <li>
+                <FiCheck />
+                {feature}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Card>
     </div>
   );
 }
