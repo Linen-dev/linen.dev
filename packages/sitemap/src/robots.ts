135d4253f5c52b70b17d874e2d9d422bd2cb3083
Sandro de Souza <sandesou@gmail.com>
2023-04-19 12:56:57 -0300

    seo: disallow crawl to specific pages

--- packages/sitemap/src/robots.ts
+++ packages/sitemap/src/robots.ts
@@ -5,6 +5,28 @@ function createRobotsTxt(domain: string) {
 User-agent: *
 Allow: /
 
+# metrics
+Disallow: /ph
+Disallow: /pp
+
+# user only pages
+Disallow: /api
+Disallow: /all
+Disallow: /branding
+Disallow: /configurations
+Disallow: /inbox
+Disallow: /members
+Disallow: /metrics
+Disallow: /plans
+Disallow: /starred
+
+# Block files ending in .json, _buildManifest.js, _middlewareManifest.js, _ssgManifest.js, and any other JS files
+Disallow: /*.json$
+Disallow: /*_buildManifest.js$
+Disallow: /*_middlewareManifest.js$
+Disallow: /*_ssgManifest.js$
+Disallow: /*.js$
+
 Sitemap: https://${domain}/sitemap.xml
 `.trim();
 }
