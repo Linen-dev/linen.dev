3070e5a9d07ab3019e2fa76990e8cddedd8c3c98
Sandro de Souza <sandesou@gmail.com>
2023-04-24 13:05:40 -0300

    debug 504 middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -7,17 +7,44 @@ export const config = {
 
 export default function middleware(request: NextRequest) {
   try {
+    console.time('middleware');
+    const timeout25 = globalThis.setTimeout(() => {
+      console.log('25 seconds, middleware is about to timeout');
+    }, 25000);
+
+    const timeout10 = globalThis.setTimeout(() => {
+      console.log('10 seconds, middleware debug');
+    }, 10000);
+
+    const timeout1 = globalThis.setTimeout(() => {
+      console.log('1 seconds, middleware debug');
+    }, 1000);
+
     const url = request.nextUrl.clone(); // clone the request url
     const { pathname } = request.nextUrl; // get pathname of request (e.g. /blog-slug)
     const hostname = request.headers.get('host'); // get hostname of request (e.g. demo.vercel.pub)
+    console.log('hostname', hostname);
 
     const res = rewrite({ hostname, pathname, url });
+    console.log('rewrite...fn');
+    console.timeLog('middleware');
 
     if (res && !!res.rewrite) {
+      console.log('rewrite...ok');
+      globalThis.clearTimeout(timeout25);
+      globalThis.clearTimeout(timeout10);
+      globalThis.clearTimeout(timeout1);
+      console.timeEnd('middleware');
       return NextResponse.rewrite(res.rewrite);
     }
+    console.log('rewrite...skip');
+    globalThis.clearTimeout(timeout25);
+    globalThis.clearTimeout(timeout10);
+    globalThis.clearTimeout(timeout1);
+    console.timeEnd('middleware');
   } catch (error) {
-    console.error(error);
+    console.log(error);
+    console.timeEnd('middleware');
     throw error;
   }
 }
