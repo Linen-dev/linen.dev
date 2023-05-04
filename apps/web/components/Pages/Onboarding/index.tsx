95cd87859890a368856dfe565481f497d0b78745
Emil Ajdyna <emil@ajdyna.com>
2023-05-04 10:35:10 +0200

    [apps/web] change the layout of the onboarding page

--- apps/web/components/Pages/Onboarding/index.tsx
+++ apps/web/components/Pages/Onboarding/index.tsx
@@ -1,11 +1,18 @@
-import Layout from 'components/layout/CardLayout';
+import Layout from 'components/layout/SplitLayout';
 import Form from './Form';
 import { createAccount } from 'utilities/requests';
+import styles from './index.module.scss';
 
 export default function OnboardingPage() {
   return (
-    <Layout header="Create New Community">
-      <Form createAccount={createAccount} />
-    </Layout>
+    <Layout
+      left={
+        <div className={styles.container}>
+          <h1 className="text-xl font-bold mb-4">Create New Community</h1>
+          <Form createAccount={createAccount} />
+        </div>
+      }
+      right={<></>}
+    ></Layout>
   );
 }
