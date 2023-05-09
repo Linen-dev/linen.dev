9ae7b81d11a6b283c82cfbad3d9917f07ba42a7b
Emil Ajdyna <emil@ajdyna.com>
2023-05-09 18:51:44 +0200

    [apps/web] dark more friendly landing page

--- apps/web/components/Footer/index.tsx
+++ apps/web/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-white">
+    <footer>
       <div className="max-w-7xl mx-auto pt-16 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
@@ -20,7 +20,7 @@ export default function Footer() {
             <div key={item.name} className="px-5 py-2">
               <a
                 href={item.href}
-                className="text-base text-gray-500 hover:text-gray-900"
+                className="text-base text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -29,7 +29,7 @@ export default function Footer() {
             </div>
           ))}
         </nav>
-        <p className="mt-8 text-center text-base text-gray-500">
+        <p className="mt-8 text-center text-base text-gray-500 dark:text-gray-400">
           &copy; 2023 Linen. All rights reserved.
         </p>
       </div>
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -138,7 +138,7 @@ const Home = ({ accounts }: Props) => {
           </a>
           <div className="flex items-center justify-end md:flex-1 lg:w-0">
             <Link legacyBehavior href="/signin" passHref>
-              <a className="mr-4 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 ">
+              <a className="mr-4 whitespace-nowrap text-base font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
                 Sign in
               </a>
             </Link>
@@ -155,14 +155,14 @@ const Home = ({ accounts }: Props) => {
 
       <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div className="sm:text-center">
-          <h1 className="flex flex-col text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
+          <h1 className="flex flex-col text-3xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-4xl md:text-5xl">
             <div className="space-x-2 content-center">
               <span>Google-searchable</span>
               <span className="text-blue-600 xl:inline">Slack alternative</span>
             </div>
             <div className="block">for Communities</div>
           </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl ">
+          <p className="mt-3 text-base text-gray-500 dark:text-gray-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl ">
             Linen is a real-time chat platform built for communities. We are SEO
             friendly while providing a modern chat experience.
           </p>
@@ -208,10 +208,10 @@ const Home = ({ accounts }: Props) => {
               <h2 className="text-base font-semibold leading-7 text-blue-600">
                 Open community
               </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
                 Benefits of a forum and a real-time chat
               </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
+              <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 ">
                 Information gets lost in real-time chat. Linen solves this by
                 letting Google find your content. Our advanced threading model
                 let you drag and drop messages and threads to reorganize your
@@ -222,14 +222,14 @@ const Home = ({ accounts }: Props) => {
               <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
                 {features.map((feature) => (
                   <div key={feature.name} className="flex flex-col">
-                    <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                    <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900 dark:text-gray-100">
                       <feature.icon
                         className="h-5 w-5 flex-none text-blue-600"
                         aria-hidden="true"
                       />
                       {feature.name}
                     </dt>
-                    <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                    <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600 dark:text-gray-300">
                       <p className="flex-auto">{feature.description}</p>
                       {/* <p className="mt-6">
                         <a
@@ -247,7 +247,7 @@ const Home = ({ accounts }: Props) => {
           </div>
         </div>
 
-        <div className="overflow-hidden bg-white py-24 sm:py-32">
+        <div className="overflow-hidden py-24 sm:py-32">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
               <div className="lg:pr-8 lg:pt-4">
@@ -255,17 +255,17 @@ const Home = ({ accounts }: Props) => {
                   <h2 className="text-base font-semibold leading-7 text-blue-600">
                     Existing communities
                   </h2>
-                  <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
                     Already have a community?
                   </p>
-                  <p className="mt-6 text-lg leading-8 text-gray-600">
+                  <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 ">
                     Linen suports two way sync between Slack and Discord so you
                     can gradually transition your community to Linen.
                   </p>
-                  <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                  <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 dark:text-gray-300 lg:max-w-none">
                     {existingCommunityFeatures.map((feature) => (
                       <div key={feature.name} className="relative pl-9">
-                        <dt className="inline font-semibold text-gray-900">
+                        <dt className="inline font-semibold text-gray-900 dark:text-gray-100">
                           <feature.icon
                             className="absolute left-1 top-1 h-5 w-5 text-blue-600"
                             aria-hidden="true"
@@ -293,7 +293,7 @@ const Home = ({ accounts }: Props) => {
           <h2 className="text-base font-semibold leading-7 text-blue-600">
             Browse
           </h2>
-          <h1 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-4xl">
+          <h1 className="text-2xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-5xl md:text-4xl">
             Find Communities
           </h1>
         </div>
@@ -327,7 +327,7 @@ const Home = ({ accounts }: Props) => {
           </div>
         </div>
 
-        <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="relative isolate px-6 py-24 sm:py-32 lg:px-8">
           <div
             className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
             aria-hidden="true"
@@ -344,11 +344,11 @@ const Home = ({ accounts }: Props) => {
             <h2 className="text-base font-semibold leading-7 text-blue-600">
               Pricing
             </h2>
-            <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
               Upgrade for branded community
             </p>
           </div>
-          <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+          <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-gray-300">
             Hosting your own branded community on your website can boost your
             online presence through SEO and ease the load on your support team
             by encouraging members to find answers on Google.{' '}
@@ -359,12 +359,12 @@ const Home = ({ accounts }: Props) => {
                 key={tier.id}
                 className={classNames(
                   tier.featured
-                    ? 'relative bg-gray-900 shadow-2xl'
-                    : 'bg-white/60 sm:mx-8 lg:mx-0',
+                    ? 'relative bg-gray-900 dark:bg-black shadow-2xl'
+                    : 'bg-white sm:mx-8 lg:mx-0',
                   tier.featured
                     ? ''
                     : tierIdx === 0
-                    ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
+                    ? 'bg-white rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
                     : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
                   'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10'
                 )}
