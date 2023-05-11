db52b2f3c302d4a74d947111edc73c991f930347
cheeseblubber <kamleung911@gmail.com>
2023-05-11 09:58:59 -0400

    redirect for private-community

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -7,8 +7,15 @@ export const config = {
 
 export default function middleware(request: NextRequest) {
   const url = request.nextUrl.clone(); // clone the request url
+
   const { pathname } = request.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = request.headers.get('host'); // get hostname of request (e.g. demo.vercel.pub)
+
+  // Testing for a custom landing page
+  if (hostname?.includes('linen.community')) {
+    url.pathname = `/private-community`;
+    return NextResponse.rewrite(url.toString());
+  }
 
   const res = rewrite({ hostname, pathname, url });
 
