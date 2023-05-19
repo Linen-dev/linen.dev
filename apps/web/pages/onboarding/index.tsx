ce704aee3a696571c93245c76d8503d22b5485be
Emil Ajdyna <emil@ajdyna.com>
2023-05-19 13:48:15 +0200

    Migrate the onboarding page

--- apps/web/pages/onboarding/index.tsx
+++ apps/web/pages/onboarding/index.tsx
@@ -1,7 +1,10 @@
-import OnboardingPage from 'components/Pages/Onboarding';
+import React from 'react';
+import Pages from '@linen/ui/Pages';
 import { NextPageContext } from 'next';
 import { trackPageView } from 'utilities/ssr-metrics';
 import { api } from 'utilities/requests';
+
+const { Onboarding: OnboardingPage } = Pages;
 
 export default function Onboarding() {
   return <OnboardingPage api={api} />;
--- packages/ui/src/Pages/index.tsx
+++ packages/ui/src/Pages/index.tsx
@@ -2,5 +2,6 @@ import Inbox from './Inbox';
 import Starred from './Starred';
 import All from './All';
 import Styleguide from './Styleguide';
+import Onboarding from './Onboarding';
 
-export default { Inbox, Starred, All, Styleguide };
+export default { Inbox, Starred, All, Onboarding, Styleguide };
