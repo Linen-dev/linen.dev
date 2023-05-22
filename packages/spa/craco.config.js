8d1b111e1f34d082d0747ac9453da6bed04504c7
Sandro de Souza <sandesou@gmail.com>
2023-05-22 13:52:50 -0300

    chore(spa): add bundle analyzer

--- packages/spa/craco.config.js
+++ packages/spa/craco.config.js
@@ -1,9 +1,16 @@
 const path = require(`path`);
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    plugins: [
+      ...(process.env.ANALYZE === 'true'
+        ? [new BundleAnalyzerPlugin({ analyzerMode: 'static' })]
+        : []),
+    ],
   },
 };
--- packages/spa/src/Router.tsx
+++ packages/spa/src/Router.tsx
@@ -1,22 +1,20 @@
-import { Suspense, lazy } from 'react';
 import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import ErrorPage from '@/pages/Error';
 import SignIn from '@/pages/SignIn';
 import RequireAuth from '@/hoc/RequireAuth';
 import RedirectToLastPageViewed from '@/components/RedirectToLastPageViewed';
-import Loading from '@/components/Loading';
 import InProgress from '@/components/InProgress';
 import RedirectToS from '@/components/RedirectToS';
 import RequireManagerAuth from '@/hoc/RequireManagerAuth';
-const InboxPage = lazy(() => import('@/pages/Inbox'));
-const StarredPage = lazy(() => import('@/pages/Starred'));
-const AllPage = lazy(() => import('@/pages/All'));
-const ChannelPage = lazy(() => import('@/pages/Channel'));
-const ThreadPage = lazy(() => import('@/pages/Thread'));
-const BrandingPage = lazy(() => import('@/pages/Branding'));
-const ConfigurationsPage = lazy(() => import('@/pages/Configurations'));
-const MembersPage = lazy(() => import('@/pages/Members'));
+import InboxPage from '@/pages/Inbox';
+import StarredPage from '@/pages/Starred';
+import AllPage from '@/pages/All';
+import ChannelPage from '@/pages/Channel';
+import ThreadPage from '@/pages/Thread';
+import BrandingPage from '@/pages/Branding';
+import ConfigurationsPage from '@/pages/Configurations';
+import MembersPage from '@/pages/Members';
 
 export default function Router() {
   const isCustomDomain = false;
@@ -54,94 +52,50 @@ export default function Router() {
                       children: [
                         {
                           path: 'c/:channelName',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <ChannelPage />
-                            </Suspense>
-                          ),
+                          element: <ChannelPage />,
                         },
                         {
                           path: 'c/:channelName/:page',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <ChannelPage />
-                            </Suspense>
-                          ),
+                          element: <ChannelPage />,
                         },
                         {
                           path: 't/:threadId',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <ThreadPage />
-                            </Suspense>
-                          ),
+                          element: <ThreadPage />,
                         },
                         {
                           path: 't/:threadId/:slug',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <ThreadPage />
-                            </Suspense>
-                          ),
+                          element: <ThreadPage />,
                         },
                         {
                           path: 'inbox',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <InboxPage />
-                            </Suspense>
-                          ),
+                          element: <InboxPage />,
                         },
                         {
                           path: 'starred',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <StarredPage />
-                            </Suspense>
-                          ),
+                          element: <StarredPage />,
                         },
                         {
                           path: 'all',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <AllPage />
-                            </Suspense>
-                          ),
+                          element: <AllPage />,
                         },
                         {
                           path: 'branding',
                           element: (
-                            <RequireManagerAuth
-                              children={
-                                <Suspense fallback={<Loading />}>
-                                  <BrandingPage />
-                                </Suspense>
-                              }
-                            />
+                            <RequireManagerAuth children={<BrandingPage />} />
                           ),
                         },
                         {
                           path: 'configurations',
                           element: (
                             <RequireManagerAuth
-                              children={
-                                <Suspense fallback={<Loading />}>
-                                  <ConfigurationsPage />
-                                </Suspense>
-                              }
+                              children={<ConfigurationsPage />}
                             />
                           ),
                         },
                         {
                           path: 'members',
                           element: (
-                            <RequireManagerAuth
-                              children={
-                                <Suspense fallback={<Loading />}>
-                                  <MembersPage />
-                                </Suspense>
-                              }
-                            />
+                            <RequireManagerAuth children={<MembersPage />} />
                           ),
                         },
                         {
@@ -154,11 +108,7 @@ export default function Router() {
                         },
                         {
                           path: '',
-                          element: (
-                            <Suspense fallback={<Loading />}>
-                              <ChannelPage />
-                            </Suspense>
-                          ),
+                          element: <ChannelPage />,
                         },
                       ],
                     },
