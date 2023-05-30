8d8bb541c2a9a8468fd68a77f06e7b3686763d24
Emil Ajdyna <emil@ajdyna.com>
2023-05-30 07:51:59 +0200

    Replace rollup-plugin-postcss with rollup-plugin-postcss-modules

--- packages/ui/rollup.config.mjs
+++ packages/ui/rollup.config.mjs
@@ -1,24 +1,26 @@
-const resolve = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('@rollup/plugin-typescript');
-const postcss = require('rollup-plugin-postcss');
-const external = require('rollup-plugin-peer-deps-external');
-const { visualizer } = require('rollup-plugin-visualizer');
-const path = require('path');
-const glob = require('glob');
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss-modules';
+import external from 'rollup-plugin-peer-deps-external';
+import { visualizer } from 'rollup-plugin-visualizer';
+import glob from 'glob';
+import { fileURLToPath } from 'node:url';
+
+const search = fileURLToPath(new URL('src/**/*.tsx', import.meta.url));
 
 const input = glob
-  .sync(path.join(__dirname, 'src/**/*.tsx'))
+  .sync(search)
   .filter((file) => !file.includes('.test.') && !file.includes('.spec.'));
 
-module.exports = {
+export default {
   input,
   output: {
     dir: 'dist',
     format: 'cjs',
     entryFileNames(chunk) {
       const file = chunk.facadeModuleId
-        .replace(path.join(__dirname, 'src/'), '')
+        .replace(fileURLToPath(new URL('src/', import.meta.url)), '')
         .replace('index.tsx', '[name].js');
       return file;
     },
