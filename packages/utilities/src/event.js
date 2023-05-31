0a6e12b25624ed2a988cacd5dd5c14823c017cdc
Emil Ajdyna <emil@ajdyna.com>
2023-05-31 08:56:09 +0200

    Emit a mention event through the event bus

--- packages/utilities/src/event.js
+++ packages/utilities/src/event.js
@@ -1,17 +1,25 @@
 const EventEmitter = {
   events: {},
-  emit (name, data) {
-    if (!this.events[name]) { return }
-    this.events[name].forEach(callback => callback(data))
+  emit(name, data) {
+    if (!this.events[name]) {
+      return;
+    }
+    this.events[name].forEach((callback) => callback(data));
   },
-  on (name, callback) {
-    if (!this.events[name]) { this.events[name] = [] }
-    this.events[name].push(callback)
+  on(name, callback) {
+    if (!this.events[name]) {
+      this.events[name] = [];
+    }
+    this.events[name].push(callback);
   },
-  off (name, callback) {
-    if (!this.events[name]) { return }
-    this.events[name] = this.events[name].filter(handler => handler !== callback)
-  }
-}
+  off(name, callback) {
+    if (!this.events[name]) {
+      return;
+    }
+    this.events[name] = this.events[name].filter(
+      (handler) => handler !== callback
+    );
+  },
+};
 
-export default EventEmitter
+export default EventEmitter;
