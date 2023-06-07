6cb629790fecdd14725255a0baad5039be0b5119
cheeseblubber <kamleung911@gmail.com>
2023-06-07 10:45:10 -0400

    Add Starter plan

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -41,8 +41,29 @@ const Home = ({ accounts }: Props) => {
         'Hosted under Linen.dev',
         'Community support',
         'Import Slack and Discord conversations',
+        'Unlimited members',
       ],
       featured: false,
+      mostPopular: false,
+    },
+    {
+      name: 'Starter',
+      id: 'tier-starter',
+      href: '#',
+      priceMonthly: '$10',
+      description:
+        'Up 100 members with increase of $5/month per 100. Perfect for those who want a small or private community',
+      features: [
+        'Google Indexable',
+        'Private communities',
+        'Unlimited history retention',
+        'Branded commmunity',
+        'Private channels',
+        'Custom domain',
+        'Sitemap generation',
+      ],
+      featured: false,
+      mostPopular: true,
     },
     {
       name: 'Business',
@@ -60,6 +81,7 @@ const Home = ({ accounts }: Props) => {
         'Sitemap generation',
       ],
       featured: true,
+      mostPopular: false,
     },
   ];
   const features = [
@@ -326,118 +348,96 @@ const Home = ({ accounts }: Props) => {
             </button>
           </div>
         </div>
-
         <div className="relative isolate px-6 py-24 sm:py-32 lg:px-8">
-          <div
-            className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
-            aria-hidden="true"
-          >
-            <div
-              className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            />
-          </div>
-          <div className="mx-auto max-w-2xl text-center lg:max-w-4xl">
-            <h2 className="text-base font-semibold leading-7 text-blue-600">
-              Pricing
-            </h2>
-            <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
-              Upgrade for branded community
-            </p>
-          </div>
-          <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-gray-300">
-            Hosting your own branded community on your website can boost your
-            online presence through SEO and ease the load on your support team
-            by encouraging members to find answers on Google.{' '}
-          </p>
-          <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-            {tiers.map((tier, tierIdx) => (
-              <div
-                key={tier.id}
-                className={classNames(
-                  tier.featured
-                    ? 'relative bg-gray-900 dark:bg-black shadow-2xl'
-                    : 'bg-white sm:mx-8 lg:mx-0',
-                  tier.featured
-                    ? ''
-                    : tierIdx === 0
-                    ? 'bg-white rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
-                    : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
-                  'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10'
-                )}
-              >
-                <h3
-                  id={tier.id}
-                  className={classNames(
-                    tier.featured ? 'text-blue-400' : 'text-blue-600',
-                    'text-base font-semibold leading-7'
-                  )}
-                >
-                  {tier.name}
-                </h3>
-                <p className="mt-4 flex items-baseline gap-x-2">
-                  <span
-                    className={classNames(
-                      tier.featured ? 'text-white' : 'text-gray-900',
-                      'text-5xl font-bold tracking-tight'
-                    )}
-                  >
-                    {tier.priceMonthly}
-                  </span>
-                  <span
-                    className={classNames(
-                      tier.featured ? 'text-gray-400' : 'text-gray-500',
-                      'text-base'
-                    )}
-                  >
-                    /month
-                  </span>
+          <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-4xl text-center">
+                <h2 className="text-base font-semibold leading-7 text-blue-600">
+                  Pricing
+                </h2>
+                <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl">
+                  Upgrade for branded community
                 </p>
-                <p
-                  className={classNames(
-                    tier.featured ? 'text-gray-300' : 'text-gray-600',
-                    'mt-6 text-base leading-7'
-                  )}
-                >
-                  {tier.description}
-                </p>
-                <ul
-                  role="list"
-                  className={classNames(
-                    tier.featured ? 'text-gray-300' : 'text-gray-600',
-                    'mt-8 space-y-3 text-sm leading-6 sm:mt-10'
-                  )}
-                >
-                  {tier.features.map((feature) => (
-                    <li key={feature} className="flex gap-x-3">
-                      <GoCheck
-                        className={classNames(
-                          tier.featured ? 'text-blue-400' : 'text-blue-600',
-                          'h-6 w-5 flex-none'
-                        )}
-                        aria-hidden="true"
-                      />
-                      {feature}
-                    </li>
-                  ))}
-                </ul>
-                <a
-                  href={'/signup'}
-                  aria-describedby={tier.id}
-                  className={classNames(
-                    tier.featured
-                      ? 'bg-blue-500 text-white shadow-sm hover:bg-blue-400 focus-visible:outline-blue-500'
-                      : 'text-blue-600 ring-1 ring-inset ring-blue-200 hover:ring-blue-300 focus-visible:outline-blue-600',
-                    'mt-8 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10'
-                  )}
-                >
-                  Get started today
-                </a>
               </div>
-            ))}
+              <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-gray-300">
+                Hosting your own branded community on your website can boost
+                your online presence through SEO and ease the load on your
+                support team by encouraging members to find answers on Google.{' '}
+              </p>
+              <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                {tiers.map((tier, tierIdx) => (
+                  <div
+                    key={tier.id}
+                    className={classNames(
+                      tier.mostPopular
+                        ? 'lg:z-10 lg:rounded-b-none'
+                        : 'lg:mt-8',
+                      tierIdx === 0 ? 'lg:rounded-r-none' : '',
+                      tierIdx === tiers.length - 1 ? 'lg:rounded-l-none' : '',
+                      'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10'
+                    )}
+                  >
+                    <div>
+                      <div className="flex items-center justify-between gap-x-4">
+                        <h3
+                          id={tier.id}
+                          className={classNames(
+                            tier.mostPopular
+                              ? 'text-blue-600'
+                              : 'text-gray-900',
+                            'text-lg font-semibold leading-8'
+                          )}
+                        >
+                          {tier.name}
+                        </h3>
+                        {tier.mostPopular ? (
+                          <p className="rounded-full bg-blue-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-blue-600">
+                            Most popular
+                          </p>
+                        ) : null}
+                      </div>
+                      <p className="mt-6 flex items-baseline gap-x-1">
+                        <span className="text-4xl font-bold tracking-tight text-gray-900">
+                          {tier.priceMonthly}
+                        </span>
+                        <span className="text-sm font-semibold leading-6 text-gray-600">
+                          /month
+                        </span>
+                      </p>
+                      <p className="mt-4 text-sm leading-6 text-gray-600">
+                        {tier.description}
+                      </p>
+                      <ul
+                        role="list"
+                        className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+                      >
+                        {tier.features.map((feature) => (
+                          <li key={feature} className="flex gap-x-3">
+                            <GoCheck
+                              className="h-6 w-5 flex-none text-blue-600"
+                              aria-hidden="true"
+                            />
+                            {feature}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <a
+                      href={tier.href}
+                      aria-describedby={tier.id}
+                      className={classNames(
+                        tier.mostPopular
+                          ? 'bg-blue-600 text-white shadow-sm hover:bg-blue-500'
+                          : 'text-blue-600 ring-1 ring-inset ring-blue-200 hover:ring-blue-300',
+                        'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+                      )}
+                    >
+                      Get Started today
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </main>
