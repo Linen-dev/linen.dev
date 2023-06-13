52656aaf899410febbdbd9b98c8fc86454dc22ea
Sandro de Souza <sandesou@gmail.com>
2023-06-13 14:12:57 -0300

    asyncFn and isMounted hooks

---
+++ packages/hooks/src/useAsync.tsx
@@ -0,0 +1,17 @@
+import { DependencyList, useEffect } from 'react';
+import useAsyncFn, { FunctionReturningPromise } from './useAsyncFn';
+
+export default function useAsync<T extends FunctionReturningPromise>(
+  fn: T,
+  deps: DependencyList = []
+) {
+  const [state, callback] = useAsyncFn(fn, deps, {
+    loading: true,
+  });
+
+  useEffect(() => {
+    callback();
+  }, [callback]);
+
+  return state;
+}
