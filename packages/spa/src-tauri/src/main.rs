625d945e4a81472b1d611bbd4ba19efc92caf203
Sandro de Souza <sandesou@gmail.com>
2023-06-14 13:59:40 -0300

    fix ubuntu/windows desktop deps

--- packages/spa/src-tauri/src/main.rs
+++ packages/spa/src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[cfg(target_os = "macos")]
 use notifications::{ Instance, NotificationPayload };
-
 use tauri::{ Manager, WindowEvent };
 mod notifications;
 
