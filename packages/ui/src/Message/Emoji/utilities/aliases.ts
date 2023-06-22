bca22ba81902f4ea48532c3b758813876c9299f5
Emil Ajdyna <emil@ajdyna.com>
2023-06-22 13:23:25 +0200

    Add few more aliases

--- packages/ui/src/Message/Emoji/utilities/aliases.ts
+++ packages/ui/src/Message/Emoji/utilities/aliases.ts
@@ -1,11 +1,18 @@
 export default {
   thumbsup: '👍',
+  '+1': '👍',
   thumbsdown: '👎',
+  '-1': '👎',
   rock: '🪨',
   smiling_face_with_tear: '🥲',
   smiling_face_with_3_hearts: '🥰',
   shame: '😳',
   white_heavy_check_mark: '✅',
-  // it would be nice to support gif emojis
+  done: '✅',
+  pants: '👖',
+  squirrel: '🐿️',
+  // it would be nice to support gif, jpg, png emojis
   partyparrot: '🦜',
+  charmander: '🔥',
+  java: '☕',
 };
