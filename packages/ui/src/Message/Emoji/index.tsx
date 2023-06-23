a5cf5aeff180a014b5d721afbd29b9497087e237
Emil Ajdyna <emil@ajdyna.com>
2023-06-23 13:07:53 +0200

    Attempt to fix an emoji bundling error

--- packages/ui/src/Message/Emoji/index.tsx
+++ packages/ui/src/Message/Emoji/index.tsx
@@ -5,6 +5,17 @@ interface Props {
   text?: string;
 }
 
+// our production build process incorrectly optimizes the following key from emojis.json
+// { "+1": "👍" }
+// the code above is converted to:
+// { 1: "👍" }
+// the issue does most likely come from next.js minification located here
+// https://github.com/vercel/next.js/blob/df08b22e7ff783523ef8fc27913d6480e66f2db0/packages/next/src/build/webpack/plugins/terser-webpack-plugin/src/minify.ts#L4
+// I haven't been able to reproduce this error with terser directly though.
+// Setting up next.js locally for development is too time consuming.
+// To make this issue even more interesting, server side rendering works fine
+EMOJIS['+1'] = '👍';
+
 function unwrap(text: string): string {
   if (text.startsWith(':') && text.endsWith(':')) {
     return text.slice(1, text.length - 1);
