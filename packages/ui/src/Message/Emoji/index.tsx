6c5acac59365a6b669c0a354f42a1d3e4e202772
Emil Ajdyna <emil@ajdyna.com>
2023-06-23 10:30:43 +0200

    Simplify emojis

--- packages/ui/src/Message/Emoji/index.tsx
+++ packages/ui/src/Message/Emoji/index.tsx
@@ -1,35 +1,21 @@
 import React from 'react';
-import data from '@emoji-mart/data';
-import ALIASES from './utilities/aliases';
-
+// @ts-ignore
+import EMOJIS from './utilities/emojis.json';
 interface Props {
   text?: string;
+}
+
+function unwrap(text: string): string {
+  if (text.startsWith(':') && text.endsWith(':')) {
+    return text.slice(1, text.length - 1);
+  }
+  return text;
 }
 
 export default function Emoji({ text }: Props) {
   if (!text) {
     return null;
   }
-  if (text.startsWith(':') && text.endsWith(':')) {
-    const name = text.slice(1, text.length - 1);
-    const alias = (ALIASES as { [key: string]: string })[name];
-    if (alias) {
-      return <>{alias}</>;
-    }
-    const { aliases, emojis }: any = data;
-    if (name.includes('::')) {
-      const [id, tone] = name.split('::');
-      if (tone.startsWith('skin-tone-')) {
-        const index = Number(tone[tone.length - 1]);
-        const emoji = emojis[id]?.skins[index]?.native;
-        if (emoji) {
-          return emoji;
-        }
-      }
-    }
-    const id = aliases[name] || name;
-    const emoji = emojis[id]?.skins[0]?.native;
-    return <>{emoji || text}</>;
-  }
-  return <>{text}</>;
+  const name = unwrap(text);
+  return <>{EMOJIS[name] || text}</>;
 }
