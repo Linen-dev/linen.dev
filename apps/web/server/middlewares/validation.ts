ebae6bfce69d65fc45988e62d6ab1bbfb5ce2867
Sandro de Souza <sandesou@gmail.com>
2023-06-29 10:48:49 -0300

    return bad request errors

--- apps/web/server/middlewares/validation.ts
+++ apps/web/server/middlewares/validation.ts
@@ -4,12 +4,12 @@ export default function validationMiddleware(
   schema: any,
   value: 'body' | 'query' | 'params' = 'body' // DEPRECATE this param
 ) {
-  return (req: Request, _: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     try {
       req.body = schema.parse({ ...req.params, ...req.query, ...req.body });
       return next();
-    } catch (error) {
-      return next(error);
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
     }
   };
 }
