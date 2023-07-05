ca62bc688ab518b4264705e5e8c95ff839402cc9
Emil Ajdyna <emil@ajdyna.com>
2023-07-05 09:51:17 +0200

    Replace the second db call

--- apps/web/services/feed/index.ts
+++ apps/web/services/feed/index.ts
@@ -1,7 +1,8 @@
-import { prisma } from '@linen/database';
+import { accounts, prisma } from '@linen/database';
 import { yesterday } from '@linen/utilities/date';
 import { serializeThread } from '@linen/serializers/thread';
 import { serializeSettings } from '@linen/serializers/settings';
+import unique from 'lodash.uniq';
 
 export default class FeedService {
   static async get({ skip, take }: { skip: number; take: number }) {
@@ -40,20 +41,20 @@ export default class FeedService {
           },
           orderBy: { sentAt: 'asc' },
         },
-        channel: true,
+        channel: {
+          include: {
+            account: true,
+          },
+        },
       },
       orderBy: { lastReplyAt: 'desc' },
       take,
       skip,
     });
 
-    const accounts = await prisma.accounts.findMany({
-      where: {
-        id: {
-          in: threads.map((thread) => thread.channel.accountId) as string[],
-        },
-      },
-    });
+    const accounts = unique(
+      threads.map((thread) => thread.channel.account)
+    ) as accounts[];
 
     return {
       threads: threads.map(serializeThread),
