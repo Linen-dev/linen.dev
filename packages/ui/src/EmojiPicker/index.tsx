406e6bdf510a61cf82fe4c18fddc64767d808f5b
Emil Ajdyna <emil@ajdyna.com>
2023-07-10 13:33:00 +0200

    Equal padding

--- packages/ui/src/EmojiPicker/index.tsx
+++ packages/ui/src/EmojiPicker/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Picker from '@emoji-mart/react';
+import styles from './index.module.scss';
 
 interface Props {
   onSelect(emoji: any): void;
@@ -7,16 +8,19 @@ interface Props {
 
 export default function EmojiPicker({ onSelect }: Props) {
   return (
-    <Picker
-      data={async () => {
-        const response = await fetch(
-          'https://cdn.jsdelivr.net/npm/@emoji-mart/data'
-        );
-        return response.json();
-      }}
-      onEmojiSelect={onSelect}
-      previewPosition="none"
-      searchPosition="none"
-    />
+    <div className={styles.container}>
+      <Picker
+        data={async () => {
+          const response = await fetch(
+            'https://cdn.jsdelivr.net/npm/@emoji-mart/data'
+          );
+          return response.json();
+        }}
+        onEmojiSelect={onSelect}
+        previewPosition="none"
+        searchPosition="none"
+        dynamicWidth
+      />
+    </div>
   );
 }
