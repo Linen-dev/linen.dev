ccb2bca56577b8965d87708fe2a2c6ca3f5195a0
Emil Ajdyna <emil@ajdyna.com>
2023-07-10 12:22:05 +0200

    CORS for /api/feed

--- apps/web/pages/api/feed/index.tsx
+++ apps/web/pages/api/feed/index.tsx
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import FeedService from 'services/feed';
+import { cors, preflight } from 'utilities/cors';
 
 export async function index({ skip, take }: { skip: number; take: number }) {
   const { threads, settings, communities } = await FeedService.get({
@@ -21,6 +22,10 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  if (request.method === 'OPTIONS') {
+    return preflight(request, response, ['GET']);
+  }
+  cors(request, response);
   const skip = Number(request.query.skip);
   const take = Number(request.query.take);
   const { status, data } = await index({ skip, take });
