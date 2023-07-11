b6c900fa2855536c22f451785a7640f7991557a0
Sandro de Souza <sandesou@gmail.com>
2023-07-11 14:44:53 -0300

    update env for push notification

--- packages/hooks/src/utils/getPushUrl.ts
+++ packages/hooks/src/utils/getPushUrl.ts
@@ -2,6 +2,9 @@ export function getPushUrl() {
   if (!!process.env.REACT_APP_PUSH_SERVICE_URL) {
     return process.env.REACT_APP_PUSH_SERVICE_URL;
   }
+  if (!!process.env.NEXT_PUBLIC_PUSH_SERVICE_URL) {
+    return process.env.NEXT_PUBLIC_PUSH_SERVICE_URL;
+  }
   if (!!process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF) {
     return `wss://push.${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}.linendev.com`;
   }
