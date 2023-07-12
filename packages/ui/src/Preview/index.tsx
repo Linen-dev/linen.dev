7db6bcadd779a5e34c9b9cfaccbb5f349b742487
Emil Ajdyna <emil@ajdyna.com>
2023-07-12 09:51:07 +0200

    Prevent default actions when showing a preview of an image

--- packages/ui/src/Preview/index.tsx
+++ packages/ui/src/Preview/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.module.scss';
 
 interface Props {
-  onClick(): void;
+  onClick(event: React.MouseEvent<HTMLDivElement>): void;
   children: React.ReactNode;
 }
 
