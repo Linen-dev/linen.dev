8f78389b78e7f7e01252ee4381cdc4541f2f9833
Emil Ajdyna <emil@ajdyna.com>
2023-07-18 13:29:15 +0200

    Channels grid improvements

--- packages/ui/src/ChannelsView/index.tsx
+++ packages/ui/src/ChannelsView/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Label from '@/Label';
 import Toggle from '@/Toggle';
+import Tooltip from '@/Tooltip';
 import NativeSelect from '@/NativeSelect';
 import LandingChannelRow from './LandingChannelRow';
 import { SerializedAccount, SerializedChannel } from '@linen/types';
@@ -27,7 +28,7 @@ export default function ChannelsView({
     <div className={styles.wrapper}>
       <Header />
       <div className={styles.content}>
-        <LandingChannelRow
+        {/* <LandingChannelRow
           key={`landing-row-${key}`}
           channels={channels}
           currentCommunity={currentCommunity}
@@ -43,23 +44,43 @@ export default function ChannelsView({
               });
             })
           }
-        />
-        <hr className={styles.my3} />
+        /> */}
+        {/* <hr className={styles.my3} />
         <Label htmlFor="channels">
           Channels
           <Label.Description>
             Drag and drop channels to define the order, decide which ones are
             shown to new members or hidden to everyone.
           </Label.Description>
-        </Label>
+        </Label> */}
         <table className={styles.table}>
           <thead>
             <tr>
               <th>Channel Name</th>
-              <th>Type</th>
-              <th>View Type</th>
-              <th>Default</th>
-              <th>Visibility</th>
+              <th>
+                Type
+                <br />
+                <small>Who can access this channel</small>
+              </th>
+              <th>
+                View Type
+                <br />
+                <small>How members see this channel</small>
+              </th>
+              <th>
+                Default
+                <br />
+                <small>Joined by new members</small>
+              </th>
+              <th>
+                Landing
+                <br />
+                <small>First channel a new member sees</small>
+              </th>
+              <th>
+                Visibility <br />
+                <small>Can members access this channel</small>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -129,11 +150,65 @@ export default function ChannelsView({
                     checked={channel.default}
                     onChange={(checked) => {
                       setChannels((channels: SerializedChannel[]) => {
-                        return channels.map((c: SerializedChannel) => {
+                        const result = channels.map((c: SerializedChannel) => {
                           if (c.id === channel.id) {
+                            if (checked) {
+                              return {
+                                ...c,
+                                default: true,
+                              };
+                            } else {
+                              return {
+                                ...c,
+                                default: false,
+                                landing: false,
+                              };
+                            }
+                          }
+                          return c;
+                        });
+
+                        const hasDefault = result.find(
+                          (channel) => channel.default
+                        );
+                        const hasLanding = result.find(
+                          (channel) => channel.landing
+                        );
+                        if (!hasLanding && hasDefault) {
+                          const defaultIndex = result.findIndex(
+                            (channel) => channel.default
+                          );
+                          return result.map((channel, index) => {
+                            if (index === defaultIndex) {
+                              return {
+                                ...channel,
+                                landing: true,
+                              };
+                            }
+                            return channel;
+                          });
+                        }
+                        return result;
+                      });
+                    }}
+                  />
+                </td>
+                <td>
+                  <Toggle
+                    checked={channel.landing}
+                    onChange={(checked) => {
+                      setChannels((channels: SerializedChannel[]) => {
+                        return channels.map((c: SerializedChannel) => {
+                          if (checked) {
+                            if (c.id === channel.id) {
+                              return {
+                                ...c,
+                                landing: true,
+                              };
+                            }
                             return {
                               ...c,
-                              default: checked,
+                              landing: false,
                             };
                           }
                           return c;
