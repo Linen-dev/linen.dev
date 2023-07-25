8ca15f829e2379946df0022c59c2a5eb332662db
Emil Ajdyna <emil@ajdyna.com>
2023-07-25 09:00:31 +0200

    Add keyboard navigation for image previews

--- packages/ui/src/ImagePreview/index.tsx
+++ packages/ui/src/ImagePreview/index.tsx
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Portal from '@/Portal';
 import Preview from '@/Preview';
+import useKeyboard from '@linen/hooks/keyboard';
 import styles from './index.module.scss';
 
 interface Props {
-  src: string;
+  current: string;
+  images: string[];
   onClick(): void;
 }
 
-export default function ImagePreview({ src, onClick }: Props) {
+export default function ImagePreview({ current, images, onClick }: Props) {
+  const [index, setIndex] = useState(images.indexOf(current));
   useEffect(() => {
     if (document.body) {
       document.body.classList.add(styles.overflow);
     }
   }, []);
+
+  useKeyboard(
+    {
+      onKeyUp(event: KeyboardEvent) {
+        switch (event.key) {
+          case 'ArrowRight':
+            return setIndex((index) => {
+              if (index === images.length - 1) {
+                return 0;
+              }
+              return index + 1;
+            });
+          case 'ArrowLeft':
+            return setIndex((index) => {
+              if (index === 0) {
+                return images.length - 1;
+              }
+              return index - 1;
+            });
+        }
+      },
+    },
+    [images]
+  );
 
   return (
     <Portal id="preview-portal">
@@ -27,7 +54,7 @@ export default function ImagePreview({ src, onClick }: Props) {
           }
         }}
       >
-        <img className={styles.preview} src={src} />
+        <img className={styles.preview} src={images[index]} />
       </Preview>
     </Portal>
   );
