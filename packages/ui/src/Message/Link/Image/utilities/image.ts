dc74aedfa2193a36e8c9734d5104063da7047dfe
Emil Ajdyna <emil@ajdyna.com>
2023-07-27 12:09:43 +0200

    Calculate default image dimensions

---
+++ packages/ui/src/Message/Link/Image/utilities/image.ts
@@ -0,0 +1,35 @@
+import { getImageDimensionsFromUrl } from '@linen/utilities/files';
+
+export function getWidthHeight(src: string) {
+  const dimensions = getImageDimensionsFromUrl(src);
+  return calculateDimensions(dimensions);
+}
+
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
+export function calculateDimensions(dimensions: Dimensions | null): Dimensions {
+  if (!dimensions || !dimensions.width || !dimensions.height) {
+    return { width: 200, height: 200 };
+  }
+
+  if (dimensions.height > 400) {
+    let height = dimensions.height;
+    let width = dimensions.width;
+    while (height > 400) {
+      height = Math.floor(height / 2);
+      width = Math.floor(width / 2);
+    }
+    return {
+      width,
+      height,
+    };
+  }
+
+  return {
+    width: dimensions.width,
+    height: dimensions.height,
+  };
+}
