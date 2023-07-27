c84db598770791db82c6c7f6a146de789d094e4f
Emil Ajdyna <emil@ajdyna.com>
2023-07-27 10:43:30 +0200

    Add a utility for getting image dimensions for a url

--- packages/utilities/src/files.ts
+++ packages/utilities/src/files.ts
@@ -1,5 +1,3 @@
-import { getImageDimensions } from './image';
-
 export const FILE_SIZE_LIMIT_IN_BYTES = 2 * 1024 * 1024;
 
 export function getExtension(filename: string) {
@@ -24,13 +22,70 @@ export function isImage(name: string): boolean {
   return SUPPORTED_EXTENSIONS.includes(extension.toLowerCase());
 }
 
+export async function fetchImageDimensions(
+  file: File
+): Promise<{ width: number; height: number }> {
+  return new Promise((resolve, reject) => {
+    var url = URL.createObjectURL(file);
+    var image = new Image();
+
+    image.onload = function () {
+      if (!image.width || !image.height) {
+        return reject(new Error(`Image is empty ${file.name}`));
+      }
+      resolve({
+        width: image.width,
+        height: image.height,
+      });
+      URL.revokeObjectURL(image.src);
+    };
+
+    image.onerror = function () {
+      reject(new Error(`Couldn't load the image ${file.name}`));
+    };
+
+    image.src = url;
+  });
+}
+
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
+export function getImageDimensionsFromUrl(filename: string): Dimensions | null {
+  const extension = getExtension(filename) as string;
+  const underscore = filename.lastIndexOf('_');
+  const x = filename.lastIndexOf('x');
+  if (underscore === -1 || x === -1) {
+    return null;
+  }
+  if (x < underscore) {
+    return null;
+  }
+  const part = filename.substring(
+    underscore + 1,
+    filename.length - extension.length - 1
+  );
+  const [part1, part2] = part.split('x');
+
+  const width = Number(part1);
+  const height = Number(part2);
+
+  if (width && height) {
+    return { width, height };
+  }
+
+  return null;
+}
+
 export async function getFormData(files: File[]) {
   const data = new FormData();
   for (let index = 0, length = files.length; index < length; index++) {
     const file = files[index];
     if (isImage(file.name)) {
       try {
-        const { width, height } = await getImageDimensions(file);
+        const { width, height } = await fetchImageDimensions(file);
         const extension = getExtension(file.name) as string;
         const name =
           file.name.substring(0, file.name.length - extension.length - 1) +
