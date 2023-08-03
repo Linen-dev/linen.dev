d6b70f72ed118d9f945d7ce33e383b04c8b11458
Sandro de Souza <sandesou@gmail.com>
2023-08-03 11:26:26 -0300

    skip unknown events from slack webhook

--- apps/web/services/slack/webhooks/index.ts
+++ apps/web/services/slack/webhooks/index.ts
@@ -12,25 +12,28 @@ import { processUserProfileChanged } from './processUserProfileChanged';
 import { processTeamJoin } from './processTeamJoin';
 
 export const handleWebhook = async (body: SlackEvent, logger: Logger) => {
-  if (body.event.type === 'team_join') {
-    return processTeamJoin(body);
-  } else if (body.event.type === 'message') {
-    return processMessageEvent(body, logger);
-  } else if (body.event.type === 'reaction_added') {
-    return processMessageReactionAddedEvent(body, logger);
-  } else if (body.event.type === 'reaction_removed') {
-    return processMessageReactionRemovedEvent(body, logger);
-  } else if (body.event.type === 'channel_created') {
-    return processChannelCreated(body);
-  } else if (body.event.type === 'channel_rename') {
-    return processChannelRename(body);
-  } else if (body.event.type === 'user_profile_changed') {
-    return processUserProfileChanged(body);
-  } else {
-    logger.error({ error: 'Event not supported!!' });
-    return {
-      status: 404,
-      error: 'Event not supported',
-    };
+  if (!body.event.type) {
+    logger.error({ error: 'Event not supported!!', body });
+    return;
+  }
+
+  switch (body.event.type) {
+    case 'team_join':
+      return processTeamJoin(body);
+    case 'message':
+      return processMessageEvent(body, logger);
+    case 'reaction_added':
+      return processMessageReactionAddedEvent(body, logger);
+    case 'reaction_removed':
+      return processMessageReactionRemovedEvent(body, logger);
+    case 'channel_created':
+      return processChannelCreated(body);
+    case 'channel_rename':
+      return processChannelRename(body);
+    case 'user_profile_changed':
+      return processUserProfileChanged(body);
+    default:
+      logger.error({ error: 'Event not supported!!', body });
+      return;
   }
 };
