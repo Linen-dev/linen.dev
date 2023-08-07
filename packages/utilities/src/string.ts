b9f5627c5046473e809e4c4d95b59d2632e844f2
Emil Ajdyna <emil@ajdyna.com>
2023-08-07 11:27:53 +0200

    Move utilities to packages

--- packages/utilities/src/string.ts
+++ packages/utilities/src/string.ts
@@ -55,3 +55,15 @@ export const slugify = (message: string) => {
   }
   return slug;
 };
+
+function isLetter(character: string): boolean {
+  return character.toLowerCase() !== character.toUpperCase();
+}
+
+export function getLetter(text: string): string {
+  const character = text.trim().slice(0, 1).toLowerCase();
+  if (isLetter(character)) {
+    return character;
+  }
+  return 'u';
+}
