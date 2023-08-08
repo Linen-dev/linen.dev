ccbd783cff7b02ea61cfbd81ba8210129ff278ef
Emil Ajdyna <emil@ajdyna.com>
2023-08-08 10:02:49 +0200

    Calculate dimensions based on width too

--- packages/ui/src/Message/Link/Image/utilities/image.ts
+++ packages/ui/src/Message/Link/Image/utilities/image.ts
@@ -15,10 +15,10 @@ export function calculateDimensions(dimensions: Dimensions | null): Dimensions {
     return { width: 200, height: 200 };
   }
 
-  if (dimensions.height > 400) {
+  if (dimensions.height > 400 || dimensions.width > 400) {
     let height = dimensions.height;
     let width = dimensions.width;
-    while (height > 400) {
+    while (height > 400 || width > 400) {
       height = Math.floor(height / 2);
       width = Math.floor(width / 2);
     }
