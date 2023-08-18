d54bb45d1ee78d5226426060f0fa1ff6824690f6
Emil Ajdyna <emil@ajdyna.com>
2023-08-18 10:56:01 +0200

    Add a hint

--- packages/ui/src/Label/index.tsx
+++ packages/ui/src/Label/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import { FiInfo } from '@react-icons/all-files/fi/FiInfo';
 import styles from './index.module.scss';
 
 interface Props {
@@ -20,6 +21,18 @@ function Description({ children }: { children: React.ReactNode }) {
   return <div className={styles.description}>{children}</div>;
 }
 
+function Hint({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.hint}>
+      <div className={styles.content}>
+        <FiInfo />
+        {children}
+      </div>
+    </div>
+  );
+}
+
 Label.Description = Description;
+Label.Hint = Hint;
 
 export default Label;
