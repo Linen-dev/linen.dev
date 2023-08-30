cdb689b56acf24b196b56a81461b20620ad8e2d2
Emil Ajdyna <emil@ajdyna.com>
2023-08-30 08:33:45 +0200

    Remove the JoinSlack and JoinDiscord links

--- packages/ui/src/JoinButton/index.tsx
+++ packages/ui/src/JoinButton/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import JoinLinen from './JoinLinen';
-import JoinDiscord from './JoinDiscord';
-import JoinSlack from './JoinSlack';
-import { ChatType, SerializedAccount, Settings } from '@linen/types';
+import { SerializedAccount, Settings } from '@linen/types';
 import type { ApiClient } from '@linen/api-client';
 
 interface WrapperProps {
@@ -25,41 +23,17 @@ export default function JoinButton({
   api,
   reload,
 }: WrapperProps) {
-  return ({ brandColor, fontColor, currentCommunity, settings }: Props) => {
-    if (status === 'loading') {
-      return <div />;
-    }
-
-    if (
-      currentCommunity.premium &&
-      settings.communityInviteUrl &&
-      settings.chat !== ChatType.MEMBERS
-    ) {
-      return settings.communityType === 'discord' ? (
-        <JoinDiscord
-          brandColor={brandColor}
-          fontColor={fontColor}
-          href={settings.communityInviteUrl}
-        />
-      ) : (
-        <JoinSlack
-          brandColor={brandColor}
-          fontColor={fontColor}
-          href={settings.communityInviteUrl}
-        />
-      );
-    } else {
-      return (
-        <JoinLinen
-          brandColor={brandColor}
-          fontColor={fontColor}
-          accountId={settings.communityId}
-          status={status}
-          startSignUp={startSignUp}
-          api={api}
-          reload={reload}
-        />
-      );
-    }
+  return ({ brandColor, fontColor, settings }: Props) => {
+    return (
+      <JoinLinen
+        brandColor={brandColor}
+        fontColor={fontColor}
+        accountId={settings.communityId}
+        status={status}
+        startSignUp={startSignUp}
+        api={api}
+        reload={reload}
+      />
+    );
   };
 }
