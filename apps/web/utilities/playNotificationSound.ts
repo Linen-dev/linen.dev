61cbad7388eae800d8ac10fdfe136ebb5cf1da77
Emil Ajdyna <emil@ajdyna.com>
2023-09-07 13:18:31 +0200

    Remove a console.log

--- apps/web/utilities/playNotificationSound.ts
+++ apps/web/utilities/playNotificationSound.ts
@@ -1,11 +1,13 @@
 export const playNotificationSound = async (volume: number) => {
-  console.log('play notification');
   try {
     const file = 'https://static.main.linendev.com/public/alert.mp3';
     const audio = new Audio(file);
-    audio.volume = volume;
 
-    await audio?.play();
+    if (audio) {
+      audio.volume = volume;
+
+      await audio.play();
+    }
   } catch (err) {
     console.error('Failed to play notification sound:', err);
   }
