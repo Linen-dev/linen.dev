ad75f81c05f706ab27a9354a75f027f3043664f1
Sandro de Souza <sandesou@gmail.com>
2023-10-04 14:06:18 -0300

    debug: turn off llmQuestion on queue

--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -88,7 +88,7 @@ async function runWorker() {
       typesenseOnMessageUpdate,
       typesenseOnThreadCreation,
       typesenseOnThreadUpdate,
-      llmQuestion,
+      // llmQuestion,
     },
     parsedCronItems: parseCronItems([
       {
