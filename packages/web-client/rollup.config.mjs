6911b7781a63e70ea0d44c8d4dd0f626f6aa1a37
Emil Ajdyna <emil@ajdyna.com>
2023-10-09 10:02:33 +0200

    Add typescript to the mix

--- packages/web-client/rollup.config.mjs
+++ packages/web-client/rollup.config.mjs
@@ -1,7 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: {
     dir: 'dist',
     format: 'cjs',
   },
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+  ],
 };
---
+++ packages/web-client/src/index.ts
@@ -0,0 +1,7 @@
+const node = document.getElementById('app');
+
+if (node) {
+  const image = document.createElement('img');
+  image.alt = 'Linen Logo';
+  node.appendChild(image);
+}
