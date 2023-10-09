50c95062d64de21bf832e45cf530f0159294fb84
Emil Ajdyna <emil@ajdyna.com>
2023-10-09 10:34:10 +0200

    Render the logo via react

--- packages/web-client/src/index.ts
+++
@@ -1,7 +0,0 @@
-const node = document.getElementById('app');
-
-if (node) {
-  const image = document.createElement('img');
-  image.alt = 'Linen Logo';
-  node.appendChild(image);
-}
---
+++ packages/web-client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const domNode = document.getElementById('root');
+if (domNode) {
+  const root = createRoot(domNode);
+  root.render(<App />);
+}
