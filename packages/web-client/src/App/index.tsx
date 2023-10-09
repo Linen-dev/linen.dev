84c3bb91284a15e5481bbdacb48f69f6ddc34121
Emil Ajdyna <emil@ajdyna.com>
2023-10-09 13:41:49 +0200

    Loading finished example

--- packages/web-client/src/App/index.tsx
+++ packages/web-client/src/App/index.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SplashLoader from './SplashLoader';
 import DefaultLayout from './DefaultLayout';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let mounted = true;
+    setTimeout(() => {
+      if (mounted) {
+        setLoading(false);
+      }
+    }, 2000);
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
   if (loading) {
     return <SplashLoader />;
   }
