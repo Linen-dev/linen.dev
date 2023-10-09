e54c1aa9654451e8b89b444cceff2744bc8bd57b
Emil Ajdyna <emil@ajdyna.com>
2023-10-09 10:34:10 +0200

    Render the logo via react

--- packages/web-client/rollup.config.mjs
+++ packages/web-client/rollup.config.mjs
@@ -1,14 +1,27 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import replace from '@rollup/plugin-replace';
+import image from '@rollup/plugin-image';
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: {
     dir: 'dist',
-    format: 'cjs',
+    format: 'umd',
   },
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
+    }),
+    resolve(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
+    image({
+      dom: false,
     }),
   ],
 };
---
+++ packages/web-client/src/App/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Logo from './Logo';
+
+export default function App() {
+  return <Logo />;
+}
