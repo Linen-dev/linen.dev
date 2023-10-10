ff62c070a817b9540f2223400e0b69ad7c98cd21
Emil Ajdyna <emil@ajdyna.com>
2023-10-10 10:01:09 +0200

    Add an example api call in the spa

--- packages/spa/test/server.js
+++ packages/spa/test/server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 8000;
 app.use(express.static(join(__dirname, 'public')));
 app.use(express.static(join(__dirname, '../dist')));
 
+app.get('/api/spa/start', (_, response) => {
+  response.status(200).json({});
+});
+
 app.listen(port, () => {
-  console.log(`web-client development server running on port ${port}`);
+  console.log(`spa development server running on port ${port}`);
 });
