3046658965a514143a9673de6e6bc3de6b8072b1
Emil Ajdyna <emil@ajdyna.com>
2023-10-10 13:02:30 +0200

    Serialize permissions

--- packages/spa/src/App/Header/index.tsx
+++ packages/spa/src/App/Header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SerializedAccount } from '@linen/types';
+import Component from '@linen/ui/Header';
 
 interface Props {
   currentCommunity: SerializedAccount;
 }
 
 export default function Header({ currentCommunity }: Props) {
-  return <header>{currentCommunity.name}</header>;
+  return <header>foo</header>;
 }
