67e7552f53a52ee131003b0d8af39a3ebd24acc3
Emil Ajdyna <emil@ajdyna.com>
2023-10-12 11:46:14 +0200

    Simplify the auth-client

--- packages/auth-client/src/server.ts
+++
@@ -1,7 +0,0 @@
-export * from './server/constraints';
-export * from './server/passport';
-export * from './server/cookies';
-export * from './server/csrf';
-export * from './server/session';
-export * from './server/tokens';
-export * from './server/router';
