101fa40c44c5229396d5301f74effa16abe6424d
Sandro de Souza <sandesou@gmail.com>
2024-04-04 00:18:19 -0300

    disable user join event

--- apps/web/services/events/eventUserJoin.ts
+++ apps/web/services/events/eventUserJoin.ts
@@ -1,46 +1,46 @@
 // import { createUserJoinJob } from 'queue/jobs';
 
-import { ChannelType, prisma } from '@linen/database';
-import { notifyChannels } from 'services/channels/userJoined';
+// import { ChannelType, prisma } from '@linen/database';
+// import { notifyChannels } from 'services/channels/userJoined';
 
 export type UserJoin = {
   userId: string;
 };
 
 export async function eventUserJoin(event: UserJoin) {
-  await Promise.allSettled([
-    userJoinTask(event), // run it synchronously
-  ]);
+  // await Promise.allSettled([
+  //   userJoinTask(event), // run it synchronously
+  // ]);
 }
 
-const userJoinTask = async (payload: { userId: string }) => {
-  const user = await prisma.users.findUnique({
-    where: { id: payload.userId },
-    include: { auth: true, account: true },
-  });
+// const userJoinTask = async (payload: { userId: string }) => {
+//   const user = await prisma.users.findUnique({
+//     where: { id: payload.userId },
+//     include: { auth: true, account: true },
+//   });
 
-  if (user && user.auth && user.account) {
-    const channels = await prisma.channels.findMany({
-      select: { id: true, channelName: true },
-      where: {
-        accountId: user.account.id,
-        hidden: false,
-        default: true,
-        type: ChannelType.PUBLIC,
-      },
-    });
+//   if (user && user.auth && user.account) {
+//     const channels = await prisma.channels.findMany({
+//       select: { id: true, channelName: true },
+//       where: {
+//         accountId: user.account.id,
+//         hidden: false,
+//         default: true,
+//         type: ChannelType.PUBLIC,
+//       },
+//     });
 
-    if (channels.length) {
-      await prisma.memberships.createMany({
-        data: channels.map((c) => {
-          return {
-            usersId: user.id,
-            channelsId: c.id,
-          };
-        }),
-        skipDuplicates: true,
-      });
-      await notifyChannels(user, channels);
-    }
-  }
-};
+//     if (channels.length) {
+//       await prisma.memberships.createMany({
+//         data: channels.map((c) => {
+//           return {
+//             usersId: user.id,
+//             channelsId: c.id,
+//           };
+//         }),
+//         skipDuplicates: true,
+//       });
+//       await notifyChannels(user, channels);
+//     }
+//   }
+// };
